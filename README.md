7d43de7d98ec1abecb0db63e76cf27c6aa5b0c8f
Eyal Bukchin <eyal@numerate.io>
2022-02-24 08:12:10 +0000

    First version that works

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A [Visual Studio Code](https://code.visualstudio.com/) extension that lets you easily redirect mirrored traffic from your production environment to your development environment.
 
+When you start debugging, mirrord will prompt you to select a pod to mirror traffic from. It will then mirror all traffic from that pod to the process you're debugging.
+
 ## Features
 
 * Mirroring API requests from your k8s environment to your debugged process.
@@ -18,4 +20,4 @@ To enable the extension, add the following fields to the launch.json of your deb
 
 ## Requirements
 
-kubectl needs to be running in your development environment.
+The extension uses **kubectl** commands, so it needs to be configured and running on your environment.
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
+import * as net from 'net';
+import { Log } from '@kubernetes/client-node';
 
 class ProcessCapturer implements vscode.DebugAdapterTracker {
 	static pid: number;
@@ -13,24 +14,35 @@ class ProcessCapturer implements vscode.DebugAdapterTracker {
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	let trackerDisposable = vscode.debug.registerDebugAdapterTrackerFactory('*', {
 		createDebugAdapterTracker(_session: vscode.DebugSession) {
 			return new ProcessCapturer();
 		}
 	});
-
 	context.subscriptions.push(trackerDisposable);
 
-	let debugDisposable = vscode.debug.onDidStartDebugSession(session => {
+	let debugDisposable = vscode.debug.onDidStartDebugSession(async session => {
 		const cp = require('child_process');
 
-		// Get pods from kubectl and let user select one to mirror
-		let pods = cp.execSync('kubectl get pods').toString().split('\n').slice(1, -1).map((line: string) => line.split(' ')[0]);
+		const k8s = require('@kubernetes/client-node');
 
-		vscode.window.showQuickPick(pods, { placeHolder: 'Select pod to mirror' }).then(pod => {
+		const kc = new k8s.KubeConfig();
+		kc.loadFromDefault();
+		const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+		// Get pods from kubectl and let user select one to mirror
+		let pods = await k8sApi.listNamespacedPod('default');
+		let podNames = pods.body.items.map((pod: { metadata: { name: any; }; }) => { return pod.metadata.name; });
+
+		vscode.window.showQuickPick(podNames, { placeHolder: 'Select pod to mirror' }).then(async pod => {
 			// Find the debugged process' port
+
+			// Infer container id from pod name
+			let containerID = cp.execSync('kubectl get -o jsonpath="{.status.containerStatuses[*].containerID}" pod ' + pod);
+
+			// Infert port from process ID
 			let port: string;
 			if (session.configuration.mirrord && session.configuration.mirrord.port) {
 				port = session.configuration.mirrord.port;
@@ -45,20 +57,78 @@ export function activate(context: vscode.ExtensionContext) {
 					console.log(e);
 				}
 			}
-			// Infer container id from pod name
-			let containerID = cp.execSync('kubectl get -o jsonpath="{.status.containerStatuses[*].containerID}" pod ' + pod);
-			let filePath = context.asAbsolutePath(path.join('bintest.sh'));
 
-			cp.exec('sh ' + filePath + ' ' + port + ' ' + containerID, (err: string, stdout: string, stderr: string) => {
-				console.log('stdout: ' + stdout);
-				console.log('stderr: ' + stderr);
-				if (err) {
-					console.log('error: ' + err);
+
+			let agentPod = {
+				metadata: { name: 'agentpod' },
+				spec: {
+					hostPID: true,
+					hostIPC: true,
+					volumes: [
+						{
+							name: 'containerd',
+							hostPath: { path: '/run/containerd/containerd.sock' }
+						}
+					],
+					containers: [
+						{
+							name: 'mirrord-agent',
+							image: 'ghcr.io/metalbear-co/mirrord-agent:main',
+							imagePullPolicy: 'Always',
+							securityContext: { privileged: true },
+							volumeMounts: [
+								{
+									mountPath: '/run/containerd/containerd.sock',
+									name: 'containerd'
+								}
+							],
+							command: [
+								"./mirrord-agent",
+								"--container-id",
+								containerID,
+								"--ports",
+								port
+							]
+						}
+					]
 				}
+
+			};
+
+			await k8sApi.createNamespacedPod('default', agentPod);
+
+			const net = require('net');
+			const stream = require('stream');
+			let log = new k8s.Log(kc);
+			let logStream = new stream.PassThrough();
+			let connections: { [connection_id: string]: net.Socket; } = {};
+
+			logStream.on('data', (chunk: Buffer) => {
+				chunk.toString().split('\n').forEach((line: string) => {
+					if (line) {
+						let parsedLine = JSON.parse(line);
+						let connectionId = parsedLine.content.connection_id;
+						if (parsedLine['type'] === 'Connected') {
+							let socket = new net.Socket();
+							socket.connect(port, 'localhost');
+							connections[connectionId] = socket;
+						}
+						else {
+							let socket = connections[connectionId];
+							if (parsedLine['type'] === 'Data') {
+								socket.write(Buffer.from(parsedLine.content.data, 'base64'));
+							}
+							if (parsedLine['type'] === 'TCPEnded') {
+								socket.end();
+								delete connections[connectionId];
+							}
+						}
+					}
+				});
 			});
-
-
+			await log.log('default', 'agentpod', '', logStream, (err: any) => { console.log(err); }, { follow: true, tailLines: 50, pretty: false, timestamps: false });
 		});
+
 	});
 
 
