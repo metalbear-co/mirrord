8cad54038980f5f43639665ed083ffcc60d64ad7
Eyal Bukchin <eyalb181@gmail.com>
2022-05-08 18:34:04 +0100

    Move some functionality to separate files (#31)

--- mirrord-layer/src/lib.rs
+++ mirrord-layer/src/lib.rs
@@ -1,34 +1,15 @@
-#![feature(c_variadic)]
+// #![feature(c_variadic)]
 
-use std::{
-    collections::HashMap,
-    env,
-    io::{Read, Write},
-    net::{IpAddr, Ipv4Addr, SocketAddr},
-    os::unix::io::AsRawFd,
-    sync::Mutex,
-    thread,
-    time::Duration,
-};
+use std::{sync::Mutex, thread, time::Duration};
 
 use ctor::ctor;
 use frida_gum::{interceptor::Interceptor, Error, Gum, Module, NativePointer};
 use futures::{SinkExt, StreamExt};
-use k8s_openapi::api::core::v1::Pod;
-use kube::{
-    api::{Api, Portforwarder, PostParams},
-    runtime::wait::{await_condition, conditions::is_pod_running},
-    Client,
-};
+use kube::api::Portforwarder;
 use lazy_static::lazy_static;
 use libc::{c_char, c_void, sockaddr, socklen_t};
 use mirrord_protocol::{ClientCodec, ClientMessage, DaemonMessage};
-use multi_map::MultiMap;
 use os_socketaddr::OsSocketAddr;
-use queues::{IsQueue, Queue};
-use rand::distributions::{Alphanumeric, DistString};
-use serde_json::json;
-use socketpair::{socketpair_stream, SocketpairStream};
 use tokio::{
     runtime::Runtime,
     select,
@@ -36,50 +17,33 @@ use tokio::{
 };
 use tracing::{debug, error};
 
-struct ConnectionSocket {
-    read_fd: i32,
-    read_socket: SocketpairStream,
-    real_read_socket_fd: i32,
-    write_socket: SocketpairStream,
-    port: u16,
-}
-
-struct DataSocket {
-    connection_id: u16,
-    #[allow(dead_code)]
-    read_socket: SocketpairStream, // Though this is unread, it's necessary to keep the socket open
-    write_socket: SocketpairStream,
-}
+mod pod_api;
+mod sockets;
 
 lazy_static! {
     static ref GUM: Gum = unsafe { Gum::obtain() };
     static ref RUNTIME: Runtime = Runtime::new().unwrap();
-    static ref CONNECTION_SOCKETS: Mutex<MultiMap<i32, u16, ConnectionSocket>> =
-        Mutex::new(MultiMap::new());
-    static ref DATA_SOCKETS_BY_CONNECTION_ID: Mutex<HashMap<u16, DataSocket>> = Mutex::new(HashMap::new());
-    static ref CONNECTION_QUEUE: Mutex<Queue<u16>> = Mutex::new(Queue::new());
-    static ref SENDER: Mutex<Option<Sender<u16>>> = Mutex::new(None); // TODO: Check OnceCell
-    static ref PENDING_DATA: Mutex<HashMap<u16, Vec<u8>>> = Mutex::new(HashMap::new());
+    static ref SOCKETS: sockets::Sockets = sockets::Sockets::default();
+    static ref NEW_CONNECTION_SENDER: Mutex<Option<Sender<i32>>> = Mutex::new(None);
 }
 
-unsafe extern "C" fn socket_detour(domain: i32, socket_type: i32, protocol: i32) -> i32 {
+#[ctor]
+fn init() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+    debug!("init called");
+
+    let pf = RUNTIME.block_on(pod_api::create_agent());
+    let (sender, receiver) = channel::<i32>(1000);
+    *NEW_CONNECTION_SENDER.lock().unwrap() = Some(sender);
+    enable_hooks();
+    RUNTIME.spawn(poll_agent(pf, receiver));
+}
+
+unsafe extern "C" fn socket_detour(_domain: i32, _socket_type: i32, _protocol: i32) -> i32 {
     debug!("socket called");
-    let (write_socket, read_socket) = socketpair_stream().unwrap();
-    let read_fd = read_socket.as_raw_fd();
-    let socket = ConnectionSocket {
-        read_fd,
-        read_socket,
-        real_read_socket_fd: libc::socket(domain, socket_type, protocol),
-        write_socket,
-        port: 0,
-    };
-
-    CONNECTION_SOCKETS
-        .lock()
-        .unwrap()
-        .insert(read_fd, 0, socket);
-
-    read_fd
+    SOCKETS.create_socket()
 }
 
 unsafe extern "C" fn bind_detour(sockfd: i32, addr: *const sockaddr, addrlen: socklen_t) -> i32 {
@@ -87,57 +51,33 @@ unsafe extern "C" fn bind_detour(sockfd: i32, addr: *const sockaddr, addrlen: so
     let parsed_addr = OsSocketAddr::from_raw_parts(addr as *const u8, addrlen as usize)
         .into_addr()
         .unwrap();
-    let port = parsed_addr.port();
 
-    let mut sockets = CONNECTION_SOCKETS.lock().unwrap();
-    if let Some(mut socket) = sockets.remove(&sockfd) {
-        socket.port = port;
-        sockets.insert(sockfd, port, socket);
-
-        0
-    } else {
-        error!("No socket found for fd: {}", sockfd);
-        -1
-    }
-}
-
-unsafe extern "C" fn connect_detour(sockfd: i32, addr: *const sockaddr, addrlen: socklen_t) -> i32 {
-    // Map port to fd
-    debug!("Called connect");
-    let real_fd = match CONNECTION_SOCKETS.lock().unwrap().get(&sockfd) {
-        Some(s) => s.real_read_socket_fd,
-        None => {
-            println!("No socket found for fd: {}", sockfd);
-            return -1;
-        }
-    };
-
-    libc::connect(real_fd, addr, addrlen)
+    SOCKETS.convert_to_connection_socket(sockfd, parsed_addr);
+    0
 }
 
 unsafe extern "C" fn listen_detour(sockfd: i32, _backlog: i32) -> i32 {
     debug!("listen called");
 
-    let mut sockets = CONNECTION_SOCKETS.lock().unwrap();
-
-    if let Some(socket) = sockets.remove(&sockfd) {
-        let lock = SENDER.lock().unwrap();
-        let send = lock.as_ref();
-        send.unwrap().blocking_send(socket.port).unwrap();
-        sockets.insert(sockfd, socket.port, socket);
-        0
-    } else {
-        error!("No socket found for fd: {}", sockfd);
-        -1
+    match SOCKETS.set_connection_state(sockfd, sockets::ConnectionState::Listening) {
+        Ok(()) => {
+            let sender = NEW_CONNECTION_SENDER.lock().unwrap();
+            sender.as_ref().unwrap().blocking_send(sockfd).unwrap(); // Tell main thread to subscribe to agent
+            0
+        }
+        Err(()) => {
+            error!("Failed to set connection state to listening");
+            -1
+        }
     }
 }
 
 unsafe extern "C" fn getpeername_detour(
-    _sockfd: i32,
+    sockfd: i32,
     addr: *mut sockaddr,
     addrlen: *mut socklen_t,
 ) -> i32 {
-    let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    let socket_addr = SOCKETS.get_data_socket_address(sockfd).unwrap();
     let os_addr: OsSocketAddr = socket_addr.into();
     let len = std::cmp::min(*addrlen as usize, os_addr.len() as usize);
     std::ptr::copy_nonoverlapping(os_addr.as_ptr() as *const u8, addr as *mut u8, len);
@@ -165,49 +105,16 @@ unsafe extern "C" fn accept_detour(
         "Accept called with sockfd {:?}, addr {:?}, addrlen {:?}",
         &sockfd, &addr, &addrlen
     );
+    let socket_addr = SOCKETS.get_connection_socket_address(sockfd).unwrap();
 
     if !addr.is_null() {
         debug!("received non-null address in accept");
-        let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
         let os_addr: OsSocketAddr = socket_addr.into();
         std::ptr::copy_nonoverlapping(os_addr.as_ptr(), addr, os_addr.len() as usize);
     }
-    let connection_id;
-    let mut sockets = CONNECTION_SOCKETS.lock().unwrap();
 
-    if let Some(mut socket) = sockets.remove(&sockfd) {
-        let mut buffer = [0; 1];
-        socket.read_socket.read_exact(&mut buffer).unwrap();
-        connection_id = CONNECTION_QUEUE.lock().unwrap().remove().unwrap();
-        sockets.insert(sockfd, socket.port, socket);
-    } else {
-        error!("No socket found for fd: {}", sockfd);
-        return -1;
-    }
-
-    let (read_socket, mut write_socket) = socketpair_stream().unwrap();
-    let read_fd = read_socket.as_raw_fd();
-    debug!(
-        "Accepted connection from read_fd:{:?}, write_sock:{:?}",
-        read_fd, write_socket
-    );
-
-    if let Some(data) = PENDING_DATA.lock().unwrap().remove(&connection_id) {
-        debug!("writing pending data for connection_id: {}", connection_id);
-        write_socket.write_all(&data).unwrap();
-    }
-
-    let data_socket = DataSocket {
-        connection_id,
-        read_socket,
-        write_socket,
-    };
-    DATA_SOCKETS_BY_CONNECTION_ID
-        .lock()
-        .unwrap()
-        .insert(connection_id, data_socket);
-
-    read_fd
+    let connection_id = SOCKETS.read_single_connection(sockfd);
+    SOCKETS.create_data_socket(connection_id, socket_addr)
 }
 
 unsafe extern "C" fn accept4_detour(
@@ -219,100 +126,17 @@ unsafe extern "C" fn accept4_detour(
     accept_detour(sockfd, addr, addrlen)
 }
 
-async fn create_agent() -> Portforwarder {
-    // Create Agent
-    let client = Client::try_default().await.unwrap();
-    let pods: Api<Pod> = Api::namespaced(client, "default");
-    let pod = pods
-        .get(&env::var("MIRRORD_IMPERSONATED_POD_NAME").unwrap())
-        .await
-        .unwrap();
-    let node_name = &pod.spec.unwrap().node_name;
-    let container_statuses = pod.status.unwrap().container_statuses.unwrap();
-    let container_id = container_statuses
-        .first()
-        .unwrap()
-        .container_id
-        .as_ref()
-        .unwrap()
-        .split("//")
-        .last()
-        .unwrap();
-    let agent_pod_name = format!(
-        "mirrord-agent-{}",
-        Alphanumeric
-            .sample_string(&mut rand::thread_rng(), 10)
-            .to_lowercase()
-    );
-
-    let agent_pod: Pod = serde_json::from_value(json!({
-        "metadata": {
-            "name": agent_pod_name
-        },
-        "spec": {
-            "hostPID": true,
-            "nodeName": node_name,
-            "restartPolicy": "Never",
-            "volumes": [
-                {
-                    "name": "containerd",
-                    "hostPath": {
-                        "path": "/run/containerd/containerd.sock"
-                    }
-                }
-            ],
-            "containers": [
-                {
-                    "name": "mirrord-agent",
-                    "image": "ghcr.io/metalbear-co/mirrord-agent:2.0.0-alpha-3",
-                    "imagePullPolicy": "Always",
-                    "securityContext": {
-                        "privileged": true
-                    },
-                    "volumeMounts": [
-                        {
-                            "mountPath": "/run/containerd/containerd.sock",
-                            "name": "containerd"
-                        }
-                    ],
-                    "command": [
-                        "./mirrord-agent",
-                        "--container-id",
-                        container_id,
-                        "-t",
-                        "60"
-                    ],
-                    "env": [{"name": "RUST_LOG", "value": "trace"}],
-                }
-            ]
-        }
-    }))
-    .unwrap();
-    pods.create(&PostParams::default(), &agent_pod)
-        .await
-        .unwrap();
-
-    //   Wait until the pod is running, otherwise we get 500 error.
-    let running = await_condition(pods.clone(), &agent_pod_name, is_pod_running());
-    let _ = tokio::time::timeout(std::time::Duration::from_secs(15), running)
-        .await
-        .unwrap();
-    let pf = pods.portforward(&agent_pod_name, &[61337]).await.unwrap();
-
-    pf
-}
-
-async fn poll_agent(mut pf: Portforwarder, mut receiver: Receiver<u16>) {
+async fn poll_agent(mut pf: Portforwarder, mut receiver: Receiver<i32>) {
     let port = pf.take_stream(61337).unwrap(); // TODO: Make port configurable
     let mut codec = actix_codec::Framed::new(port, ClientCodec::new());
-    thread::sleep(Duration::from_millis(5000));
     loop {
         select! {
             message = receiver.recv() => {
                 match message {
-                    Some(message) => {
-                        debug!("send message to client {:?}", &message);
-                        codec.send(ClientMessage::PortSubscribe(vec![message])).await.unwrap();
+                    Some(sockfd) => {
+                        let port = SOCKETS.get_connection_socket_address(sockfd).unwrap().port();
+                        debug!("send message to client {:?}", port);
+                        codec.send(ClientMessage::PortSubscribe(vec![port])).await.unwrap();
                     }
                     None => {
                         debug!("NONE in recv");
@@ -323,36 +147,14 @@ async fn poll_agent(mut pf: Portforwarder, mut receiver: Receiver<u16>) {
             message = codec.next() => {
                 match message {
                     Some(Ok(DaemonMessage::NewTCPConnection(conn))) => {
-                        let mut sockets = CONNECTION_SOCKETS.lock().unwrap();
-                        if let Some(mut socket) = sockets.remove_alt(&conn.port) {
-                                debug!("new connection id: {:?}", &conn.connection_id);
-                                write!(socket.write_socket, "a").unwrap();
-                                CONNECTION_QUEUE.lock().unwrap().add(conn.connection_id).unwrap();
-                                sockets.insert(socket.read_fd, socket.port, socket);
-                            }
-                            else
-                            {
-                                error!("No socket found for port: {}", conn.port);
-                                break
-                            }
+                        SOCKETS.open_connection(conn.connection_id, conn.port);
                     }
                     Some(Ok(DaemonMessage::TCPData(d))) => {
                         // Write to socket - need to find it in OPEN_CONNECTION_SOCKETS by conn_id
-                        let mut sockets = DATA_SOCKETS_BY_CONNECTION_ID.lock().unwrap();
-                        if let Some(mut socket) = sockets.remove(&d.connection_id) {
-                                socket.write_socket.write_all(&d.data).unwrap();
-                                sockets.insert(socket.connection_id, socket);
-                            }
-                            else
-                            {
-                                // Not necessarily an error - sometime the TCPData message is handled before NewTcpConnection
-                                debug!("No socket found for connection_id: {}", d.connection_id);
-                                PENDING_DATA.lock().unwrap().insert(d.connection_id, d.data);
-                                continue
-                            }
+                        SOCKETS.write_data(d.connection_id, d.data);
                     }
                     Some(Ok(DaemonMessage::TCPClose(d))) => {
-                        DATA_SOCKETS_BY_CONNECTION_ID.lock().unwrap().remove(&d.connection_id).unwrap();
+                        SOCKETS.close_connection(d.connection_id)
                     }
                     Some(_) => {
                         debug!("NONE in some");
@@ -367,20 +169,6 @@ async fn poll_agent(mut pf: Portforwarder, mut receiver: Receiver<u16>) {
             }
         }
     }
-}
-
-#[ctor]
-fn init() {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .init();
-    debug!("init called");
-
-    let pf = RUNTIME.block_on(create_agent());
-    let (sender, receiver) = channel::<u16>(1000);
-    *SENDER.lock().unwrap() = Some(sender);
-    enable_hooks();
-    RUNTIME.spawn(poll_agent(pf, receiver));
 }
 
 macro_rules! hook {
@@ -421,7 +209,6 @@ fn enable_hooks() {
     let mut interceptor = Interceptor::obtain(&GUM);
     hook!(interceptor, "socket", socket_detour);
     hook!(interceptor, "bind", bind_detour);
-    hook!(interceptor, "connect", connect_detour);
     hook!(interceptor, "listen", listen_detour);
     hook!(interceptor, "getpeername", getpeername_detour);
     hook!(interceptor, "setsockopt", setsockopt_detour);
---
+++ mirrord-layer/src/sockets.rs
@@ -0,0 +1,240 @@
+use std::{
+    collections::HashMap,
+    io::{Read, Write},
+    net::SocketAddr,
+    os::unix::io::AsRawFd,
+    sync::Mutex,
+};
+
+use multi_map::MultiMap;
+use queues::{IsQueue, Queue};
+use socketpair::{socketpair_stream, SocketpairStream};
+use tracing::{debug, error};
+
+pub struct Socket {
+    pub read_fd: SockFd,
+    pub read_socket: SocketpairStream,
+    pub write_socket: SocketpairStream,
+}
+
+pub struct ConnectionSocket {
+    pub read_fd: SockFd,
+    pub read_socket: SocketpairStream,
+    pub write_socket: SocketpairStream,
+    pub address: SocketAddr,
+    pub state: ConnectionState,
+}
+
+#[derive(PartialEq)]
+pub enum ConnectionState {
+    Bound,
+    Listening,
+}
+
+pub struct DataSocket {
+    pub connection_id: ConnectionId,
+    #[allow(dead_code)]
+    pub read_socket: SocketpairStream, /* Though this is unread, it's necessary to keep the
+                                        * socket open */
+    pub write_socket: SocketpairStream,
+    pub address: SocketAddr,
+}
+
+type SockFd = i32;
+type Port = u16;
+type ConnectionId = u16;
+type TCPBuffer = Vec<u8>;
+
+pub struct Sockets {
+    new_sockets: Mutex<HashMap<SockFd, Socket>>,
+    connections: Mutex<MultiMap<SockFd, Port, ConnectionSocket>>,
+    data: Mutex<MultiMap<SockFd, ConnectionId, DataSocket>>,
+    connection_queues: Mutex<HashMap<SockFd, Queue<ConnectionId>>>, /* Used to enqueue incoming
+                                                                     * connection
+                                                                     * ids from the
+                                                                     * agent, to be read in the
+                                                                     * 'accept'
+                                                                     * call */
+    pending_data: Mutex<HashMap<ConnectionId, TCPBuffer>>, /* Used to store data that arrived
+                                                            * before its
+                                                            * connection was opened. When the
+                                                            * connection is
+                                                            * later opened, pending_data is read
+                                                            * and
+                                                            * emptied. */
+}
+
+impl Default for Sockets {
+    fn default() -> Self {
+        Self {
+            new_sockets: Mutex::new(HashMap::new()),
+            connections: Mutex::new(MultiMap::new()),
+            data: Mutex::new(MultiMap::new()),
+            connection_queues: Mutex::new(HashMap::new()),
+            pending_data: Mutex::new(HashMap::new()),
+        }
+    }
+}
+
+impl Sockets {
+    pub fn create_socket(&self) -> SockFd {
+        let (write_socket, read_socket) = socketpair_stream().unwrap();
+        let read_fd = read_socket.as_raw_fd();
+        let socket = Socket {
+            read_fd,
+            read_socket,
+            write_socket,
+        };
+
+        self.new_sockets.lock().unwrap().insert(read_fd, socket);
+
+        read_fd
+    }
+
+    pub fn convert_to_connection_socket(&self, sockfd: SockFd, address: SocketAddr) {
+        let mut sockets = self.new_sockets.lock().unwrap();
+
+        // let mut sockets = self.connections.lock().unwrap();
+        if let Some(socket) = sockets.remove(&sockfd) {
+            // socket.port = port;
+            self.connections.lock().unwrap().insert(
+                sockfd,
+                address.port(),
+                ConnectionSocket {
+                    read_fd: socket.read_fd,
+                    read_socket: socket.read_socket,
+                    write_socket: socket.write_socket,
+                    address,
+                    state: ConnectionState::Bound,
+                },
+            );
+        } else {
+            error!("No socket found for fd: {}", sockfd);
+        }
+    }
+
+    pub fn set_connection_state(&self, sockfd: SockFd, state: ConnectionState) -> Result<(), ()> {
+        let mut connections = self.connections.lock().unwrap();
+        if let Some(connection) = connections.get_mut(&sockfd) {
+            if connection.state != state {
+                connection.state = state;
+                Ok(())
+            } else {
+                error!("No connection found for fd: {}", sockfd);
+                Err(())
+            }
+        } else {
+            error!("No connection found for fd: {}", sockfd);
+            Err(())
+        }
+    }
+
+    pub fn get_connection_socket_address(&self, sockfd: SockFd) -> Option<SocketAddr> {
+        let sockets = self.connections.lock().unwrap();
+        sockets.get(&sockfd).map(|socket| socket.address)
+    }
+
+    pub fn get_data_socket_address(&self, sockfd: SockFd) -> Option<SocketAddr> {
+        let sockets = self.data.lock().unwrap();
+        sockets.get(&sockfd).map(|socket| socket.address)
+    }
+
+    pub fn read_single_connection(&self, sockfd: SockFd) -> ConnectionId {
+        let mut sockets = self.connections.lock().unwrap();
+
+        if let Some(mut socket) = sockets.remove(&sockfd) {
+            let mut buffer = [0; 1];
+            socket.read_socket.read_exact(&mut buffer).unwrap();
+            sockets.insert(sockfd, socket.address.port(), socket);
+            let mut queues = self.connection_queues.lock().unwrap();
+            let queue = queues.get_mut(&sockfd).unwrap();
+            queue.remove().unwrap()
+        } else {
+            error!("No socket found for fd: {}", sockfd);
+            0
+        }
+    }
+
+    pub fn open_connection(&self, connection_id: ConnectionId, port: Port) {
+        let mut connections = self.connections.lock().unwrap();
+        if let Some(mut socket) = connections.remove_alt(&port) {
+            debug!("new connection id: {:?}", connection_id);
+            let mut queues = self.connection_queues.lock().unwrap();
+            match queues.get_mut(&socket.read_fd) {
+                Some(queue) => {
+                    queue.add(connection_id).unwrap();
+                }
+                None => {
+                    let mut queue = Queue::new();
+                    queue.add(connection_id).unwrap();
+                    queues.insert(socket.read_fd, queue);
+                }
+            }
+
+            write!(socket.write_socket, "a").unwrap(); // Need to write one byte per incoming connection, hence "a"
+            connections.insert(socket.read_fd, socket.address.port(), socket);
+        } else {
+            error!("No socket found for port: {}", port);
+        }
+    }
+
+    pub fn create_data_socket(&self, connection_id: ConnectionId, address: SocketAddr) -> SockFd {
+        let (read_socket, mut write_socket) = socketpair_stream().unwrap();
+        let read_fd = read_socket.as_raw_fd();
+        debug!(
+            "Accepted connection from read_fd:{:?}, write_sock:{:?}",
+            read_fd, write_socket
+        );
+
+        if let Some(data) = self.pending_data.lock().unwrap().remove(&connection_id) {
+            debug!("writing pending data for connection_id: {}", connection_id);
+            write_socket.write_all(&data).unwrap();
+        }
+        let read_fd = read_socket.as_raw_fd();
+        let data_socket = DataSocket {
+            connection_id,
+            read_socket,
+            write_socket,
+            address,
+        };
+        self.data
+            .lock()
+            .unwrap()
+            .insert(read_fd, connection_id, data_socket);
+        read_fd
+    }
+
+    pub fn write_data(&self, connection_id: ConnectionId, data: TCPBuffer) {
+        let mut sockets = self.data.lock().unwrap();
+        if let Some(mut socket) = sockets.remove_alt(&connection_id) {
+            socket.write_socket.write_all(&data).unwrap();
+            clear_data(&mut socket.write_socket); // clear HTTP response data that the app wrote to this socket
+            sockets.insert(socket.read_socket.as_raw_fd(), socket.connection_id, socket);
+        } else {
+            // Not necessarily an error - sometime the TCPData message is handled before
+            // NewTcpConnection
+            debug!("No socket found for connection_id: {}", connection_id);
+            self.pending_data
+                .lock()
+                .unwrap()
+                .insert(connection_id, data);
+        }
+    }
+
+    pub fn close_connection(&self, connection_id: ConnectionId) {
+        self.data
+            .lock()
+            .unwrap()
+            .remove_alt(&connection_id)
+            .unwrap();
+    }
+}
+
+fn clear_data(socket: &mut SocketpairStream) {
+    let num_ready_bytes = socket.num_ready_bytes().unwrap();
+    let mut buffer = Vec::with_capacity(num_ready_bytes as usize);
+    socket
+        .take(num_ready_bytes)
+        .read_to_end(&mut buffer)
+        .unwrap();
+}
