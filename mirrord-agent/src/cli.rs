866b5e32a53c4fc6e7854bddf8c3d39dfabf6b46
meowjesty <meowjesty_nyan@outlook.com>
2022-06-14 01:56:14 -0300

    Fix clap deprecations.

--- mirrord-agent/src/cli.rs
+++ mirrord-agent/src/cli.rs
@@ -4,23 +4,23 @@ use clap::Parser;
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
     /// Container id to get traffic from
-    #[clap(short, long)]
+    #[clap(short, long, value_parser)]
     pub container_id: Option<String>,
 
     /// Container runtime to use
-    #[clap(short = 'r', long)]
+    #[clap(short = 'r', long, value_parser)]
     pub container_runtime: Option<String>,
 
     /// Port to use for communication
-    #[clap(short = 'l', long, default_value_t = 61337)]
+    #[clap(short = 'l', long, default_value_t = 61337, value_parser)]
     pub communicate_port: u16,
 
     /// Communication timeout in seconds
-    #[clap(short = 't', long, default_value_t = 30)]
+    #[clap(short = 't', long, default_value_t = 30, value_parser)]
     pub communication_timeout: u16,
 
     /// Interface to use
-    #[clap(short = 'i', long, default_value = "eth0")]
+    #[clap(short = 'i', long, default_value = "eth0", value_parser)]
     pub interface: String,
 }
 
