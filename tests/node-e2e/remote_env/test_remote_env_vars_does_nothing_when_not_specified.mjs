399f8a2cdf050a5ed8dbe62751aeff769cf99248
meowjesty <meowjesty_nyan@outlook.com>
2022-06-20 16:47:13 -0300

    Less clutter in console for remote env tests.

--- tests/node-e2e/remote_env/test_remote_env_vars_does_nothing_when_not_specified.mjs
+++ tests/node-e2e/remote_env/test_remote_env_vars_does_nothing_when_not_specified.mjs
@@ -1,8 +1,4 @@
-console.log(">> starting remote_env test app");
-
-console.log(">> node env vars");
-
-console.log(process.env);
+console.log(">> test_remote_env_vars_does_nothing_when_not_specified");
 
 if (process.env.MIRRORD_FAKE_VAR_FIRST || process.env.MIRRORD_FAKE_VAR_SECOND) {
   process.exit(-1);
--- tests/node-e2e/remote_env/test_remote_env_vars_panics_when_both_filters_are_specified.mjs
+++ tests/node-e2e/remote_env/test_remote_env_vars_panics_when_both_filters_are_specified.mjs
@@ -1,5 +1,1 @@
-console.log(">> starting remote_env test app");
-
-console.log(">> node env vars");
-
-console.log(process.env);
+console.log(">> test_remote_env_vars_panics_when_both_filters_are_specified");
