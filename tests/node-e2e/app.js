14f9f21f25512cbf9ac478e0d0e299b3827d5f90
Mehul Arora <aroram18@mcmaster.ca>
2022-07-07 18:17:00 +0000

    ...

--- tests/node-e2e/app.js
+++ tests/node-e2e/app.js
@@ -8,7 +8,6 @@ const PORT = 80;
 const TEXT =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 var path = process.cwd() + crypto.randomBytes(16).toString("hex");
-var deletePath = process.cwd() + crypto.randomBytes(16).toString("hex");
 
 app.get("/", (req, res) => {
   console.log("GET: Request completed");
@@ -29,19 +28,14 @@ app.put("/", (req, res) => {
       if (err) {
         throw err;
       }
-    });
-    fs.writeFile(deletePath, data.toString(), (err) => {
-      if (err) {
-        throw err;
-      }
-    });
+    });    
   });
   console.log("PUT: Request completed");
 });
 
 app.delete("/", (req, res) => {
   req.on("data", (data) => {
-    fs.unlink(deletePath, (err) => {
+    fs.unlink(path, (err) => {
       if (err) {
         console.err("app.js failed with ", err);
         throw err;
--- tests/python-e2e/app.py
+++ tests/python-e2e/app.py
@@ -15,8 +15,7 @@ cli.show_server_banner = lambda *x: print("Server listening on port 80")
 app = Flask(__name__)
 
 TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-DELETE_PATH = getcwd() + str(uuid.uuid4())
-CREATE_PATH = getcwd() + str(uuid.uuid4())
+PATH = getcwd() + str(uuid.uuid4())
 
 
 @app.route("/", methods=["GET"])
@@ -34,21 +33,20 @@ def post():
 
 @app.route("/", methods=["PUT"])
 def put():
-    with open(CREATE_PATH, "w") as f:
-        f.write(TEXT)
-    with open(DELETE_PATH, "w") as f:
-        f.write(TEXT)
+    with open(PATH, "w") as f:
+        f.write(TEXT)    
     print("PUT: Request completed")
     return "OK"
 
 
 @app.route("/", methods=["DELETE"])
 def delete():
-    remove(DELETE_PATH)
+    remove(PATH)
     print("DELETE: Request completed")
     # killing Flask is the hardest thing I've done in my life - A.H
     kill(getpid(), SIGTERM)
     return "OK"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
