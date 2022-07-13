474aeb7cb2bfff8a1c7fde38469967028f7da700
Mehul Arora <aroram18@mcmaster.ca>
2022-07-13 21:11:30 +0000

    fmt

--- mirrord-layer/src/config.rs
+++ mirrord-layer/src/config.rs
@@ -42,7 +42,7 @@ pub struct LayerConfig {
 
     #[envconfig(from = "MIRRORD_EPHEMERAL_CONTAINER", default = "false")]
     pub ephemeral_container: bool,
-    
+
     /// Enables resolving a remote DNS.
     #[envconfig(from = "MIRRORD_REMOTE_DNS", default = "false")]
     pub remote_dns: bool,
