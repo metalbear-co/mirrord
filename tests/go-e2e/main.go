ecefda856ba32091655e60d24a98daf9a20e4879
Mehul Arora <aroram18@mcmaster.ca>
2022-07-13 19:51:19 +0000

    go

---
+++ tests/go-e2e/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
