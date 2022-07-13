82859b58f911449cc0f34221598f3ac17c1dfcd0
Aviram Hassan <aviramyhassan@gmail.com>
2022-07-13 16:15:33 +0300

    wip

--- mirrord-layer/src/common.rs
+++ mirrord-layer/src/common.rs
@@ -1,84 +1,29 @@
-use std::{io::SeekFrom, path::PathBuf};
-
-use mirrord_protocol::{
-    CloseFileResponse, OpenFileResponse, OpenOptionsInternal, ReadFileResponse, ResponseError,
-    SeekFileResponse, WriteFileResponse,
-};
+use mirrord_protocol::{AddrInfoHint, GetAddrInfoResponse};
 use tokio::sync::oneshot;
 
-use crate::tcp::HookMessageTcp;
+use crate::{error::LayerError, file::HookMessageFile, tcp::HookMessageTcp, HOOK_SENDER};
 
-// TODO: Some ideas around abstracting file operations:
-// Alright, all these are pretty much the same thing, they could be
-// abstract over a generic dependent-ish type like so:
-/*
-struct FakeFile<Operation: FileOperation> {
-    pub(crate) file_channel_tx: oneshot::Sender<Operation::Response>,
-}
-
-struct OpenHook {
-    pub(crate) path: PathBuf,
-}
-
-impl FileOperation for OpenHook {
-    type Response = OpenFileResponse;
-}
- */
-// But maybe `FakeFile` could be even simpler? These are just some initial thoughts.
-//
-// An issue I can see right now is that we would be tying file operations to the fake file holder,
-// which isn't nice (it would be just the same handling as it is right now, but with improved
-// guarantees).
-#[derive(Debug)]
-pub struct OpenFileHook {
-    pub(crate) path: PathBuf,
-    pub(crate) file_channel_tx: oneshot::Sender<Result<OpenFileResponse, ResponseError>>,
-    pub(crate) open_options: OpenOptionsInternal,
+pub(crate) fn blocking_send_hook_message(message: HookMessage) -> Result<(), LayerError> {
+    unsafe {
+        HOOK_SENDER
+            .as_ref()
+            .ok_or(LayerError::EmptyHookSender)
+            .and_then(|hook_sender| hook_sender.blocking_send(message).map_err(Into::into))
+    }
 }
 
 #[derive(Debug)]
-pub struct OpenRelativeFileHook {
-    pub(crate) relative_fd: usize,
-    pub(crate) path: PathBuf,
-    pub(crate) file_channel_tx: oneshot::Sender<Result<OpenFileResponse, ResponseError>>,
-    pub(crate) open_options: OpenOptionsInternal,
-}
-
-#[derive(Debug)]
-pub struct ReadFileHook {
-    pub(crate) fd: usize,
-    pub(crate) buffer_size: usize,
-    pub(crate) file_channel_tx: oneshot::Sender<Result<ReadFileResponse, ResponseError>>,
-}
-
-#[derive(Debug)]
-pub struct SeekFileHook {
-    pub(crate) fd: usize,
-    pub(crate) seek_from: SeekFrom,
-    pub(crate) file_channel_tx: oneshot::Sender<Result<SeekFileResponse, ResponseError>>,
-}
-
-#[derive(Debug)]
-pub struct WriteFileHook {
-    pub(crate) fd: usize,
-    pub(crate) write_bytes: Vec<u8>,
-    pub(crate) file_channel_tx: oneshot::Sender<Result<WriteFileResponse, ResponseError>>,
-}
-
-#[derive(Debug)]
-pub struct CloseFileHook {
-    pub(crate) fd: usize,
-    pub(crate) file_channel_tx: oneshot::Sender<Result<CloseFileResponse, ResponseError>>,
+pub struct GetAddrInfoHook {
+    pub(crate) node: Option<String>,
+    pub(crate) service: Option<String>,
+    pub(crate) hints: Option<AddrInfoHint>,
+    pub(crate) hook_channel_tx: oneshot::Sender<GetAddrInfoResponse>,
 }
 
 /// These messages are handled internally by -layer, and become `ClientMessage`s sent to -agent.
 #[derive(Debug)]
 pub enum HookMessage {
     Tcp(HookMessageTcp),
-    OpenFileHook(OpenFileHook),
-    OpenRelativeFileHook(OpenRelativeFileHook),
-    ReadFileHook(ReadFileHook),
-    SeekFileHook(SeekFileHook),
-    WriteFileHook(WriteFileHook),
-    CloseFileHook(CloseFileHook),
+    File(HookMessageFile),
+    GetAddrInfoHook(GetAddrInfoHook),
 }
--- mirrord-layer/src/file.rs
+++ mirrord-layer/src/file.rs
@@ -1,14 +1,24 @@
 use std::{
-    collections::HashMap,
+    collections::{HashMap, VecDeque},
     env,
+    io::SeekFrom,
     os::unix::io::RawFd,
+    path::PathBuf,
     sync::{LazyLock, Mutex},
 };
 
+use futures::SinkExt;
 use libc::{c_int, O_ACCMODE, O_APPEND, O_CREAT, O_RDONLY, O_RDWR, O_TRUNC, O_WRONLY};
-use mirrord_protocol::OpenOptionsInternal;
+use mirrord_protocol::{
+    ClientCodec, ClientMessage, FileRequest, OpenFileRequest, OpenFileResponse,
+    OpenOptionsInternal, OpenRelativeFileRequest, ReadFileRequest, ReadFileResponse, ResponseError,
+    SeekFileRequest, SeekFileResponse, WriteFileResponse,
+};
 use regex::RegexSet;
-use tracing::warn;
+use tokio::sync::oneshot;
+use tracing::{debug, warn};
+
+use crate::error::LayerError;
 
 pub(crate) mod hooks;
 pub(crate) mod ops;
@@ -105,4 +115,271 @@ impl OpenOptionsInternalExt for OpenOptionsInternal {
                 open_options
             })
     }
+}
+
+type ResponseDeque<T> = VecDeque<ResponseChannel<T>>;
+#[derive(Default)]
+struct FileHandler {
+    /// idea: Replace all VecDeque with HashMap, the assumption order will remain is dangerous :O
+    open_queue: ResponseDeque<OpenFileResponse>,
+    read_queue: ResponseDeque<ReadFileResponse>,
+    seek_queue: ResponseDeque<SeekFileResponse>,
+}
+
+impl FileHandler {
+    pub fn handle_hook_message(
+        &mut self,
+        message: HookMessageFile,
+        codec: &mut actix_codec::Framed<
+            impl tokio::io::AsyncRead + tokio::io::AsyncWrite + Unpin + Send,
+            ClientCodec,
+        >,
+    ) -> Result<(), LayerError> {
+        use HookMessageFile::*;
+        match message {
+            Open(open) => self.handle_hook_open(open, codec).await,
+            OpenRelative(open_relative) => {
+                self.handle_hook_open_relative(open_relative, codec).await
+            }
+
+            Read(read) => self.handle_hook_read(read, codec).await,
+            Seek(seek) => self.handle_hook_seek(seek, codec).await,
+            Write(write) => self.handle_hook_write(write, codec).await,
+            HookMessage::CloseFileHook(CloseFileHook {
+                fd,
+                file_channel_tx,
+            }) => {
+                debug!("HookMessage::CloseFileHook fd {:#?}", fd);
+
+                close_file_handler.lock().unwrap().push(file_channel_tx);
+
+                let close_file_request = CloseFileRequest { fd };
+
+                let request = ClientMessage::FileRequest(FileRequest::Close(close_file_request));
+                let codec_result = codec.send(request).await;
+
+                debug!(
+                    "HookMessage::CloseFileHook codec_result {:#?}",
+                    codec_result
+                );
+            }
+        }
+        Ok(())
+    }
+
+    async fn handle_hook_open(
+        &mut self,
+        open: Open,
+        codec: &mut actix_codec::Framed<
+            impl tokio::io::AsyncRead + tokio::io::AsyncWrite + Unpin + Send,
+            ClientCodec,
+        >,
+    ) -> Result<(), LayerError> {
+        let Open {
+            file_channel_tx,
+            path,
+            open_options,
+        } = open;
+        debug!(
+            "HookMessage::OpenFileHook path {:#?} | options {:#?}",
+            path, open_options
+        );
+
+        self.open_queue.push_back(file_channel_tx);
+
+        let open_file_request = OpenFileRequest { path, open_options };
+
+        let request = ClientMessage::FileRequest(FileRequest::Open(open_file_request));
+        let codec_result = codec.send(request).await;
+
+        debug!("HookMessage::OpenFileHook codec_result {:#?}", codec_result);
+        Ok(())
+    }
+    async fn handle_hook_open_relative(
+        &mut self,
+        open_relative: OpenRelative,
+        codec: &mut actix_codec::Framed<
+            impl tokio::io::AsyncRead + tokio::io::AsyncWrite + Unpin + Send,
+            ClientCodec,
+        >,
+    ) -> Result<(), LayerError> {
+        let OpenRelative {
+            relative_fd,
+            path,
+            file_channel_tx,
+            open_options,
+        } = open_relative;
+        debug!(
+            "HookMessage::OpenRelativeFileHook fd {:#?} | path {:#?} | options {:#?}",
+            relative_fd, path, open_options
+        );
+
+        self.open_queue.push_back(file_channel_tx);
+
+        let open_relative_file_request = OpenRelativeFileRequest {
+            relative_fd,
+            path,
+            open_options,
+        };
+
+        let request =
+            ClientMessage::FileRequest(FileRequest::OpenRelative(open_relative_file_request));
+        let codec_result = codec.send(request).await;
+
+        debug!("HookMessage::OpenFileHook codec_result {:#?}", codec_result);
+        Ok(())
+    }
+
+    async fn handle_hook_read(
+        &mut self,
+        read: Read,
+        codec: &mut actix_codec::Framed<
+            impl tokio::io::AsyncRead + tokio::io::AsyncWrite + Unpin + Send,
+            ClientCodec,
+        >,
+    ) -> Result<(), LayerError> {
+        let Read {
+            fd,
+            buffer_size,
+            file_channel_tx,
+        } = read;
+        debug!(
+            "HookMessage::ReadFileHook fd {:#?} | buffer_size {:#?}",
+            fd, buffer_size
+        );
+
+        self.read_queue.push_back(file_channel_tx);
+
+        let read_file_request = ReadFileRequest { fd, buffer_size };
+
+        debug!(
+            "HookMessage::ReadFileHook read_file_request {:#?}",
+            read_file_request
+        );
+
+        let request = ClientMessage::FileRequest(FileRequest::Read(read_file_request));
+        let codec_result = codec.send(request).await;
+
+        debug!("HookMessage::ReadFileHook codec_result {:#?}", codec_result);
+        Ok(())
+    }
+
+    async fn handle_hook_seek(
+        &mut self,
+        seek: Seek,
+        codec: &mut actix_codec::Framed<
+            impl tokio::io::AsyncRead + tokio::io::AsyncWrite + Unpin + Send,
+            ClientCodec,
+        >,
+    ) -> Result<(), LayerError> {
+        let Seek {
+            fd,
+            seek_from,
+            file_channel_tx,
+        } = seek;
+        debug!(
+            "HookMessage::SeekFileHook fd {:#?} | seek_from {:#?}",
+            fd, seek_from
+        );
+
+        self.seek_queue.push_back(file_channel_tx);
+
+        let seek_file_request = SeekFileRequest {
+            fd,
+            seek_from: seek_from.into(),
+        };
+
+        let request = ClientMessage::FileRequest(FileRequest::Seek(seek_file_request));
+        let codec_result = codec.send(request).await;
+
+        debug!("HookMessage::SeekFileHook codec_result {:#?}", codec_result);
+        Ok(())
+    }
+
+    async fn handle_hook_write(
+        &mut self,
+        write: Write,
+        codec: &mut actix_codec::Framed<
+            impl tokio::io::AsyncRead + tokio::io::AsyncWrite + Unpin + Send,
+            ClientCodec,
+        >,
+    ) -> Result<(), LayerError> {
+        let Write {
+            fd,
+            write_bytes,
+            file_channel_tx,
+        } = write;
+        debug!(
+            "HookMessage::WriteFileHook fd {:#?} | length {:#?}",
+            fd,
+            write_bytes.len()
+        );
+
+        write_file_handler.lock().unwrap().push(file_channel_tx);
+
+        let write_file_request = WriteFileRequest { fd, write_bytes };
+
+        let request = ClientMessage::FileRequest(FileRequest::Write(write_file_request));
+        let codec_result = codec.send(request).await;
+
+        debug!(
+            "HookMessage::WriteFileHook codec_result {:#?}",
+            codec_result
+        );
+        Ok(())
+    }
+}
+
+type ResponseChannel<T> = oneshot::Sender<Result<T, ResponseError>>;
+
+#[derive(Debug)]
+pub struct Open {
+    pub(crate) path: PathBuf,
+    pub(crate) file_channel_tx: ResponseChannel<OpenFileResponse>,
+    pub(crate) open_options: OpenOptionsInternal,
+}
+
+#[derive(Debug)]
+pub struct OpenRelative {
+    pub(crate) relative_fd: usize,
+    pub(crate) path: PathBuf,
+    pub(crate) file_channel_tx: ResponseChannel<OpenFileResponse>,
+    pub(crate) open_options: OpenOptionsInternal,
+}
+
+#[derive(Debug)]
+pub struct Read {
+    pub(crate) fd: usize,
+    pub(crate) buffer_size: usize,
+    pub(crate) file_channel_tx: ResponseChannel<ReadFileResponse>,
+}
+
+#[derive(Debug)]
+pub struct Seek {
+    pub(crate) fd: usize,
+    pub(crate) seek_from: SeekFrom,
+    pub(crate) file_channel_tx: ResponseChannel<SeekFileResponse>,
+}
+
+#[derive(Debug)]
+pub struct Write {
+    pub(crate) fd: usize,
+    pub(crate) write_bytes: Vec<u8>,
+    pub(crate) file_channel_tx: ResponseChannel<WriteFileResponse>,
+}
+
+#[derive(Debug)]
+pub struct Close {
+    pub(crate) fd: usize,
+    pub(crate) file_channel_tx: ResponseChannel<WriteFileResponse>,
+}
+
+#[derive(Debug)]
+pub enum HookMessageFile {
+    Open(Open),
+    OpenRelative(OpenRelative),
+    Read(Read),
+    Seek(Seek),
+    Write(Write),
+    Close(Close),
 }
