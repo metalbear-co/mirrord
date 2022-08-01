d68b8e56fa2cf7581ab7cd54d91aac5deef8a940
Mehul Arora <aroram18@mcmaster.ca>
2022-08-01 13:34:19 +0000

    Add a macro

--- mirrord-layer/src/macros.rs
+++ mirrord-layer/src/macros.rs
@@ -32,5 +32,23 @@ macro_rules! try_hook {
     };
 }
 
+macro_rules! hook_symbol {
+    ($interceptor:expr, $func:expr, $detour_name:expr, $binary:expr) => {
+        $interceptor
+            .replace(
+                frida_gum::Module::find_symbol_by_name(Some($binary), $func).unwrap(),
+                frida_gum::NativePointer($detour_name as *mut libc::c_void),
+                frida_gum::NativePointer(std::ptr::null_mut::<libc::c_void>()),
+            )
+            .inspect_err(|err| {
+                debug!("Failed to hook {:?}", err);
+            })
+            .inspect(|_| debug!("{:?} hooked", $func))
+            .unwrap()
+    };
+}
+
+
 pub(crate) use hook;
 pub(crate) use try_hook;
+pub(crate) use hook_symbol;
