2ce84c3b6d0cb21264e99c5a8b18689b2b466985
Aviram Hassan <aviram@metalbear.co>
2022-08-04 17:05:55 +0300

    reduce e2e flakiness (#237)
    * reduce e2e flakiness (add message sent on tcp listen subscription, wait for that message)

--- mirrord-layer/src/tcp.rs
+++ mirrord-layer/src/tcp.rs
@@ -89,6 +89,11 @@ pub trait TcpHandler {
             }
             DaemonTcp::Data(tcp_data) => self.handle_new_data(tcp_data).await,
             DaemonTcp::Close(tcp_close) => self.handle_close(tcp_close),
+            DaemonTcp::Subscribed => {
+                // Added this so tests can know when traffic can be sent
+                debug!("daemon subscribed");
+                Ok(())
+            }
         };
 
         debug!("handle_incoming_message -> handled {:#?}", handled);
