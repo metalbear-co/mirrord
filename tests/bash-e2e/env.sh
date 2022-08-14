25074adcc2c5f8b8919b09bbc3b1a5bd67ef7546
DmitryDodzin <d.dodzin@gmail.com>
2022-08-14 18:37:31 +0300

    add include & exclude tests to env

--- tests/bash-e2e/env.sh
+++ tests/bash-e2e/env.sh
@@ -1,22 +1,40 @@
+case $1 in
+  include) first=1; second=0;;
+  exclude) first=0; second=1;;
+  *) first=1; second=1;;
+esac
+
 if [ $MIRRORD_FAKE_VAR_FIRST ]; then
+  if [ $first == 0 ]; then
+    echo "MIRRORD_FAKE_VAR_FIRST should not be set";
+
+    exit -1
+  fi
+
   if [[ $MIRRORD_FAKE_VAR_FIRST != "mirrord.is.running" ]]; then
     echo "MIRRORD_FAKE_VAR_FIRST wasn't of value mirrord.is.running";
 
     exit -1
   fi
-else
+elif [ $first == 1 ]; then
   echo "MIRRORD_FAKE_VAR_FIRST was not set";
 
   exit -1
 fi
 
 if [ $MIRRORD_FAKE_VAR_SECOND ]; then
+  if [ $second == 0 ]; then
+    echo "MIRRORD_FAKE_VAR_SECOND should not be set";
+
+    exit -1
+  fi
+
   if [[ $MIRRORD_FAKE_VAR_SECOND != "7777" ]]; then
     echo "MIRRORD_FAKE_VAR_SECOND wasn't of value 7777";
 
     exit -1
   fi
-else
+elif [ $second == 1 ]; then
   echo "MIRRORD_FAKE_VAR_SECOND was not set";
 
   exit -1
