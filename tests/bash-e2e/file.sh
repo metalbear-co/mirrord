3e465fbdc21a99824ccbcfed95566611b9b3f307
DmitryDodzin <d.dodzin@gmail.com>
2022-08-15 11:35:56 +0300

    Remove cat print

--- tests/bash-e2e/file.sh
+++ tests/bash-e2e/file.sh
@@ -4,8 +4,6 @@ validation_file="/app/test.txt";
 write_target="/tmp/bash_write_test.txt";
 validation_text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-# echo $"\n\n $(cat "$validation_file") \n\n";
-
 case $1 in
   read)
       if [[ -r "$validation_file" ]]; then
