5e4b0b7569f9ab926f800d553b83537cb9fdb2df
meowjesty <meowjesty_nyan@outlook.com>
2022-08-30 15:33:46 -0300

    Change to assert_eq.

--- tests/src/sanity.rs
+++ tests/src/sanity.rs
@@ -930,7 +930,7 @@ mod tests {
         let mut process = run(node_command, &service.pod_name, None, Some(mirrord_args)).await;
 
         let res = process.child.wait().await.unwrap();
-        // assert!(!res.success());
-        assert!(!process.stderr.lock().unwrap().is_empty());
+        assert_eq!(res.success(), false);
+        assert_eq!(process.stderr.lock().unwrap().is_empty(), false);
     }
 }
