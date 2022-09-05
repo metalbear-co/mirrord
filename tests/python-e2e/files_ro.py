1e91018b3d384df5f4b39dbe6b9700d8d8b33657
Aviram Hassan <aviramyhassan@gmail.com>
2022-09-05 16:11:38 +0300

    ..

--- tests/python-e2e/files_ro.py
+++ tests/python-e2e/files_ro.py
@@ -12,12 +12,13 @@ class FileOpsTest(unittest.TestCase):
         Check that we can't open the file for writing because it opens it locally and it doesn't exist
         then make sure we can still read it remotely.
         """
-        self.assertRaises(FileNotFoundError, open("/app/test.txt", "wb"))
+        with self.assertRaises(FileNotFoundError):
+            open("/app/test.txt", "wb")
         with open("/app/test.txt", "r") as f:
             f.seek(0)
             self.assertEqual(f.readline(), TEXT)
-        self.assertRaises(FileNotFoundError, open("/app/test.txt", "wb"))
-
+        with self.assertRaises(FileNotFoundError):
+            open("/app/test.txt", "wb")
 
 
 if __name__ == "__main__":
