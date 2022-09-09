afb888d6a7c1e69c054055d4725694bca14f6053
infiniteregrets <aroram18@mcmaster.ca>
2022-09-09 18:40:55 -0400

    wip

---
+++ intellij-ext/src/main/kotlin/com/metalbear/mirrord/DonotShowAction.kt
@@ -0,0 +1,11 @@
+package com.metalbear.mirrord
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class DonotShowAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+
+}
