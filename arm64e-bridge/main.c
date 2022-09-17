19afd50f4bc553204414b919aeed60ffff49bf77
infiniteregrets <aroram18@mcmaster.ca>
2022-09-17 19:56:03 -0400

    ..

--- arm64e-bridge/main.c
+++ arm64e-bridge/main.c
@@ -6,16 +6,16 @@
 
 __attribute__((constructor))
 static void arm64e_ctor(void) {
-    printf("hello from arm64e-bridge\n");
+    // printf("hello from arm64e-bridge\n");
     char* mirrord_dylib_path = getenv("MIRRORD_DYLIB_PATH");
     const char* name = getprogname();
-    printf("name: %s\n", name);
+    // printf("name: %s\n", name);
     if (mirrord_dylib_path) {
-        printf("arm64e-bridge: loading %s\n", mirrord_dylib_path);
+        // printf("arm64e-bridge: loading %s\n", mirrord_dylib_path);
         if (strcmp(name, "debugserver") != 0 && strcmp(name, "dlv") != 0) {
-            void* handle = dlopen(mirrord_dylib_path, RTLD_GLOBAL);
+            void* handle = dlopen(mirrord_dylib_path, RTLD_NOW);
             if (handle == NULL) {
-                printf("arm64e-bridge: failed to load");   
+                // printf("arm64e-bridge: failed to load");   
             }     
     } else {
         return;
