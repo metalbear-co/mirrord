d3c112c10a7e5d4ed85b325cb86eadcdd4ef95b4
Aviram Hassan <aviram@metalbear.co>
2022-09-20 11:07:08 +0300

    Change log level for tunnel connection closed scenarios (#443)
    * mirrord-layer: Failing to write to local tunneled streams doesn't logs are now info instead of error.

--- mirrord-layer/src/outgoing/udp.rs
+++ mirrord-layer/src/outgoing/udp.rs
@@ -121,7 +121,7 @@ impl UdpOutgoingHandler {
                             continue;
                         },
                         Err(fail) => {
-                            error!("Failed reading mirror_stream with {:#?}", fail);
+                            info!("Failed reading mirror_stream with {:#?}", fail);
                             close_remote_stream(layer_tx.clone()).await;
 
                             break;
