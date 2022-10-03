74aa27e8399132428437e7c957af46a950654255
meowjesty <meowjesty_nyan@outlook.com>
2022-10-03 16:03:33 -0300

    Fix test_listen_localhost path.

--- tests/src/sanity.rs
+++ tests/src/sanity.rs
@@ -1253,7 +1253,7 @@ mod tests {
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     pub async fn test_listen_localhost(#[future] service: KubeService) {
         let service = service.await;
-        let node_command = vec!["node", "node-e2e/outgoing/test_listen_localhost.mjs"];
+        let node_command = vec!["node", "node-e2e/listen/test_listen_localhost.mjs"];
         let mut process = run(node_command, &service.target, None, None).await;
         let res = process.child.wait().await.unwrap();
         assert!(res.success());
