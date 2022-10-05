b0b557c2e59d46676408dbd98f466651a073cd50
Isaac Cloos <cloosisaac@gmail.com>
2022-10-04 22:47:17 -0400

    fileops feedback

--- tests/dotnet-e2e/fileops/Program.cs
+++ tests/dotnet-e2e/fileops/Program.cs
@@ -1,7 +1,9 @@
 ﻿var TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+TestRead();
+TestWrite();
+TestLseek();
 
-// Read
-
+void TestRead()
 {
     var dat = File.ReadAllText("/app/test.txt");
 
@@ -9,12 +11,9 @@
         throw new Exception($"Expected {TEXT}, got {dat}");
 }
 
-
-// Write
-
+void TestWrite()
 {
     var filePath = createTempFile();
-    checkFileExistsOnHost(filePath);
 
     var dat = File.ReadAllText(filePath);
 
@@ -24,55 +23,28 @@
     File.Delete(filePath);
 }
 
-
-// Lseek
-
+void TestLseek()
 {
     var filePath = createTempFile();
-    checkFileExistsOnHost(filePath);
 
     var fileStream = File.Open(filePath, FileMode.Open);
     var buf = new byte[TEXT.Length];
 
-    // https://learn.microsoft.com/en-us/dotnet/api/system.io.filestream.canseek?view=net-6.0
-    if (!fileStream.CanSeek)
-        throw new Exception("file stream CanSeek failure");
-
     // Arg 2 specifies offset for stream
     fileStream.Read(buf, 0, buf.Length);
 
-    if (System.Text.Encoding.Default.GetString(buf) != TEXT)
-        throw new Exception($"Expected {TEXT}, got {System.Text.Encoding.Default.GetString(buf)}");
+    var decodedBuffer = System.Text.Encoding.Default.GetString(buf);
+
+    if (decodedBuffer != TEXT)
+        throw new Exception($"Expected {TEXT}, got {decodedBuffer}");
 
     fileStream.Close();
     File.Delete(filePath);
 }
-
-
-// Faccessat
-// TODO: syscall limitations x-platform. Will revisit after discussion
-
-{
-    var filePath = createTempFile();
-    checkFileExistsOnHost(filePath);
-
-    var fileStream = File.Open(filePath, FileMode.Append);
-
-    fileStream.Close();
-    File.Delete(filePath);
-}
-
-// helpers
 
 string createTempFile()
 {
     var filePath = $"/tmp/tmpF-{Guid.NewGuid()}.txt";
     File.WriteAllText(filePath, TEXT);
     return filePath;
-}
-
-void checkFileExistsOnHost(string fileName)
-{
-    if (File.Exists(fileName))
-        throw new Exception("file exists on host");
 }
