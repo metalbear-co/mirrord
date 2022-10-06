cdbbf42347b9c18c330ba187740fa62a1400ab28
Mehul <65443164+infiniteregrets@users.noreply.github.com>
2022-10-06 12:33:48 -0400

    Add SIP protection check (#495)

--- mirrord-cli/src/main.rs
+++ mirrord-cli/src/main.rs
@@ -15,6 +15,8 @@ use rand::distributions::{Alphanumeric, DistString};
 use semver::Version;
 use tracing::{debug, error, info, warn};
 use tracing_subscriber::{fmt, prelude::*, registry, EnvFilter};
+#[cfg(target_os = "macos")]
+use {regex::RegexSet, which::which};
 
 mod config;
 
@@ -98,11 +100,42 @@ fn add_to_preload(path: &str) -> Result<()> {
     }
 }
 
+#[cfg(target_os = "macos")]
+fn sip_check(binary_path: &str) -> Result<()> {
+    let sip_set = RegexSet::new(&[
+        r"/System/.*",
+        r"/bin/.*",
+        r"/sbin/.*",
+        r"/usr/.*",
+        r"/var/.*",
+        r"/Applications/.*",
+    ])?;
+    let complete_path = which(binary_path)?;
+
+    let sliced_path = complete_path.to_str().ok_or_else(|| {
+        anyhow!(
+            "Failed to convert path to a string slice: {}",
+            binary_path.to_string()
+        )
+    })?;
+
+    if sip_set.is_match(sliced_path) {
+        println!("[WARNING]: Provided binary: {:?} is located in a SIP directory. mirrord might fail to load into it.
+        >> for more info visit https://support.apple.com/en-us/HT204899", binary_path);
+    }
+
+    Ok(())
+}
+
 fn exec(args: &ExecArgs) -> Result<()> {
     info!(
         "Launching {:?} with arguments {:?}",
         args.binary, args.binary_args
     );
+
+    #[cfg(target_os = "macos")]
+    sip_check(&args.binary)?;
+
     if !(args.no_tcp_outgoing || args.no_udp_outgoing) && args.no_remote_dns {
         warn!("TCP/UDP outgoing enabled without remote DNS might cause issues when local machine has IPv6 enabled but remote cluster doesn't")
     }
@@ -112,7 +145,6 @@ fn exec(args: &ExecArgs) -> Result<()> {
     }
 
     if let Some(pod) = &args.pod_name {
-        // TODO: do we need a print here or just a log is fine?
         println!("[WARNING]: DEPRECATED - `--pod-name` is deprecated, consider using `--target instead.\nDeprecated since: [28/09/2022] | Scheduled removal: [28/10/2022]");
         std::env::set_var("MIRRORD_AGENT_IMPERSONATED_POD_NAME", pod);
     }
