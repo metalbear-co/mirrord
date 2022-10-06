30a1fdca128f871f5918a69502c1699eb7a82a92
Isaac Cloos <cloosisaac@gmail.com>
2022-10-06 12:27:45 -0400

    cleanup dotnet dns test

--- tests/dotnet-e2e/dns/Program.cs
+++ tests/dotnet-e2e/dns/Program.cs
@@ -1,6 +1,4 @@
 using System.Net;
 
-var url = "https://google.com";
-Uri myUri = new Uri(url);
-var ip = (await Dns.GetHostAddressesAsync(myUri.Host))[0];
-Console.WriteLine(ip);
+Uri uri = new(uriString: "https://google.com");
+await Dns.GetHostAddressesAsync(uri.Host);
