2f8258548a84d7a75c26242ef42854ff8373f18b
Aviram Hassan <aviram@metalbear.co>
2022-10-08 23:54:26 +0300

    Docker cross (#518)
    * Replaced `pcap` dependency with our own `rawsocket` to make cross compiling faster and easier.

---
+++ mirrord-agent/platform.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Used in Docker build to set platform dependent variables
+
+case $TARGETARCH in
+
+    "amd64")
+	echo "x86_64-unknown-linux-gnu" > /.platform
+	echo "" > /.compiler 
+	;;
+    "arm64") 
+	echo "aarch64-unknown-linux-gnu" > /.platform
+	echo "gcc-aarch64-linux-gnu" > /.compiler
+	;;
+    "arm")
+	echo "armv7-unknown-linux-gnueabihf" > /.platform
+	echo "gcc-arm-linux-gnueabihf" > /.compiler
+	;;
+esac
+
