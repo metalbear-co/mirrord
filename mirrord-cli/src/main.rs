0bc7f1a19db95dc35c5fa26ed88d455dca2b4270
tamasfe <me@tamasfe.dev>
2022-10-10 17:55:34 +0200

    fix(cli): only set progress env if absent

--- mirrord-cli/src/main.rs
+++ mirrord-cli/src/main.rs
@@ -237,7 +237,9 @@ fn exec(args: &ExecArgs) -> Result<()> {
     if let Some(config_file) = &args.config_file {
         std::env::set_var("MIRRORD_CONFIG_FILE", config_file.clone());
     }
-    std::env::set_var(MIRRORD_PROGRESS_ENV, "standard");
+    if std::env::var(MIRRORD_PROGRESS_ENV).is_err() {
+        std::env::set_var(MIRRORD_PROGRESS_ENV, "standard");
+    }
 
     let library_path = extract_library(args.extract_path.clone())?;
     add_to_preload(library_path.to_str().unwrap()).unwrap();
