9c6ad81aa4b361648d9c3547d7e26f8ded7c6d4f
Aviram Hassan <aviram@metalbear.co>
2022-10-13 13:48:57 +0000

    Reuse agent (#566)
    ### Added
    - Reuse agent - first process that runs will create the agent and its children will be able to reuse the same one to avoid creating many agents.
    
    Co-authored-by: Aviram Hassan <aviramyhassan@gmail.com>

--- mirrord-config/src/lib.rs
+++ mirrord-config/src/lib.rs
@@ -44,6 +44,14 @@ pub struct LayerFileConfig {
     #[config(env = "MIRRORD_CONNECT_TCP")]
     pub connect_tcp: Option<String>,
 
+    /// Agent name that already exists that we can connect to.
+    #[config(env = "MIRRORD_CONNECT_AGENT")]
+    pub connect_agent_name: Option<String>,
+
+    /// Agent listen port that already exists that we can connect to.
+    #[config(env = "MIRRORD_CONNECT_PORT")]
+    pub connect_agent_port: Option<u16>,
+
     #[serde(default)]
     #[config(nested)]
     pub agent: AgentFileConfig,
@@ -232,6 +240,8 @@ mod tests {
 
         let expect = LayerFileConfig {
             accept_invalid_certificates: Some(false),
+            connect_agent_name: None,
+            connect_agent_port: None,
             target: Some("pod/test-service-abcdefg-abcd".to_owned()),
             target_namespace: Some("default".to_owned()),
             skip_processes: None,
