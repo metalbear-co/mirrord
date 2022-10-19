5e4ccf0f9074f02607f7f67ebfe31c8e2d758224
t4lz <t4lz.git@gmail.com>
2022-10-19 18:04:11 +0200

    Version check on enable, no cool-down.

--- intellij-ext/src/main/kotlin/com/metalbear/mirrord/MirrordEnabler.kt
+++ intellij-ext/src/main/kotlin/com/metalbear/mirrord/MirrordEnabler.kt
@@ -8,6 +8,14 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
+import com.github.zafarkhaja.semver.Version
+import com.intellij.ide.plugins.PluginManagerConfigurable
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.notification.NotificationAction
+import com.intellij.openapi.extensions.PluginId
+import java.net.URL
+
 
 @Suppress("DialogTitleCapitalization")
 class MirrordEnabler : ToggleAction() {
@@ -35,10 +43,49 @@ class MirrordEnabler : ToggleAction() {
     override fun setSelected(e: AnActionEvent, state: Boolean) {
         if (state) {
             notify("mirrord enabled", NotificationType.INFORMATION, e.project)
+            e.project?.let { checkVersion(it) }
         } else {
             notify("mirrord disabled", NotificationType.INFORMATION, e.project)
         }
 
         MirrordListener.enabled = state
+    }
+
+    private val pluginId = PluginId.getId("com.metalbear.mirrord")
+    private val version: String? = PluginManagerCore.getPlugin(pluginId)?.version
+    private val versionCheckEndpoint: String =
+        "https://version.mirrord.dev/get-latest-version?source=3&version=$version"
+    private var versionCheckDisabled
+        get() = PropertiesComponent.getInstance().getBoolean("versionCheckDisabled", false)
+        set(value) {
+            PropertiesComponent.getInstance().setValue("versionCheckDisabled", value)
+        }
+
+    private fun checkVersion(project: Project) {
+        val remoteVersion = Version.valueOf(URL(versionCheckEndpoint).readText())
+        val localVersion = Version.valueOf(version)
+        if (localVersion.lessThan(remoteVersion)) {
+            notifier(
+                "Your version of mirrord is outdated, you should update.",
+                NotificationType.INFORMATION
+            )
+                .addAction(object : NotificationAction("Update") {
+                    override fun actionPerformed(e: AnActionEvent, notification: Notification) {
+                        try {
+                            PluginManagerConfigurable.showPluginConfigurable(project, listOf(pluginId))
+                        } catch (e: Exception) {
+                            notification.expire()
+                        }
+                        notification.expire()
+                    }
+                })
+                .addAction(object : NotificationAction("Don't show again") {
+                    override fun actionPerformed(e: AnActionEvent, notification: Notification) {
+                        versionCheckDisabled = true
+                        notification.expire()
+                    }
+                }).notify(project)
+        }
+        return
     }
 }
