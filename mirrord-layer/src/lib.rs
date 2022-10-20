2fe821832a857098d582e1769aadde190ee6957b
Dmitry Dodzin <d.dodzin@gmail.com>
2022-10-20 18:18:25 +0300

    Small

--- mirrord-layer/src/lib.rs
+++ mirrord-layer/src/lib.rs
@@ -177,7 +177,7 @@ fn init(config: LayerConfig) {
     let mut guards = Vec::new();
 
     let file_log = {
-        let file_appender = tracing_appender::rolling::hourly("/tmp/mirrord", "mirrord-layer.log");
+        let file_appender = tracing_appender::rolling::never("/tmp/mirrord", "mirrord-layer.log");
         let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
 
         guards.push(guard);
@@ -185,10 +185,12 @@ fn init(config: LayerConfig) {
         tracing_subscriber::fmt::layer()
             .with_thread_ids(true)
             .with_span_events(FmtSpan::ACTIVE)
+            .json()
             .with_writer(non_blocking)
     };
 
     tracing_subscriber::registry()
+        .with(file_log)
         .with(
             tracing_subscriber::fmt::layer()
                 .with_thread_ids(true)
@@ -196,7 +198,6 @@ fn init(config: LayerConfig) {
                 .compact(),
         )
         .with(tracing_subscriber::EnvFilter::from_default_env())
-        .with(file_log)
         .init();
 
     info!("Initializing mirrord-layer!");
