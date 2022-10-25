21a203ded09e5495b793b4ea86e02cab3e854b00
infiniteregrets <aroram18@mcmaster.ca>
2022-10-25 13:46:12 -0400

    Update build file

--- intellij-ext/build.gradle.kts
+++ intellij-ext/build.gradle.kts
@@ -124,7 +124,6 @@ tasks {
     }
 
     publishPlugin {
-        dependsOn("patchChangelog")
         token.set(System.getenv("PUBLISH_TOKEN"))
         // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
