9d233efabe7c75685081b481adb868bf481c0799
infiniteregrets <aroram18@mcmaster.ca>
2022-11-02 17:47:00 -0400

    .

--- intellij-ext/src/main/kotlin/com/metalbear/mirrord/KubeDataProvider.kt
+++ intellij-ext/src/main/kotlin/com/metalbear/mirrord/KubeDataProvider.kt
@@ -1,20 +1,21 @@
 package com.metalbear.mirrord
+
 import io.kubernetes.client.openapi.apis.CoreV1Api
 import io.kubernetes.client.util.Config
 import io.kubernetes.client.util.Namespaces
 
 class KubeDataProvider : CoreV1Api(Config.defaultClient()) {
-    fun getNameSpacedPods(namespace: String?): List<String> {
+    fun getNameSpacedPods(namespace: String): List<String> {
         val pods = listNamespacedPod(namespace, null, null, null, null, null, null, null, null, null, null)
-        return pods.items.map{ it.metadata!!.name!! }
+        return pods.items.map { it.metadata!!.name!! }
     }
 
-    fun getNamespaces() : List<String> {
+    fun getNamespaces(): List<String> {
         val namespaces = listNamespace(null, null, null, null, null, null, null, null, null, null)
         return namespaces.items.map { it.metadata!!.name!! }
     }
 
-    fun getPodNamespace() : String {
+    fun getPodNamespace(): String {
         return Namespaces.getPodNamespace()
     }
 }
