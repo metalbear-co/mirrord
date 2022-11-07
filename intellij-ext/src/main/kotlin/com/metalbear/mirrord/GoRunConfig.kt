d107a440fd54e99178bed0cc30b54b00f942b15d
infiniteregrets <aroram18@mcmaster.ca>
2022-11-07 11:03:10 -0500

    update

--- intellij-ext/src/main/kotlin/com/metalbear/mirrord/GoRunConfig.kt
+++ intellij-ext/src/main/kotlin/com/metalbear/mirrord/GoRunConfig.kt
@@ -63,7 +63,8 @@ class GoRunConfig : GoRunConfigurationExtension() {
     ) {
         if (commandLineType == GoRunningState.CommandLineType.RUN &&
             MirrordListener.enabled && !MirrordListener.envSet &&
-            System.getProperty("os.name").toLowerCase().startsWith("mac")
+            System.getProperty("os.name").toLowerCase().startsWith("mac") &&
+            System.getProperty("os.arch") == "aaarch64"
         ) {
             val delvePath = getCustomDelvePath()
             // convert the delve file to an executable
