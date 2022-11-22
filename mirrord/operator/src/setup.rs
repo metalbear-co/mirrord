1dd07360a7d27ec55cb8354f957a33266ab9f322
DmitryDodzin <d.dodzin@gmail.com>
2022-11-22 18:36:40 +0200

    Role

--- mirrord/operator/src/setup.rs
+++ mirrord/operator/src/setup.rs
@@ -3,7 +3,10 @@ use std::{collections::BTreeMap, convert::Infallible, io::Write, str::FromStr};
 use k8s_openapi::{
     api::{
         apps::v1::{Deployment, DeploymentSpec},
-        core::v1::{Container, Namespace, PodSpec, PodTemplateSpec, ServiceAccount},
+        core::v1::{
+            Container, ContainerPort, EnvVar, Namespace, PodSpec, PodTemplateSpec, ServiceAccount,
+        },
+        rbac::v1::{ClusterRole, ClusterRoleBinding, PolicyRule, RoleRef, Subject},
     },
     apimachinery::pkg::apis::meta::v1::{LabelSelector, ObjectMeta},
 };
@@ -29,17 +32,23 @@ pub struct Operator {
     namespace: OperatorNamespace,
     deployment: OperatorDeployment,
     service_account: OperatorServiceAccount,
+    role: OperatorRole,
+    role_binding: OperatorRoleBinding,
 }
 
 impl Operator {
     pub fn new(namespace: OperatorNamespace) -> Self {
         let deployment = OperatorDeployment::new(&namespace);
         let service_account = OperatorServiceAccount::new(&namespace);
+        let role = OperatorRole::new();
+        let role_binding = OperatorRoleBinding::new(&namespace);
 
         Operator {
             namespace,
             deployment,
             service_account,
+            role,
+            role_binding,
         }
     }
 }
@@ -47,8 +56,16 @@ impl Operator {
 impl OperatorSetup for Operator {
     fn to_writer<W: Write>(&self, mut writer: W) -> Result<()> {
         self.namespace.to_writer(&mut writer)?;
+
         writer.write_all(b"---\n")?;
         self.service_account.to_writer(&mut writer)?;
+
+        writer.write_all(b"---\n")?;
+        self.role.to_writer(&mut writer)?;
+
+        writer.write_all(b"---\n")?;
+        self.role_binding.to_writer(&mut writer)?;
+
         writer.write_all(b"---\n")?;
         self.deployment.to_writer(&mut writer)?;
 
@@ -97,11 +114,23 @@ impl OperatorDeployment {
         let container = Container {
             name: "operator".to_owned(),
             image: Some("ghcr.io/metalbear-co/operator:latest".to_owned()),
+            image_pull_policy: Some("IfNotPresent".to_owned()),
+            env: Some(vec![EnvVar {
+                name: "RUST_LOG".to_owned(),
+                value: Some("mirrord=info,operator=info".to_owned()),
+                value_from: None,
+            }]),
+            ports: Some(vec![ContainerPort {
+                name: Some("tcp".to_owned()),
+                container_port: 8080,
+                ..Default::default()
+            }]),
             ..Default::default()
         };
 
         let pod_spec = PodSpec {
             containers: vec![container],
+            service_account_name: Some("operator".to_owned()),
             ..Default::default()
         };
 
@@ -163,6 +192,80 @@ impl OperatorServiceAccount {
 }
 
 impl OperatorSetup for OperatorServiceAccount {
+    fn to_writer<W: Write>(&self, mut writer: W) -> Result<()> {
+        serde_yaml::to_writer(&mut writer, &self.0).map_err(SetupError::from)
+    }
+}
+
+#[derive(Debug)]
+pub struct OperatorRole(ClusterRole);
+
+impl OperatorRole {
+    pub fn new() -> Self {
+        let role = ClusterRole {
+            metadata: ObjectMeta {
+                name: Some("operator".to_owned()),
+                ..Default::default()
+            },
+            rules: Some(vec![
+                PolicyRule {
+                    api_groups: Some(vec!["".to_owned(), "apps".to_owned(), "batch".to_owned()]),
+                    resources: Some(vec![
+                        "pods".to_owned(),
+                        "deployments".to_owned(),
+                        "jobs".to_owned(),
+                    ]),
+                    verbs: vec!["get".to_owned(), "list".to_owned(), "watch".to_owned()],
+                    ..Default::default()
+                },
+                PolicyRule {
+                    api_groups: Some(vec!["batch".to_owned()]),
+                    resources: Some(vec!["jobs".to_owned()]),
+                    verbs: vec!["create".to_owned()],
+                    ..Default::default()
+                },
+            ]),
+            ..Default::default()
+        };
+
+        OperatorRole(role)
+    }
+}
+
+impl OperatorSetup for OperatorRole {
+    fn to_writer<W: Write>(&self, mut writer: W) -> Result<()> {
+        serde_yaml::to_writer(&mut writer, &self.0).map_err(SetupError::from)
+    }
+}
+
+#[derive(Debug)]
+pub struct OperatorRoleBinding(ClusterRoleBinding);
+
+impl OperatorRoleBinding {
+    pub fn new(namespace: &OperatorNamespace) -> Self {
+        let role_binding = ClusterRoleBinding {
+            metadata: ObjectMeta {
+                name: Some("operator".to_owned()),
+                ..Default::default()
+            },
+            role_ref: RoleRef {
+                api_group: "rbac.authorization.k8s.io".to_owned(),
+                kind: "Role".to_owned(),
+                name: "operator".to_owned(),
+            },
+            subjects: Some(vec![Subject {
+                api_group: Some("".to_owned()),
+                kind: "ServiceAccount".to_owned(),
+                name: "operator".to_owned(),
+                namespace: Some(namespace.name().to_owned()),
+            }]),
+        };
+
+        OperatorRoleBinding(role_binding)
+    }
+}
+
+impl OperatorSetup for OperatorRoleBinding {
     fn to_writer<W: Write>(&self, mut writer: W) -> Result<()> {
         serde_yaml::to_writer(&mut writer, &self.0).map_err(SetupError::from)
     }
