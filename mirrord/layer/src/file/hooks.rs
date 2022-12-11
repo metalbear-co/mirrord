019bcd1803eea753cd2def14df37c4e03d75db87
Aviram Hassan <aviramyhassan@gmail.com>
2022-12-11 19:40:37 +0200

    ..

--- mirrord/layer/src/file/hooks.rs
+++ mirrord/layer/src/file/hooks.rs
@@ -500,6 +500,55 @@ unsafe extern "C" fn fstatat_detour(
     result
 }
 
+#[cfg(target_os = "linux")]
+/// Hook for `libc::statx`. This is only available on Linux.
+#[hook_guard_fn]
+unsafe extern "C" fn statx_detour(
+    fd: RawFd,
+    raw_path: *const c_char,
+    flag: c_int,
+    out_stat: *mut statx,
+) -> c_int {
+    let follow_symlink = (flag & libc::AT_SYMLINK_NOFOLLOW) == 0;
+    let path = if flags & libc::AT_EMPTYPATH != 0 {
+        None
+    } else {
+        Some((!raw_path.is_null()).then(|| CStr::from_ptr(raw_path)))
+    };
+    let (Ok(result) | Err(result)) = xstat(path, Some(fd), follow_symlink)
+        .map(|res| {
+            let res = res.metadata;
+            out_stat.write_bytes(0, 1);
+            let out = &mut *out_stat;
+            // Set mask for available fields
+            out.stx_mask = libc::STATX_BASIC_STATS;
+            out.stx_mode = best_effort_cast(metadata.mode);
+            out.stx_size = best_effort_cast(metadata.size);
+            out.stx_atime.tv_nsec = metadata.access_time;
+            out.stx_mtime.tv_nsec = metadata.modification_time;
+            out.stx_ctime.tv_nsec = metadata.creation_time;
+            out.stx_atime.tv_sec = nano_to_secs(metadata.access_time);
+            out.stx_mtime.tv_sec = nano_to_secs(metadata.modification_time);
+            out.stx_ctime.tv_sec = nano_to_secs(metadata.creation_time);
+            out.stx_nlink = best_effort_cast(metadata.hard_links);
+            out.stx_uid = metadata.user_id;
+            out.stx_gid = metadata.group_id;
+            out.stx_ino = best_effort_cast(metadata.inode);
+            out.stx_blksize = best_effort_cast(metadata.block_size);
+            out.stx_blocks = best_effort_cast(metadata.blocks);
+
+            out.stx_dev_major = libc::major(metadata.device_id);
+            out.stx_dev_minor = libc::minor(metadata.device_id);
+            out.stx_rdev_major = libc::major(metadata.rdevice_id);
+            out.stx_rdev_minor = libc::minor(metadata.rdevice_id);
+            0
+        })
+        .bypass_with(|_| FN_STATX(fd, raw_path, flag, out_stat))
+        .map_err(From::from);
+
+    result
+}
+
 /// Convenience function to setup file hooks (`x_detour`) with `frida_gum`.
 pub(crate) unsafe fn enable_file_hooks(hook_manager: &mut HookManager) {
     replace!(hook_manager, "open", open_detour, FnOpen, FN_OPEN);
@@ -524,6 +573,10 @@ pub(crate) unsafe fn enable_file_hooks(hook_manager: &mut HookManager) {
         FnFaccessat,
         FN_FACCESSAT
     );
+    #[cfg(target_os = "linux")]
+    {
+        replace!(hook_manager, "statx", statx_detour, FnStatx, FN_STATX);
+    }
     #[cfg(not(all(target_os = "macos", target_arch = "x86_64")))]
     {
         replace!(hook_manager, "lstat", lstat_detour, FnLstat, FN_LSTAT);
