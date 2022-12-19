b1db36723965bbd0bd1575929cc66a67122f0272
meowjesty <meowjesty_nyan@outlook.com>
2022-12-19 17:43:51 -0300

    Test passthrough channel.

--- mirrord/agent/src/steal/http_traffic.rs
+++ mirrord/agent/src/steal/http_traffic.rs
@@ -260,8 +260,96 @@ mod http_traffic_tests {
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn test_http_traffic_filter_total_passthrough_not_http() {
+    async fn test_http_traffic_filter_no_headers_match_the_request() {
         let server_address: SocketAddr = (Ipv4Addr::LOCALHOST, 8888).into();
+        let server = TcpListener::bind(server_address)
+            .await
+            .expect("Bound TcpListener.");
+
+        let request_task = tokio::spawn(async move {
+            let client = reqwest::Client::new();
+            let request = client
+                .get(format!("http://127.0.0.1:{}", server_address.port()))
+                .header("First-Header", "mirrord")
+                .header("Mirrord-Test", "Hello")
+                .build()
+                .unwrap();
+
+            // Send a request and wait compare the dummy response we get from the filter's hyper
+            // handler.
+            let response = client.execute(request).await.unwrap();
+            assert_eq!(response.text().await.unwrap(), "Passthrough!".to_string());
+        });
+
+        let (tcp_stream, _) = server.accept().await.expect("Connection success!");
+
+        let client_id = 1;
+        let filter = Regex::new("Goodbye").expect("Valid regex.");
+
+        let (captured_tx, _) = channel(15000);
+        let (passthrough_tx, mut passthrough_rx) = channel(15000);
+
+        let http_filter_manager = HttpFilterManager::new(
+            tcp_stream.local_addr().unwrap().port(),
+            client_id,
+            filter,
+            captured_tx,
+            passthrough_tx,
+        );
+
+        let HttpFilter {
+            hyper_task,
+            mut reversable_stream,
+            mut interceptor_stream,
+        } = http_filter_manager
+            .new_connection(tcp_stream, server_address, 0)
+            .await
+            .unwrap()
+            .unwrap();
+
+        let mut interceptor_buffer = vec![0; 15000];
+
+        loop {
+            select! {
+                // Server stream reads what it received from the client (remote app), and sends it
+                // to the hyper task via the intermmediate DuplexStream.
+                Ok(read) = reversable_stream.read(&mut interceptor_buffer) => {
+                    if read == 0 {
+                        break;
+                    }
+
+                    let wrote = interceptor_stream.write(&interceptor_buffer[..read]).await.unwrap();
+                    assert_eq!(wrote, read);
+                }
+
+                // Receives requests from the hyper task that did not match any filter.
+                Some(_) = passthrough_rx.recv() => {
+                    // Send the dummy response from hyper to our client, so it can stop blocking
+                    // and exit.
+                    let mut response_buffer = vec![0;1500];
+                    let read_amount = interceptor_stream.read(&mut response_buffer).await.unwrap();
+                    reversable_stream.write(&response_buffer[..read_amount]).await.unwrap();
+
+                    break;
+                }
+
+                else => {
+                    break;
+                }
+            }
+        }
+
+        // Manually close this stream to notify the filter's hyper handler that this connection is
+        // over.
+        drop(interceptor_stream);
+
+        assert!(hyper_task.await.is_ok());
+        assert!(request_task.await.is_ok());
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_http_traffic_filter_total_passthrough_not_http() {
+        let server_address: SocketAddr = (Ipv4Addr::LOCALHOST, 9999).into();
         let server = TcpListener::bind(server_address)
             .await
             .expect("Bound TcpListener.");
