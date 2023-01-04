63542c0ca56f28679cd0760de78783e8e7ed0649
infiniteregrets <aroram18@mcmaster.ca>
2023-01-04 08:26:26 +0000

    .

--- mirrord/cli/tests/common/applications.rs
+++ mirrord/cli/tests/common/applications.rs
@@ -1,9 +1,27 @@
+use test_binary::build_test_binary;
+
 pub enum Application {
     RustFileOps,
 }
 
 impl Application {
     pub async fn get_executable(&self) -> String {
-        todo!()
+        match self {
+            Application::RustFileOps => String::from("target/debug/fileops"),
+        }
+    }
+
+    pub async fn build_executable(&self) {
+        match self {
+            Application::RustFileOps => {
+                let test_bin_path =
+                    build_test_binary("fileops", "../../../").expect("error building test binary");
+                let test_bin_path = test_bin_path
+                    .to_str()
+                    .expect("error converting test binary path to string")
+                    .to_string();
+                println!("test_bin_path: {}", test_bin_path);
+            }
+        }
     }
 }
