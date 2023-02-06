151156cf2c4aef4b2623a165e947d2f5146c3bab
meowjesty <meowjesty_nyan@outlook.com>
2023-02-06 18:49:59 -0300

    Refactor HTTP version handling into separate modules (layer).

---
+++ mirrord/layer/src/tcp_steal/httpv1.rs
@@ -0,0 +1,123 @@
+use std::net::SocketAddr;
+
+use anyhow::Result;
+use bytes::Bytes;
+use futures::FutureExt;
+use http_body_util::Full;
+use hyper::client::conn::http1;
+use mirrord_protocol::{
+    tcp::{HttpRequest, HttpResponse},
+    ConnectionId, Port,
+};
+use tokio::{
+    net::TcpStream,
+    sync::mpsc::{Receiver, Sender},
+};
+use tracing::error;
+
+use super::{handle_response, ConnectionTask};
+use crate::{detour::DetourGuard, tcp_steal::http_forwarding::HttpForwarderError};
+
+pub(super) struct V1 {
+    address: SocketAddr,
+    sender: http1::SendRequest<Full<Bytes>>,
+}
+
+impl V1 {
+    async fn send_http_request_to_application(
+        &mut self,
+        request: HttpRequest,
+        port: Port,
+        connection_id: ConnectionId,
+    ) -> Result<HttpResponse, HttpForwarderError> {
+        let request_id = request.request_id;
+
+        let response = self
+            .sender
+            .send_request(request.internal_request.clone().into())
+            .map(|response| handle_response(request, response, port, connection_id, request_id))
+            .await
+            .await;
+
+        // Retry once if the connection was closed.
+        if let Err(HttpForwarderError::ConnectionClosedTooSoon(request)) = response {
+            let Self { address, sender } =
+                ConnectionTask::<Self>::connect_to_application(self.address).await?;
+
+            self.address = address;
+            self.sender = sender;
+
+            Ok(self
+                .sender
+                .send_request(request.internal_request.clone().into())
+                .map(|response| handle_response(request, response, port, connection_id, request_id))
+                .await
+                .await?)
+        } else {
+            response
+        }
+    }
+}
+
+impl ConnectionTask<V1> {
+    pub(super) async fn new(
+        connect_to: SocketAddr,
+        request_receiver: Receiver<HttpRequest>,
+        response_sender: Sender<HttpResponse>,
+        port: Port,
+        connection_id: ConnectionId,
+    ) -> Result<Self, HttpForwarderError> {
+        let http_version = Self::connect_to_application(connect_to).await?;
+
+        Ok(Self {
+            connect_to,
+            request_receiver,
+            response_sender,
+            port,
+            connection_id,
+            http_version,
+        })
+    }
+
+    async fn connect_to_application(connect_to: SocketAddr) -> Result<V1, HttpForwarderError> {
+        let target_stream = {
+            let _ = DetourGuard::new();
+            TcpStream::connect(connect_to).await?
+        };
+
+        let (http_request_sender, connection) = http1::handshake(target_stream).await?;
+
+        // spawn a task to poll the connection.
+        tokio::spawn(async move {
+            if let Err(fail) = connection.await {
+                error!("Error in http connection with addr {connect_to:?}: {fail:?}");
+            }
+        });
+
+        Ok(V1 {
+            address: connect_to,
+            sender: http_request_sender,
+        })
+    }
+
+    pub(super) async fn start(self) -> Result<(), HttpForwarderError> {
+        let Self {
+            connect_to,
+            mut request_receiver,
+            response_sender,
+            port,
+            connection_id,
+            mut http_version,
+        } = self;
+
+        while let Some(request) = request_receiver.recv().await {
+            let response = http_version
+                .send_http_request_to_application(request, port, connection_id)
+                .await?;
+
+            response_sender.send(response).await?;
+        }
+
+        Ok(())
+    }
+}
