6892f755493b0db6fd23fa39147fe835a7f8ea53
t4lz <t4lz.git@gmail.com>
2023-02-08 12:24:12 +0100

    no fs in http test.

--- mirrord/layer/tests/http_mirroring.rs
+++ mirrord/layer/tests/http_mirroring.rs
@@ -46,7 +46,7 @@ async fn test_mirroring_with_http(
     dylib_path: &PathBuf,
 ) {
     let (mut test_process, mut layer_connection) = application
-        .start_process_with_layer_and_port(dylib_path, vec![])
+        .start_process_with_layer_and_port(dylib_path, vec![("MIRRORD_FILE_MODE", "local")])
         .await;
 
     println!("Application subscribed to port, sending tcp messages.");
