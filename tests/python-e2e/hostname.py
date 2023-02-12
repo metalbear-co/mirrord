9abd3086a32ba10ec00e353c583fa624817d58b5
Dmitry Dodzin <d.dodzin@gmail.com>
2023-02-12 22:39:27 +0200

    Trim \n test

--- tests/python-e2e/hostname.py
+++ tests/python-e2e/hostname.py
@@ -2,9 +2,13 @@ import socket
 import unittest
 
 class Hostname(unittest.TestCase):
-
-	def test_is_equal(self):
+	def test_contains_hostname_echo(self):
 		self.assertIn('hostname-echo', socket.gethostname())
 
+	def test_trimmed(self):
+		hostname = socket.gethostname()
+		self.assertEqual(hostname.rstrip(), hostname)
+
 if __name__ == "__main__":
+	print("{} socket".format(socket.gethostname()))
 	unittest.main()
