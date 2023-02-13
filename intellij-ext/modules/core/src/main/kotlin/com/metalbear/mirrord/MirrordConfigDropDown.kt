16bb5941adf7f92d95c3c91b1d7ef23ede6628c6
infiniteregrets <aroram18@mcmaster.ca>
2023-02-13 11:42:51 -0500

    fme

--- intellij-ext/modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordConfigDropDown.kt
+++ intellij-ext/modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordConfigDropDown.kt
@@ -1,38 +1,55 @@
 package com.metalbear.mirrord
 
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.ex.ComboBoxAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.newvfs.BulkFileListener
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import javax.swing.JComponent
 
 class MirrordConfigDropDown : ComboBoxAction() {
-    private lateinit var configPaths: ArrayList<String>
+    private var project: Project? = null
+    private var configPaths: ArrayList<String> = ArrayList()
+
     companion object {
         var selectedConfig = "Select Configuration"
     }
 
-    override fun createPopupActionGroup(button: JComponent?): DefaultActionGroup {
-        val actionGroup = DefaultActionGroup()
-        configPaths.forEach { configPath ->
-            val trimmedPath = configPath.split("/").takeLast(3).joinToString("/")
-            actionGroup.add(object : AnAction(configPath) {
-                override fun actionPerformed(e: AnActionEvent) {
-                    selectedConfig = trimmedPath
-                    e.presentation.text = selectedConfig
-                }
-            })
+    override fun createPopupActionGroup(button: JComponent, dataContext: DataContext): DefaultActionGroup {
+        project = dataContext.getData(CommonDataKeys.PROJECT)
+        configPaths = MirrordConfigAPI.searchConfigPaths(project!!) as ArrayList<String>
+
+        return DefaultActionGroup().apply {
+            configPaths.forEach { configPath ->
+                add(object : AnAction(configPath) {
+                    override fun actionPerformed(e: AnActionEvent) {
+                        selectedConfig = configPath
+                        //  e.presentation.text = selectedConfig
+                        //  ^ apparently nothing happens when text is updated here,
+                        //  so I guess need to update it in update()?
+                    }
+                })
+            }
         }
-        return actionGroup
+    }
+
+    fun updatePaths() {
+        project?.let {
+            configPaths = MirrordConfigAPI.searchConfigPaths(it) as ArrayList<String>
+            selectedConfig = "Select Configuration"
+        }
     }
 
     override fun update(e: AnActionEvent) {
-        val project = e.project ?: throw Error("required to have configured project to use mirrord config.")
-        configPaths = MirrordConfigAPI.searchConfigPaths(project) as ArrayList<String>
-        if (configPaths.size > 1) {
-            configPaths.add(0, "Default Configuration")
+        e.presentation.apply {
+            isEnabledAndVisible = configPaths.size > 1
+            text = selectedConfig
         }
-        e.presentation.isEnabledAndVisible = configPaths.size > 1
-        e.presentation.text = selectedConfig
+    }
+}
+
+class MirrordConfigFileRefresher : BulkFileListener {
+    override fun before(events: MutableList<out VFileEvent>) {
+        MirrordConfigDropDown().updatePaths()
     }
 }
