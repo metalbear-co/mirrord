c0cdd9322a1cba45ecf67052cf159431d2ff50f7
meowjesty <meowjesty_nyan@outlook.com>
2023-02-17 16:55:21 -0300

    Change http2 steal test to be a bit more chatty.

--- tests/node-e2e/http2/test_http2_traffic_steal.mjs
+++ tests/node-e2e/http2/test_http2_traffic_steal.mjs
@@ -3,39 +3,56 @@ import http2 from 'node:http2';
 const server = http2.createServer();
 
 server.on('error', (err) => {
-    console.error(err);
-    process.exit(-1);
+  console.error(err);
+  process.exit(-1);
 });
 
 server.on('stream', (stream, headers) => {
-    // stream is a Duplex
-    stream.respond({
-        'content-type': 'text/html; charset=utf-8',
-        ':status': 200,
-    });
+  console.log(`> Request ${JSON.stringify(headers)}`);
 
-    stream.end('<h1>Hello World: from local</h1>');
+  // stream is a Duplex
+  stream.respond({
+    'content-type': 'text/html; charset=utf-8',
+    ':status': 200,
+  });
 
-    stream.on('error', (fail) => {
-        console.error(`Stream failed with ${fail}!`);
-        process.exit(-2);
-    });
+  stream.end('<h1>Hello World: <b>from local app</b></h1>');
 
-    stream.on('aborted', () => {
-        console.error(`Stream was aborted!`);
-        process.exit(-3);
-    });
+  stream.on('error', (fail) => {
+    console.error(`> Stream failed with ${fail}!`);
+    process.exit(-2);
+  });
 
-    stream.on('timeout', () => {
-        console.error(`Stream timed out!`);
-        process.exit(-4);
-    });
+  stream.on('aborted', () => {
+    console.error(`> Stream was aborted!`);
+    process.exit(-3);
+  });
 
-    stream.on('end', () => {
-        console.log(`Stream is done.`);
+  stream.on('timeout', () => {
+    console.error(`> Stream timed out!`);
+    process.exit(-4);
+  });
 
-        process.exit();
-    });
+  stream.on('end', () => {
+    console.log(`> Stream is done.`);
+    stream.close();
+  });
+
+  stream.on('close', () => {
+    console.log('> stream closed');
+    server.close();
+    process.kill(process.pid);
+  })
+});
+
+server.on('close', () => {
+  console.log('> server close');
+  process.exit();
 });
 
 server.listen(80);
+
+process.on('SIGTERM', () => {
+  console.log('> shutdown');
+  server.close();
+});
