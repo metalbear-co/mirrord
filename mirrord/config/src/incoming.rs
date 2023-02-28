1e1a2b739c1fdfdecf9241ef2ae04caa767758a4
Aviram Hassan <aviramyhassan@gmail.com>
2023-02-28 08:27:42 +0200

    ..

--- mirrord/config/src/incoming.rs
+++ mirrord/config/src/incoming.rs
@@ -93,7 +93,10 @@ impl MirrordConfig for IncomingFileConfig {
                     .http_header_filter
                     .unwrap_or_default()
                     .generate_config()?,
-                port_mapping: advanced.port_mapping.unwrap_or_default(),
+                port_mapping: advanced
+                    .port_mapping
+                    .map(|m| m.into_iter().collect())
+                    .unwrap_or_default(),
             },
         };
 
@@ -131,8 +134,8 @@ pub struct IncomingAdvancedFileConfig {
     ///
     /// This is useful when you want to mirror/steal a port to a different port on the remote
     /// machine. For example, your local process listens on port 9333 and the container listens
-    /// on port 80. You'd use { 9333: 80 }
-    pub port_mapping: Option<HashMap<u16, u16>>,
+    /// on port 80. You'd use [[9333, 80]]
+    pub port_mapping: Option<Vec<(u16, u16)>>,
 }
 
 #[derive(Default, PartialEq, Eq, Clone, Debug)]
