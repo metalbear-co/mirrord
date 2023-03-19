3e61e0b7901c696ddf94a35454eed4f25c1528f6
Dmitry Dodzin <d.dodzin@gmail.com>
2023-03-19 10:41:49 +0200

    chain -> chain_name

--- mirrord/agent/src/steal/ip_tables/chain.rs
+++ mirrord/agent/src/steal/ip_tables/chain.rs
@@ -11,7 +11,7 @@ use crate::{
 #[derive(Debug)]
 pub struct IPTableChain<IPT: IPTables> {
     inner: Arc<IPT>,
-    chain: String,
+    chain_name: String,
     chain_size: AtomicI32,
 }
 
@@ -19,25 +19,25 @@ impl<IPT> IPTableChain<IPT>
 where
     IPT: IPTables,
 {
-    pub fn create(inner: Arc<IPT>, chain: String) -> Result<Self> {
-        inner.create_chain(&chain)?;
+    pub fn create(inner: Arc<IPT>, chain_name: String) -> Result<Self> {
+        inner.create_chain(&chain_name)?;
 
         // Start with 1 because the chain will allways have atleast `-A <chain name>` as a rule
         let chain_size = AtomicI32::from(1);
 
         Ok(IPTableChain {
             inner,
-            chain,
+            chain_name,
             chain_size,
         })
     }
 
-    pub fn load(inner: Arc<IPT>, chain: String) -> Result<Self> {
-        let existing_rules = inner.list_rules(&chain)?.len();
+    pub fn load(inner: Arc<IPT>, chain_name: String) -> Result<Self> {
+        let existing_rules = inner.list_rules(&chain_name)?.len();
 
         if existing_rules == 0 {
             return Err(AgentError::IPTablesError(format!(
-                "Unable to load rules for chain {chain}"
+                "Unable to load rules for chain {chain_name}"
             )));
         }
 
@@ -46,13 +46,13 @@ where
 
         Ok(IPTableChain {
             inner,
-            chain,
+            chain_name,
             chain_size,
         })
     }
 
     pub fn chain_name(&self) -> &str {
-        &self.chain
+        &self.chain_name
     }
 
     pub fn inner(&self) -> &IPT {
@@ -62,7 +62,7 @@ where
     pub fn add_rule(&self, rule: &str) -> Result<i32> {
         self.inner
             .insert_rule(
-                &self.chain,
+                &self.chain_name,
                 rule,
                 self.chain_size.fetch_add(1, Ordering::Relaxed),
             )
@@ -74,7 +74,7 @@ where
     }
 
     pub fn remove_rule(&self, rule: &str) -> Result<()> {
-        self.inner.remove_rule(&self.chain, rule)?;
+        self.inner.remove_rule(&self.chain_name, rule)?;
 
         self.chain_size.fetch_sub(1, Ordering::Relaxed);
 
@@ -87,6 +87,6 @@ where
     IPT: IPTables,
 {
     fn drop(&mut self) {
-        let _ = self.inner.remove_chain(&self.chain);
+        let _ = self.inner.remove_chain(&self.chain_name);
     }
 }
