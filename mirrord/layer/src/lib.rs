f8a26da1d2fc3030f461f1f92ba783a01f9e64d4
Aviram Hassan <aviram@metalbear.co>
2023-03-22 00:35:29 +0200

    remove all f functions, add open to cover missed function (#1201)
    * remove all f functions, add open to cover missed function
    
    * change notes
    
    * fmt
    
    * re-enable flask http test
    
    * nitpick
    
    * ?
    
    * ..
    
    * ..
    
    * more logs
    
    * ..
    
    * add __close and __close_nocancel

--- mirrord/layer/src/lib.rs
+++ mirrord/layer/src/lib.rs
@@ -822,15 +822,34 @@ fn enable_hooks(enabled_file_ops: bool, enabled_remote_dns: bool, patch_binaries
             FnClose_nocancel,
             FN_CLOSE_NOCANCEL
         );
-        // Solve leak on uvloop which calls the syscall directly.
-        #[cfg(target_os = "linux")]
+
         replace!(
             &mut hook_manager,
-            "uv_fs_close",
-            uv_fs_close,
-            FnUv_fs_close,
-            FN_UV_FS_CLOSE
+            "__close_nocancel",
+            __close_nocancel_detour,
+            Fn__close_nocancel,
+            FN___CLOSE_NOCANCEL
         );
+
+        replace!(
+            &mut hook_manager,
+            "__close",
+            __close_detour,
+            Fn__close,
+            FN___CLOSE
+        );
+
+        // Solve leak on uvloop which calls the syscall directly.
+        #[cfg(target_os = "linux")]
+        {
+            replace!(
+                &mut hook_manager,
+                "uv_fs_close",
+                uv_fs_close_detour,
+                FnUv_fs_close,
+                FN_UV_FS_CLOSE
+            );
+        }
     };
 
     unsafe { socket::hooks::enable_socket_hooks(&mut hook_manager, enabled_remote_dns) };
@@ -902,14 +921,30 @@ pub(crate) unsafe extern "C" fn close_nocancel_detour(fd: c_int) -> c_int {
     close_detour(fd)
 }
 
+#[hook_fn]
+pub(crate) unsafe extern "C" fn __close_nocancel_detour(fd: c_int) -> c_int {
+    close_detour(fd)
+}
+
+#[hook_fn]
+pub(crate) unsafe extern "C" fn __close_detour(fd: c_int) -> c_int {
+    close_detour(fd)
+}
+
 // TODO(alex) [mid] 2023-01-24: What is this?
 ///
 /// ## Hook
 ///
-/// Replaces `?`.
+/// Needed for libuv that calls the syscall directly.
+/// https://github.dev/libuv/libuv/blob/7b84d5b0ecb737b4cc30ce63eade690d994e00a6/src/unix/core.c#L557-L558
 #[cfg(target_os = "linux")]
 #[hook_guard_fn]
-pub(crate) unsafe extern "C" fn uv_fs_close(a: usize, b: usize, fd: c_int, c: usize) -> c_int {
+pub(crate) unsafe extern "C" fn uv_fs_close_detour(
+    a: usize,
+    b: usize,
+    fd: c_int,
+    c: usize,
+) -> c_int {
     // In this case we call `close_layer_fd` before the original close function, because execution
     // does not return to here after calling `FN_UV_FS_CLOSE`.
     close_layer_fd(fd);
