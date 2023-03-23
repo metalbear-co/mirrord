30c899fd065efeec6becb5f324f315d583978819
Dmitry Dodzin <d.dodzin@gmail.com>
2023-03-23 18:51:29 +0200

    Fallible Operator Discover (#1219)
    * Cool
    
    * Subprogress
    
    * CR

--- mirrord/cli/src/connection.rs
+++ mirrord/cli/src/connection.rs
@@ -1,11 +1,13 @@
 use std::time::Duration;
 
+use miette::IntoDiagnostic;
 use mirrord_config::LayerConfig;
 use mirrord_kube::api::{kubernetes::KubernetesAPI, AgentManagment};
 use mirrord_operator::client::OperatorApi;
 use mirrord_progress::Progress;
 use mirrord_protocol::{ClientMessage, DaemonMessage};
 use tokio::sync::mpsc;
+use tracing::warn;
 
 use crate::{CliError, Result};
 
@@ -21,6 +23,40 @@ pub(crate) struct AgentConnection {
     pub receiver: mpsc::Receiver<DaemonMessage>,
 }
 
+pub(crate) async fn connect_operator<P>(
+    config: &LayerConfig,
+    progress: &P,
+) -> Option<(mpsc::Sender<ClientMessage>, mpsc::Receiver<DaemonMessage>)>
+where
+    P: Progress + Send + Sync,
+{
+    let sub_progress = progress.subtask("Checking Operator");
+
+    match OperatorApi::discover(config)
+        .await
+        .map_err(CliError::OperatorConnectionFailed)
+        .into_diagnostic()
+    {
+        Ok(Some(connection)) => {
+            sub_progress.done_with("Connected to Operator");
+
+            Some(connection)
+        }
+        Ok(None) => {
+            sub_progress.done_with("No Operator Detected");
+
+            None
+        }
+        Err(err) => {
+            sub_progress.fail_with("Unable to connect to Operator");
+
+            warn!("{err}");
+
+            None
+        }
+    }
+}
+
 /// Creates an agent if needed then connects to it.
 pub(crate) async fn create_and_connect<P>(
     config: &LayerConfig,
@@ -29,9 +65,7 @@ pub(crate) async fn create_and_connect<P>(
 where
     P: Progress + Send + Sync,
 {
-    if config.operator && let Some(connection) = OperatorApi::discover(config).await.transpose() {
-        let (sender, receiver) = connection.map_err(CliError::OperatorConnectionFailed)?;
-
+    if config.operator && let Some((sender, receiver)) = connect_operator(config, progress).await {
         Ok((
             AgentConnectInfo::Operator,
             AgentConnection { sender, receiver },
