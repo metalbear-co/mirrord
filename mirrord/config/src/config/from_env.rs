5c592ec53527e33b4c8d0961e6d2913d0ae05dd3
Aviram Hassan <aviramyhassan@gmail.com>
2023-04-13 20:14:08 +0300

    this works

--- mirrord/config/src/config/from_env.rs
+++ mirrord/config/src/config/from_env.rs
@@ -25,6 +25,26 @@ where
     }
 }
 
+#[derive(Clone)]
+pub struct FromEnvWithError<T>(&'static str, PhantomData<T>);
+
+impl<T> FromEnvWithError<T> {
+    pub fn new(env: &'static str) -> Self {
+        FromEnvWithError(env, PhantomData::<T>)
+    }
+}
+
+impl<T> MirrordConfigSource for FromEnvWithError<T>
+where
+    T: FromStr<Err = ConfigError>,
+{
+    type Value = T;
+
+    fn source_value(self) -> Option<Result<Self::Value>> {
+        std::env::var(self.0).ok().map(|var| var.parse())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::rstest;
