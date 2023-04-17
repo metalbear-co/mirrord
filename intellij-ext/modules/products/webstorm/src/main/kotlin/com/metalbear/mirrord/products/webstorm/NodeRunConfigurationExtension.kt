55abb8a2e30daf0bc28c9d85f459a4d94eb8e93c
t4lz <t4lz.git@gmail.com>
2023-04-17 21:36:21 +0200

    Tried to use NodeJsRunConfigurationExtension, which can't be found.

--- intellij-ext/modules/products/webstorm/src/main/kotlin/com/metalbear/mirrord/products/webstorm/NodeRunConfigurationExtension.kt
+++ intellij-ext/modules/products/webstorm/src/main/kotlin/com/metalbear/mirrord/products/webstorm/NodeRunConfigurationExtension.kt
@@ -12,131 +12,42 @@ import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunCo
 import com.metalbear.mirrord.MirrordExecManager
 import com.metalbear.mirrord.MirrordLogger
 import com.jetbrains.nodejs.run.NodeJsRunConfiguration
-import java.lang.Override
+import com.jetbrains.nodejs.run.NodeJSRunConfigurationExtension
 
 import java.util.concurrent.ExecutionException
 
-
-
-
-
-class NodeRunConfigurationExtension: AbstractNodeRunConfigurationExtension() {
-    override fun readExternal(runConfiguration: AbstractNodeTargetRunProfile?, element: Element?) {
-        RunConfigSettingsEditor.readExternal(runConfiguration, element)
-    }
-
-    protected fun writeExternal(runConfiguration: AbstractNodeTargetRunProfile?, element: Element?) {
-        RunConfigSettingsEditor.writeExternal(runConfiguration, element)
-    }
-
-    override fun <P : AbstractNodeTargetRunProfile> createEditor(configuration: P): SettingsEditor<P> {
-        return RunConfigSettingsEditor(configuration)
-    }
-
-    fun isApplicableFor(configuration: AbstractNodeTargetRunProfile?): Boolean {
+class NodeRunConfigurationExtension: NodeJsRunConfigurationExtension() {
+    override fun isApplicableFor(configuration: AbstractNodeRunConfigurationExtension<*>): Boolean {
         return true
     }
 
-    @kotlin.Throws(java.util.concurrent.ExecutionException::class)
-    protected fun patchCommandLine(
-        configuration: AbstractNodeTargetRunProfile,
-        @Nullable runnerSettings: RunnerSettings?,
-        cmdLine: GeneralCommandLine?,
-        runnerId: String?,
-        executor: Executor?
+    override fun isEnabledFor(
+        applicableConfiguration: AbstractNodeRunConfigurationExtension<*>,
+        runnerSettings: RunnerSettings?
+    ): Boolean {
+        return true
+    }
+
+
+    override fun patchCommandLine(
+        configuration: AbstractNodeRunConfigurationExtension<*>,
+        runnerSettings: RunnerSettings?,
+        cmdLine: GeneralCommandLine,
+        runnerId: String
     ) {
-        configuration.getSelectedOptions()
-    }
+        val wsl = when (val request = createEnvironmentRequest(configuration, configuration.project)) {
+            is WslTargetEnvironmentRequest -> request.configuration.distribution!!
+            else -> null
+        }
 
-    protected fun extendCreatedConfiguration(
-        configuration: AbstractNodeTargetRunProfile?,
-        location: Location?
-    ) {
-        super.extendCreatedConfiguration(configuration, location)
-    }
+        val project = configuration.project
+        val currentEnv = cmdLine.environment
 
-    @Nullable
-    fun getEditorTitle(): String? {
-        return RunConfigSettingsEditor.getEditorTitle()
+        MirrordExecManager.start(wsl, project)?.let {
+                env ->
+            for (entry in env.entries.iterator()) {
+                currentEnv[entry.key] =  entry.value
+            }
+        }
     }
-
-    @Nullable
-    @kotlin.Throws(java.util.concurrent.ExecutionException::class)
-    fun createLaunchSession(
-        configuration: AbstractNodeTargetRunProfile,
-        environment: ExecutionEnvironment?
-    ): NodeRunConfigurationLaunchSession? {
-        val config: NodeJsRunConfiguration = configuration as NodeJsRunConfiguration
-        val newEnvs: Map<String, String> = RunConfigSettingsEditor
-            .collectEnv(configuration, config.getWorkingDirectory(), config.getEnvs())
-        config.setEnvs(newEnvs)
-        return null
-    }
-//    override fun isApplicableFor(configuration: RunConfigurationBase<*>): Boolean {
-//        return true
-//    }
-//
-//
-//    override fun isEnabledFor(
-//        applicableConfiguration: RunConfigurationBase<*>,
-//        runnerSettings: RunnerSettings?
-//    ): Boolean {
-//        return true
-//    }
-//
-//
-//    private fun < T: RunConfigurationBase<*>> patchEnv (configuration: T, params: JavaParameters) {
-//        MirrordLogger.logger.debug("Check if relevant")
-//        if (configuration.name.startsWith("Build ")) {
-//            MirrordLogger.logger.info("Configuration name %s ignored".format(configuration.name))
-//            return
-//        }
-//        MirrordLogger.logger.debug("wsl check")
-//        val wsl = when (val request = createEnvironmentRequest(configuration, configuration.project)) {
-//            is WslTargetEnvironmentRequest -> request.configuration.distribution!!
-//            else -> null
-//        }
-//
-//        MirrordLogger.logger.debug("getting env")
-//        val project = configuration.project
-//        val currentEnv = HashMap<String, String>()
-//        currentEnv.putAll(params.env)
-//
-//        val mirrordEnv = HashMap<String, String>()
-//        MirrordLogger.logger.debug("calling start")
-//        MirrordExecManager.start(wsl, project)?.let {
-//                env ->
-//            for (entry in env.entries.iterator()) {
-//                mirrordEnv[entry.key] =  entry.value
-//            }
-//        }
-//
-//        params.env = currentEnv + mirrordEnv
-//
-//        // Gradle support (and external system configuration)
-//        if (configuration is ExternalSystemRunConfiguration) {
-//            val ext = configuration as ExternalSystemRunConfiguration
-//            val newEnv = ext.settings.env + mirrordEnv
-//            ext.settings.env = newEnv
-//        }
-//        MirrordLogger.logger.debug("setting env and finishing")
-//    }
-//    override fun <T : RunConfigurationBase<*>> updateJavaParameters(
-//        configuration: T,
-//        params: JavaParameters,
-//        runnerSettings: RunnerSettings?,
-//        executor: Executor
-//    ) {
-//        MirrordLogger.logger.debug("updateJavaParameters called")
-//        patchEnv(configuration, params)
-//    }
-//
-//    override fun <T : RunConfigurationBase<*>> updateJavaParameters(
-//        configuration: T,
-//        params: JavaParameters,
-//        runnerSettings: RunnerSettings?
-//    ) {
-//        MirrordLogger.logger.debug("updateJavaParameters (with less parameters) called")
-//        patchEnv(configuration, params)
-//    }
 }
