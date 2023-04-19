e393a2059b76ad1db1573bdfed810bd0ee15aa9f
meowjesty <43983236+meowjesty@users.noreply.github.com>
2023-04-19 14:44:57 -0300

    Open hostname file remotely. (#1226)
    * Use RemoteFile ops to open/read/close in gethostname.
    
    * Try to fix integration expecting different message.
    
    * Increase test timeout.
    
    * Revert get_initialized function.
    
    * Change integration to be hostname aware.
    
    * Try to expand the integration test.
    
    * Moving the integration test forward.
    
    * Still trying to get the messages right.
    
    * Dont do UDP stuff in http_mirroring test.
    
    * Fix unused.
    
    * Print args.
    
    * Check if its go.
    
    * Disable pause test.
    
    * Fix is_go.
    
    * Better fixture?
    
    * Fix unwrap.
    
    * Add docs for the test changes. Disable UDP in node_spawn.
    
    * Remove change for spawn.
    
    * Increase timeout for spawn.
    
    * Use proper test assert.
    
    * Notes on what to do.
    
    * Handle gethostname for node spawn.
    
    * Only handle 2 layers in spawn?
    
    * Linux and macos handle spawn differently?
    
    * Proper macos cfg macro usage.
    
    * Check if first message is open file request.
    
    * Simplify test.
    
    * Added check to avoid next being called on the same message.
    
    * Add comments for the refactored test.
    
    * Make bash_layer_connection live longer (long live bash_layer_connection).
    
    * layer connection lives longer now?
    
    * Revert log changes
    
    * Change changelog
    
    * Don't log data.
    
    * Blank line.
    
    * Remove log from test fn.
    
    * Refactor node_spawn to use promises. Sleep a bit to get a proper close.
    
    ---------
    
    Co-authored-by: Aviram Hassan <aviram@metalbear.co>

--- mirrord/layer/src/socket/ops.rs
+++ mirrord/layer/src/socket/ops.rs
@@ -10,14 +10,16 @@ use std::{
 };
 
 use libc::{c_int, sockaddr, socklen_t};
-use mirrord_protocol::{dns::LookupRecord, file::OpenOptionsInternal};
+use mirrord_protocol::{
+    dns::LookupRecord,
+    file::{OpenFileResponse, OpenOptionsInternal, ReadFileResponse},
+};
 use socket2::SockAddr;
 use tokio::sync::oneshot;
 use tracing::{debug, error, trace};
 
 use super::{hooks::*, *};
 use crate::{
-    close_layer_fd,
     common::{blocking_send_hook_message, HookMessage},
     detour::{Detour, OnceLockExt, OptionExt},
     dns::GetAddrInfo,
@@ -708,23 +710,24 @@ pub(super) fn getaddrinfo(
 fn remote_hostname_string() -> Detour<CString> {
     let hostname_path = PathBuf::from("/etc/hostname");
 
-    let hostname_fd = file::ops::open(
-        Detour::Success(hostname_path),
+    let OpenFileResponse { fd } = file::ops::RemoteFile::remote_open(
+        hostname_path,
         OpenOptionsInternal {
             read: true,
             ..Default::default()
         },
     )?;
 
-    let hostname_file = file::ops::read(hostname_fd, 256)?;
+    let ReadFileResponse { bytes, read_amount } = file::ops::RemoteFile::remote_read(fd, 256)?;
 
-    close_layer_fd(hostname_fd);
+    let _ = file::ops::RemoteFile::remote_close(fd).inspect_err(|fail| {
+        trace!("Leaking remote file fd (should be harmless) due to {fail:#?}!")
+    });
 
     CString::new(
-        hostname_file
-            .bytes
+        bytes
             .into_iter()
-            .take(hostname_file.read_amount as usize - 1)
+            .take(read_amount as usize - 1)
             .collect::<Vec<_>>(),
     )
     .map(Detour::Success)?
