35fa4fa2e9b3c07cb00bd6f8db67b92a5433ca06
t4lz <t4lz.git@gmail.com>
2023-04-19 17:27:16 +0200

    Support JavaScript on IntelliJ (#1301)
    * Add node, ClassNotFoundException
    
    * no idea
    
    * maybe
    
    * connect webstorm to gradle project.
    
    * Now it's about making the code compile :)
    
    * Tried to use NodeJsRunConfigurationExtension, which can't be found.
    
    * start trying with abstract.
    
    * ><()))>
    
    * rats
    
    * Working!
    
    mirrord is successfuly injected into the process and and runs.
    Now we need to see if overriding the editor creation is a problem
    for the user.
    
    * changelog
    
    * cleanup
    
    * CR and finishes.
    
    * 1.13.3 -> 1.+
    
    * webstorm -> nodejs

---
+++ intellij-ext/modules/products/nodejs/build.gradle.kts
@@ -0,0 +1,27 @@
+fun properties(key: String) = project.findProperty(key).toString()
+
+plugins {
+    // Java support
+    id("java")
+    // Kotlin support
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    // Gradle IntelliJ Plugin
+    id("org.jetbrains.intellij") version "1.+"
+
+}
+
+tasks {
+    buildSearchableOptions {
+        enabled = false
+    }
+}
+
+intellij {
+    type.set("IU")
+    version.set(properties("platformVersion"))
+    plugins.set(listOf("NodeJS"))
+}
+
+dependencies {
+    implementation(project(":mirrord-core"))
+}
