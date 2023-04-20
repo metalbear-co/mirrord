2c4eb9c78a0629bf9fb7bbd8340fe412c2b02166
meowjesty <43983236+meowjesty@users.noreply.github.com>
2023-04-20 18:17:17 -0300

    Magic fix for "connection was not ready". (#1340)
    * Apply Barbara fix to use poll_fn. Add bunch of logs.
    
    * Comments.
    
    * Changelog.

--- mirrord/layer/src/tcp_steal/http/v1.rs
+++ mirrord/layer/src/tcp_steal/http/v1.rs
@@ -1,6 +1,8 @@
 //! [`HttpV1`]
 //!
 //! Handles HTTP/1 requests.
+use std::future;
+
 use bytes::Bytes;
 use http_body_util::Full;
 use hyper::client::conn::http1::{self, Connection, SendRequest};
@@ -22,21 +24,32 @@ impl HttpV for HttpV1 {
 
     type Connection = Connection<TcpStream, Full<Bytes>>;
 
+    #[tracing::instrument(level = "trace")]
     fn new(http_request_sender: Self::Sender) -> Self {
         Self(http_request_sender)
     }
 
+    #[tracing::instrument(level = "trace")]
     async fn handshake(
         target_stream: TcpStream,
     ) -> Result<(Self::Sender, Self::Connection), HttpForwarderError> {
         Ok(http1::handshake(target_stream).await?)
     }
 
+    #[tracing::instrument(level = "trace", skip(self))]
     async fn send_request(
         &mut self,
         request: HttpRequest,
     ) -> hyper::Result<hyper::Response<hyper::body::Incoming>> {
-        self.0.send_request(request.internal_request.into()).await
+        let request_sender = &mut self.0;
+
+        // Solves a "connection was not ready" client error.
+        // https://rust-lang.github.io/wg-async/vision/submitted_stories/status_quo/barbara_tries_unix_socket.html#the-single-magical-line
+        future::poll_fn(|cx| request_sender.poll_ready(cx)).await?;
+
+        request_sender
+            .send_request(request.internal_request.into())
+            .await
     }
 
     fn take_sender(self) -> Self::Sender {
