79c0a2f301a1de884b656d0c68f87b8be4556adb
infiniteregrets <aroram18@mcmaster.ca>
2023-05-05 16:47:24 -0400

    .

--- intellij-ext/modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordConfigDropDown.kt
+++ intellij-ext/modules/core/src/main/kotlin/com/metalbear/mirrord/MirrordConfigDropDown.kt
@@ -1,21 +1,38 @@
 package com.metalbear.mirrord
 
-import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.ex.ComboBoxAction
+import com.intellij.openapi.project.Project
 import com.intellij.psi.search.FilenameIndex
 import javax.swing.JComponent
-import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.psi.search.GlobalSearchScope
 
 class MirrordConfigDropDown : ComboBoxAction() {
 
+    private var configFiles = mutableListOf<String>()
     override fun getActionUpdateThread() = ActionUpdateThread.BGT
     override fun createPopupActionGroup(button: JComponent, dataContext: DataContext): DefaultActionGroup {
         val currentProject = CommonDataKeys.PROJECT.getData(dataContext) ?: throw Error("No project found")
-        var allFiles = FilenameIndex.getAllFilenames(currentProject)
-//        var mirrordConfigFiles = FilenameIndex.getAllFilesByExt(currentProject, "mirrord")
-//        var mirrordConfigFileNames = mirrordConfigFiles.map { it.name }
-        return DefaultActionGroup()
+        configFiles = findPaths(currentProject)
+
+        val actions = configFiles.map { configPath ->
+            object : AnAction(configPath) {
+                override fun actionPerformed(e: AnActionEvent) {
+
+                }
+            }
+        }
+        return DefaultActionGroup().apply { addAll(actions) }
+    }
+
+    override fun update(e: AnActionEvent)  {
+        e.presentation.apply {
+
+        }
+    }
+    private fun findPaths(project: Project): MutableList<String> {
+        var mirrordConfigFiles = FilenameIndex.getAllFilesByExt(project, "json")
+        var mirrordConfigFileNames = mirrordConfigFiles.map { it.path }.filter { it.contains("mirrord") }
+        return mirrordConfigFileNames.toMutableList()
     }
 }
