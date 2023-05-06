acaeb7863fbcb1adea8211ef043599842f49a48e
infiniteregrets <aroram18@mcmaster.ca>
2023-05-06 09:53:56 -0400

    Update dns resolver test

--- tests/node-e2e/outgoing/test_outgoing_traffic_original_address.mjs
+++ tests/node-e2e/outgoing/test_outgoing_traffic_original_address.mjs
@@ -1,23 +1,12 @@
-import { createSocket } from 'dgram';
+import { resolve } from 'dns';
 
-const socket = createSocket('udp4');
+const hostname = 'example.com';
+const qtype = 'A';
 
-const payload = Uint8Array.from(Buffer.from("076d6972726f7264036465760000010001", "hex"));
-
-socket.send(payload, 0, payload.length, 53, '8.8.8.8', (err, bytes) => {
+resolve(hostname, qtype, (err, addresses) => {
   if (err) {
-    console.error(err);
-    process.exit(1);
+    console.error(err);    
   } else {
-    console.log(`Sent ${bytes} bytes to 8.8.8.8`);
+    console.log(`IP addresses for ${hostname}: ${addresses}`);
   }
-});
-
-socket.on('message', (msg, _) => {  
-  console.log(msg);
-  process.exit(0);
-});
-
-socket.on('error', (err) => {
-    process.exit(1);
 });
