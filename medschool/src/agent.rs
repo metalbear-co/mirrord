0db3cc1de46e8348976639c4b71b12fe43a7dcbb
meowjesty <meowjesty_nyan@outlook.com>
2023-05-23 18:04:44 -0300

    Tool to extract documentation into md.

---
+++ medschool/src/agent.rs
@@ -0,0 +1,247 @@
+use std::collections::HashMap;
+
+use mirrord_config_derive::MirrordConfig;
+use schemars::JsonSchema;
+
+use crate::config::source::MirrordConfigSource;
+
+///
+/// Configuration for the mirrord-agent pod that is spawned in the Kubernetes cluster.
+///
+/// We provide sane defaults for this option, so you don't have to set up anything here.
+///
+/// ```json
+/// {
+///   "agent": {
+///     "log_level": "info",
+///     "namespace": "default",
+///     "image": "ghcr.io/metalbear-co/mirrord:latest",
+///     "image_pull_policy": "IfNotPresent",
+///     "image_pull_secrets": [ { "secret-key": "secret" } ],
+///     "ttl": 30,
+///     "ephemeral": false,
+///     "communication_timeout": 30,
+///     "startup_timeout": 360,
+///     "network_interface": "eth0",
+///     "pause": false,
+///     "flush_connections": false,
+///   }
+/// }
+/// ```
+#[derive(MirrordConfig, Clone, Debug)]
+#[config(map_to = "AgentFileConfig", derive = "JsonSchema")]
+#[cfg_attr(test, config(derive = "PartialEq, Eq"))]
+pub struct AgentConfig {
+    /// ### agent.log_level {#agent-log_level}
+    ///
+    /// Log level for the agent.
+    ///
+    ///
+    /// Supports `"trace"`, `"debug"`, `"info"`, `"warn"`, `"error"`, or any string that would work
+    /// with `RUST_LOG`.
+    ///
+    /// ```json
+    /// {
+    ///   "agent": {
+    ///     "log_level": "mirrord=debug,warn"
+    ///   }
+    /// }
+    /// ```
+    #[config(env = "MIRRORD_AGENT_RUST_LOG", default = "info")]
+    pub log_level: String,
+
+    /// ### agent.namespace {#agent-namespace}
+    ///
+    /// Namespace where the agent shall live.
+    ///
+    /// Defaults to the current kubernetes namespace.
+    #[config(env = "MIRRORD_AGENT_NAMESPACE")]
+    pub namespace: Option<String>,
+
+    /// ### agent.image {#agent-image}
+    ///
+    /// Name of the agent's docker image.
+    ///
+    /// Useful when a custom build of mirrord-agent is required, or when using an internal
+    /// registry.
+    ///
+    /// Defaults to the latest stable image `"ghcr.io/metalbear-co/mirrord:latest"`.
+    ///
+    /// ```json
+    /// {
+    ///   "agent": {
+    ///     "image": "internal.repo/images/mirrord:latest"
+    ///   }
+    /// }
+    /// ```
+    #[config(env = "MIRRORD_AGENT_IMAGE")]
+    pub image: Option<String>,
+
+    /// ### agent.image_pull_policy {#agent-image_pull_policy}
+    ///
+    /// Controls when a new agent image is downloaded.
+    ///
+    /// Supports `"IfNotPresent"`, `"Always"`, `"Never"`, or any valid kubernetes
+    /// [image pull policy](https://kubernetes.io/docs/concepts/containers/images/#image-pull-policy)
+    ///
+    /// Defaults to `"IfNotPresent"`
+    #[config(env = "MIRRORD_AGENT_IMAGE_PULL_POLICY", default = "IfNotPresent")]
+    pub image_pull_policy: String,
+
+    /// ### agent.image_pull_secrets {#agent-image_pull_secrets}
+    ///
+    /// List of secrets the agent pod has access to.
+    ///
+    /// Takes an array of hash with the format `{ name: <secret-name> }`.
+    ///
+    /// Read more [here](https://kubernetes.io/docs/concepts/containers/images/).
+    ///
+    /// ```json
+    /// {
+    ///   "agent": {
+    ///     "image_pull_secrets": [
+    ///       { "very-secret": "secret-key" },
+    ///       { "very-secret": "keep-your-secrets" }
+    ///     ]
+    ///   }
+    /// }
+    /// ```
+    pub image_pull_secrets: Option<Vec<HashMap<String, String>>>,
+
+    /// ### agent.ttl {#agent-ttl}
+    ///
+    /// Controls how long the agent pod persists for after the agent exits (in seconds).
+    ///
+    /// Can be useful for collecting logs.
+    ///
+    /// Defaults to `1`.
+    #[config(env = "MIRRORD_AGENT_TTL", default = 1)]
+    pub ttl: u16,
+
+    /// ### agent.ephemeral {#agent-ephemeral}
+    ///
+    /// Runs the agent as an
+    /// [ephemeral container](https://kubernetes.io/docs/concepts/workloads/pods/ephemeral-containers/)
+    ///
+    /// Defaults to `false`.
+    #[config(env = "MIRRORD_EPHEMERAL_CONTAINER", default = false)]
+    pub ephemeral: bool,
+
+    /// ### agent.communication_timeout {#agent-communication_timeout}
+    ///
+    /// Controls how long the agent lives when there are no connections.
+    ///
+    /// Each connection has its own heartbeat mechanism, so even if the local application has no
+    /// messages, the agent stays alive until there are no more heartbeat messages.
+    #[config(env = "MIRRORD_AGENT_COMMUNICATION_TIMEOUT")]
+    pub communication_timeout: Option<u16>,
+
+    /// ### agent.startup_timeout {#agent-startup_timeout}
+    ///
+    /// Controls how long to wait for the agent to finish initialization.
+    ///
+    /// If initialization takes longer than this value, mirrord exits.
+    ///
+    /// Defaults to `60`.
+    #[config(env = "MIRRORD_AGENT_STARTUP_TIMEOUT", default = 60)]
+    pub startup_timeout: u64,
+
+    /// ### agent.network_interface {#agent-network_interface}
+    ///
+    /// Which network interface to use for mirroring.
+    ///
+    /// The default behavior is try to access the internet and use that interface. If that fails
+    /// it uses `eth0`.
+    #[config(env = "MIRRORD_AGENT_NETWORK_INTERFACE")]
+    pub network_interface: Option<String>,
+
+    /// ### agent.pause {#agent-pause}
+    ///
+    /// **Unstable**
+    ///
+    /// Controls target pause feature.
+    ///
+    /// With this feature enabled, the remote container is paused while clients are connected to
+    /// the agent.
+    ///
+    /// Defaults to `false`.
+    #[config(env = "MIRRORD_PAUSE", default = false, unstable)]
+    pub pause: bool,
+
+    /// ### agent.flush_connections {#agent-flush_connections}
+    ///
+    /// Flushes existing connections when starting to steal, might fix issues where connections
+    /// aren't stolen (due to being already established)
+    ///
+    /// Defaults to `true`.
+    // Temporary fix for issue [#1029](https://github.com/metalbear-co/mirrord/issues/1029).
+    #[config(
+        env = "MIRRORD_AGENT_STEALER_FLUSH_CONNECTIONS",
+        default = true,
+        unstable
+    )]
+    pub flush_connections: bool,
+
+    // rustdoc-stripper-ignore-next
+    /// Create an agent that returns an error after accepting the first client. For testing
+    /// purposes. Only supported with job agents (not with ephemeral agents).
+    // rustdoc-stripper-ignore-next-stop
+    #[cfg(all(debug_assertions, not(test)))] // not(test) so that it's not included in the schema json.
+    #[config(env = "MIRRORD_AGENT_TEST_ERROR", default = false, unstable)]
+    pub test_error: bool,
+}
+
+#[cfg(test)]
+#[allow(clippy::too_many_arguments)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+    use crate::{config::MirrordConfig, util::testing::with_env_vars};
+
+    #[rstest]
+    fn default(
+        #[values((None, "info"), (Some("trace"), "trace"))] log_level: (Option<&str>, &str),
+        #[values((None, None), (Some("app"), Some("app")))] namespace: (Option<&str>, Option<&str>),
+        #[values((None, None), (Some("test"), Some("test")))] image: (Option<&str>, Option<&str>),
+        #[values((None, "IfNotPresent"), (Some("Always"), "Always"))] image_pull_policy: (
+            Option<&str>,
+            &str,
+        ),
+        #[values((None, 1), (Some("30"), 30))] ttl: (Option<&str>, u16),
+        #[values((None, false), (Some("true"), true))] ephemeral: (Option<&str>, bool),
+        #[values((None, None), (Some("30"), Some(30)))] communication_timeout: (
+            Option<&str>,
+            Option<u16>,
+        ),
+        #[values((None, 60), (Some("30"), 30))] startup_timeout: (Option<&str>, u64),
+    ) {
+        with_env_vars(
+            vec![
+                ("MIRRORD_AGENT_RUST_LOG", log_level.0),
+                ("MIRRORD_AGENT_NAMESPACE", namespace.0),
+                ("MIRRORD_AGENT_IMAGE", image.0),
+                ("MIRRORD_AGENT_IMAGE_PULL_POLICY", image_pull_policy.0),
+                ("MIRRORD_AGENT_TTL", ttl.0),
+                ("MIRRORD_EPHEMERAL_CONTAINER", ephemeral.0),
+                (
+                    "MIRRORD_AGENT_COMMUNICATION_TIMEOUT",
+                    communication_timeout.0,
+                ),
+                ("MIRRORD_AGENT_STARTUP_TIMEOUT", startup_timeout.0),
+            ],
+            || {
+                let agent = AgentFileConfig::default().generate_config().unwrap();
+
+                assert_eq!(agent.log_level, log_level.1);
+                assert_eq!(agent.namespace.as_deref(), namespace.1);
+                assert_eq!(agent.image.as_deref(), image.1);
+                assert_eq!(agent.image_pull_policy, image_pull_policy.1);
+                assert_eq!(agent.ttl, ttl.1);
+                assert_eq!(agent.ephemeral, ephemeral.1);
+                assert_eq!(agent.communication_timeout, communication_timeout.1);
+                assert_eq!(agent.startup_timeout, startup_timeout.1);
+            },
+        );
+    }
+}
---
+++ medschool/src/main.rs
@@ -0,0 +1,245 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs::File,
+    io::Read,
+};
+
+use syn::{spanned::Spanned, Attribute, Expr, Ident, Type, TypePath};
+use thiserror::Error;
+
+#[derive(Debug)]
+struct PartialType {
+    ident: Ident,
+    docs: Vec<String>,
+    fields: HashSet<PartialField>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct PartialField {
+    ident: Ident,
+    ty: Ident,
+    docs: Vec<String>,
+}
+
+fn get_ident_from_field_skipping_generics(type_path: TypePath) -> Option<Ident> {
+    let span = type_path.span();
+    let mut ignore_idents = HashSet::with_capacity(32);
+    ignore_idents.insert(Ident::new("Option", span));
+    ignore_idents.insert(Ident::new("VecOrSingle", span));
+    ignore_idents.insert(Ident::new("Result", span));
+    ignore_idents.insert(Ident::new("Vec", span));
+    ignore_idents.insert(Ident::new("HashMap", span));
+    ignore_idents.insert(Ident::new("HashSet", span));
+
+    // welp, this path probably contains generics
+    type_path
+        .path
+        .segments
+        .into_iter()
+        // eliminate outer types that we don't want in our docs
+        .filter(|segment| !ignore_idents.contains(&segment.ident))
+        // guarantee that we're done with generics
+        .filter(|segment| segment.arguments.is_empty())
+        .last()
+        .map(|segment| segment.ident)
+}
+
+impl PartialField {
+    fn new(field: syn::Field) -> Option<Self> {
+        let type_ident = match field.ty {
+            Type::Path(type_path) => {
+                // `get_ident` returns `Some` if the `path` doesn't contain generics.
+                type_path
+                    .path
+                    .get_ident()
+                    .cloned()
+                    .or_else(|| get_ident_from_field_skipping_generics(type_path))
+            }
+            _ => None,
+        }?;
+
+        Some(Self {
+            ident: field.ident?,
+            ty: type_ident,
+            docs: docs_from_attributes(field.attrs),
+        })
+    }
+}
+
+/// Very nice comments!
+///
+/// But can it handle multiple comments?
+///
+/// What about multiline comments, that go really far, how does it handle stuff
+/// like this?
+///
+/// ```json
+/// {
+///   "field": "value"
+/// }
+#[derive(Debug, Error)]
+enum DocsError {
+    #[error("Glob error {0}")]
+    Glob(#[from] glob::GlobError),
+
+    #[error("Glob error {0}")]
+    Pattern(#[from] glob::PatternError),
+
+    #[error("Glob error {0}")]
+    IO(#[from] std::io::Error),
+}
+
+fn parse_files() -> Result<Vec<syn::File>, DocsError> {
+    let paths = glob::glob("./src/**/*.rs")?;
+
+    Ok(paths
+        .into_iter()
+        .filter_map(Result::ok)
+        .by_ref()
+        .map(File::open)
+        .filter_map(Result::ok)
+        .map(|mut file| {
+            let mut source = String::with_capacity(30 * 1024);
+            let read_amount = file.read_to_string(&mut source)?;
+            Ok::<_, DocsError>(String::from(&source[..read_amount]))
+        })
+        .filter_map(Result::ok)
+        .map(|raw_contents| syn::parse_file(&raw_contents))
+        .filter_map(Result::ok)
+        .collect::<Vec<_>>())
+}
+
+fn docs_from_attributes(attributes: Vec<Attribute>) -> Vec<String> {
+    attributes
+        .into_iter()
+        // drill into `Meta::NameValue`
+        .filter_map(|attribute| match attribute.meta {
+            syn::Meta::NameValue(meta_doc) => Some(meta_doc),
+            _ => None,
+        })
+        // retrieve only when `PathSegment::Ident` is "doc"
+        .filter_map(|meta_doc| {
+            let ident = meta_doc.path.segments.first()?.ident.clone();
+
+            if ident == Ident::new("doc", ident.span()) {
+                Some(meta_doc.value)
+            } else {
+                None
+            }
+        })
+        // get the doc `Expr::Lit`
+        .filter_map(|docs| match docs {
+            Expr::Lit(doc_expr) => Some(doc_expr.lit),
+            _ => None,
+        })
+        .filter_map(|doc_lit| match doc_lit {
+            syn::Lit::Str(doc_lit) => Some(doc_lit.value()),
+            _ => None,
+        })
+        .collect()
+}
+
+fn main() -> Result<(), DocsError> {
+    // TODO(alex) [high] 2023-05-22: The plan is:
+    //
+    // 1. make a `HashMap<Type, (Field { type, docs }, Docs)>` with all the types;
+    //
+    // Start by making a map of all the types and their docs, we need to include the fields with
+    // their types as well, in order to inline the field-type docs later on.
+    //
+    // 2. search through each Type and check if their Fields belong to another Type;
+    //
+    // This is the pre-inline process, where we look if the Field { type } belongs in another Type.
+    //
+    // 3. insert the Field { docs } in the outer Type Docs;
+    //
+    // Now we inline the docs of Field { docs } in the outer Type Docs.
+
+    let type_docs = parse_files()?
+        .into_iter()
+        .map(|syntaxed_file| {
+            syntaxed_file
+                .items
+                .into_iter()
+                .filter_map(|item| match item {
+                    syn::Item::Mod(item_mod) => {
+                        docs_from_attributes(item_mod.attrs);
+                        None
+                    }
+                    syn::Item::Enum(item) => {
+                        let thing_docs_untreated = docs_from_attributes(item.attrs);
+
+                        (!thing_docs_untreated.is_empty()).then_some(PartialType {
+                            ident: item.ident,
+                            docs: thing_docs_untreated,
+                            fields: HashSet::new(),
+                        })
+                    }
+                    syn::Item::Struct(item) => {
+                        let fields = item
+                            .fields
+                            .into_iter()
+                            .filter_map(PartialField::new)
+                            .collect::<HashSet<_>>();
+
+                        let thing_docs_untreated = docs_from_attributes(item.attrs);
+
+                        (!thing_docs_untreated.is_empty()).then_some(PartialType {
+                            ident: item.ident,
+                            docs: thing_docs_untreated,
+                            fields,
+                        })
+                    }
+                    _ => {
+                        println!("other item");
+                        None
+                    }
+                })
+                .map(|partial_type| (partial_type.ident.clone(), partial_type))
+                .collect::<HashMap<_, _>>()
+        })
+        .collect::<Vec<_>>();
+
+    println!("{type_docs:#?}");
+
+    // TODO(alex) [high] 2023-05-23: What's the best way to represent the hierarchy here?
+    //
+    // Need a way of saying "hey type, are you an inner field of some other type?".
+    let types = type_docs
+        .iter()
+        .map(|type_doc| type_doc.keys().cloned().collect::<Vec<_>>())
+        .collect::<Vec<_>>()
+        .concat();
+
+    for type_doc in type_docs.into_iter().peekable() {
+        for type_key in types.iter() {
+            if let Some(foo) = type_doc
+                .values()
+                .find_map(|partial| partial.fields.iter().find(|field| &field.ident == type_key))
+            {
+                println!("type {type_key:#?} is in {type_doc:#?} \n\n foo {foo:#?}");
+            }
+        }
+    }
+
+    Ok(())
+}
+
+// TODO(alex) [high] 2023-05-19: Each comment lives in it's own `Attribute`, so need a handler that
+// goes through each attribute merging the comments.
+
+/// Very nice comments!
+///
+/// But can it handle multiple comments?
+///
+/// What about multiline comments, that go really far, how does it handle stuff
+/// like this?
+///
+/// ```json
+/// {
+///   "field": "value"
+/// }
+struct Foo {
+    /// Commented field.
+    a: i32,
+}
