5728cd2574a5ae91156218e2b28ce2f7aa9b0eb7
infiniteregrets <aroram18@mcmaster.ca>
2023-05-23 01:43:19 -0400

    pain

--- vscode-ext/src/tests/e2e.ts
+++ vscode-ext/src/tests/e2e.ts
@@ -1,49 +1,82 @@
-import { existsSync, stat } from "fs";
-import { assert }  from "chai"
+import { existsSync } from "fs";
+import { assert, expect } from "chai"
 import { join } from "path";
-import { By, VSBrowser, EditorView, WebView, Workbench, Notification, StatusBar, NotificationType } from "vscode-extension-tester";
+import { VSBrowser, StatusBar, TextEditor, EditorView, ActivityBar, DebugView, InputBox, DebugToolbar } from "vscode-extension-tester";
+import  get  from "axios"
 
 
-describe("mirrord sample flow test", function() {
+// This suite tests basic flow of mirroring traffic from remote pod
+// - Enable mirrord -> Disable mirrord
+// - Create mirrord config by pressing the gear icon
+// - Set a breakpoint in the python file
+// - Start debugging the python file
+// - Select the pod from the QuickPick
+// - Send traffic to the pod
+// - Verify that the breakpoint is hit
+describe("mirrord sample flow test", function () {
     console.log("Initializing workspace");
     this.timeout(10000000000);
-    let browser: VSBrowser;    
-  
-    before(async function() {
+    let browser: VSBrowser;
+
+    before(async function () {
         browser = VSBrowser.instance;
-        await browser.openResources(join(__dirname, '../../test-workspace'));
-        await sleep(3000);
+        await browser.openResources(join(__dirname, '../../test-workspace'), join(__dirname, '../../test-workspace/app_flask.py'));
+        await sleep(10000);
     });
 
-    it("enable mirrord", async function() {        
-        console.log("Enabling mirrord");        
+    it("enable mirrord", async function () {
         const statusBar = new StatusBar();
         const enableButton = await statusBar.getItem("Enable mirrord");
         assert(enableButton !== undefined, "Enable mirrord button not found");
-        await enableButton?.click();                
+        await enableButton?.click();
         assert(await enableButton?.getText() == "Disable mirrord", "Disable mirrord button not found");
     });
 
-    it("create mirrord config", async function() {
+    it("create mirrord config", async function () {
         this.timeout(1000000);
-        console.log("Clicking on the create mirrord settings button");        
         const statusBar = new StatusBar();
+        const mirrordSettingsButton = await statusBar.getItem("gear");
+        assert(mirrordSettingsButton !== undefined, "Mirrord settings button not found");
 
-        const items = await statusBar.getItems();
-        for (const item of items) {
-            console.log(await item.getId());
-            console.log(await item.getAttribute("value"));            
+        await mirrordSettingsButton?.click();
+        await browser.driver.wait(async () => {
+            const mirrordConfigPath = join(__dirname, '../../test-workspace/.mirrord/mirrord.json');
+            return await existsSync(mirrordConfigPath);
         }
-        // const mirrordSettingsButton = await statusBar.getItem("mirrord.changeSettings"); 
-        // console.log("Mirrord settings button: " + mirrordSettingsButton);   
-        // assert(mirrordSettingsButton != undefined, "Mirrord settings button not found");    
-        // await mirrordSettingsButton?.click();        
-        // await browser.driver.wait(async () => {            
-        //     const mirrordConfigPath = join(__dirname, '../../test-workspace/.mirrord/mirrord.json');
-        //     return await existsSync(mirrordConfigPath);
-        // }
-        // , 100000, "Mirrord config not found");
+            , 100000, "Mirrord config not found");
     });
+
+
+    it("set breakpoint", async function () {
+        const editorView = new EditorView();
+        await editorView.openEditor("app_flask.py");
+
+        const textEditor = new TextEditor();
+        const result = await textEditor.toggleBreakpoint(30);
+        expect(result).to.be.true;
+    });
+
+
+    it("start debugging", async function () {
+        const activityBar = await new ActivityBar().getViewControl("Run and Debug");
+        const view = await activityBar?.openView() as DebugView;
+        await view.selectLaunchConfiguration("Python: Current File");
+        view.start();
+    });
+
+    it("select pod from quickpick", async function () {
+        const input = await InputBox.create();
+        const picks = await input.selectQuickPick("pod/py-serv-deployment-ff89b5974-dhl2q")
+        await sleep(10000);
+    });
+
+    it("wait for breakpoint to be hit", async function () {
+        const debugBar = await DebugToolbar.create();
+        console.log("waiting for breakpoint");
+        await debugBar.waitForBreakPoint();
+        // todo: now need to figure how to wait on the breakpoint and send traffic      
+    });
+
 });
 
 
