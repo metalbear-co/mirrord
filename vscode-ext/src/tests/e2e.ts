78a5a2119cd0f68257b3439435286463aee83bc5
infiniteregrets <aroram18@mcmaster.ca>
2023-05-24 23:12:21 -0400

    refactoring

--- vscode-ext/src/tests/e2e.ts
+++ vscode-ext/src/tests/e2e.ts
@@ -1,5 +1,5 @@
 import { existsSync } from "fs";
-import { assert, expect } from "chai";
+import { assert, config, expect } from "chai";
 import { join } from "path";
 import { VSBrowser, StatusBar, TextEditor, EditorView, ActivityBar, DebugView, InputBox, DebugToolbar } from "vscode-extension-tester";
 import get from "axios";
@@ -27,6 +27,8 @@ describe("mirrord sample flow test", function () {
     const testWorkspace = join(__dirname, '../../test-workspace');
     const fileName = "app_flask.py";
     const mirrordConfigPath = join(testWorkspace, '.mirrord/mirrord.json');
+    const configurationFile = "Python: Current File";
+    const breakpointLine = 9;
 
     before(async function () {
         console.log("podToSelect: " + podToSelect);
@@ -36,16 +38,17 @@ describe("mirrord sample flow test", function () {
         browser = VSBrowser.instance;
         // need to bring the flask app in open editors
         await browser.openResources(testWorkspace, join(testWorkspace, fileName));
-        await sleep(10000);
+        await sleep(10000); // --> wait for the IDE to load
     });
 
     after(async function () {
+        // stop debugging
         if (await debugToolbar.isDisplayed()) {
             await debugToolbar.stop();
         }
     });
 
-    it("enable mirrord", async function () {
+    it("Enable mirrord", async function () {
         statusBar = new StatusBar();
         const enableButton = await statusBar.getItem("Enable mirrord");
         expect(enableButton).to.not.be.undefined;
@@ -54,7 +57,7 @@ describe("mirrord sample flow test", function () {
         assert(await enableButton?.getText() === "Disable mirrord", "`Disable mirrord` button not found");
     });
 
-    it("create mirrord config", async function () {
+    it("Create mirrord config", async function () {
         // gear -> $(gear) clicked to open mirrord config
         const mirrordSettingsButton = await statusBar.getItem("gear");
         expect(mirrordSettingsButton).to.not.be.undefined;
@@ -65,19 +68,23 @@ describe("mirrord sample flow test", function () {
             , 10000, "Mirrord config not found");
     });
 
-    it("select pod from quickpick", async function () {
-        await setBreakPoint(fileName);
-        await startDebugging();
-        
+    it("Select pod from quickpick", async function () {
+        await setBreakPoint(fileName, breakpointLine);
+        await startDebugging(configurationFile);
+
         const input = await InputBox.create();
         // assertion that podToSelect is not undefined is done in "before" block   
-        await sleep(5000);
+        await sleep(5000); // --> wait for the quickpick dialog to load
         await input.selectQuickPick(podToSelect!);
     });
 
-    it("wait for breakpoint to be hit", async function () {
-        await sleep(10000);
+    it("Wait for breakpoint to be hit", async function () {
+        await sleep(10000);// --> wait for mirrord to start
         debugToolbar = await DebugToolbar.create();
+
+        // waiting for breakpoint and sending traffic to pod are run in parallel
+        // however, traffic is sent after 10 seconds that we are sure the IDE is listening
+        // for breakpoints
         await Promise.all([debugToolbar.waitForBreakPoint(), sendTrafficToPod()])
     });
 
@@ -98,7 +105,8 @@ async function sendTrafficToPod() {
     expect(response.data).to.equal("OK - GET: Request completed\n");
 }
 
-async function setBreakPoint(fileName: string) {        
+// opens and sets a breakpoint in the given file
+async function setBreakPoint(fileName: string, lineNumber: number) {
     const editorView = new EditorView();
     await editorView.openEditor(fileName);
     await sleep(5000);
@@ -107,15 +115,17 @@ async function setBreakPoint(fileName: string) {
     assert(await currentTab?.getTitle() === fileName, "${fileName} not found");
 
     const textEditor = new TextEditor();
-    const result = await textEditor.toggleBreakpoint(9);
-    expect(result).to.be.true;    
+    const result = await textEditor.toggleBreakpoint(lineNumber);
+    expect(result).to.be.true;
 }
 
-async function startDebugging() {
+// starts debugging the current file with the provided configuration
+// debugging starts from the "Run and Debug" button in the activity bar
+async function startDebugging(configurationFile: string) {
     const activityBar = await new ActivityBar().getViewControl("Run and Debug");
     expect(activityBar).to.not.be.undefined;
     const debugView = await activityBar?.openView() as DebugView;
-    await debugView.selectLaunchConfiguration("Python: Current File");
+    await debugView.selectLaunchConfiguration(configurationFile);
     debugView.start();
     await sleep(10000);
 }
