b94b894425aac1f9a3330fa96d7304fdad3d303d
Eyal Bukchin <eyal@metalbear.co>
2023-05-28 15:52:35 +0300

    Change warning text

--- mirrord/operator/src/client.rs
+++ mirrord/operator/src/client.rs
@@ -65,16 +65,16 @@ impl OperatorApi {
             // propagating an env var, don't think it's worth the extra complexity though
             let mirrord_version = Version::parse(env!("CARGO_PKG_VERSION")).unwrap();
             if operator_version != mirrord_version {
-                progress.subtask("comparing versions").print_message(MessageKind::Warning, Some(&format!("Your mirrord version {} does not match the operator version {}. This can lead to unforeseen issues.", mirrord_version, operator_version)));
+                progress.subtask("comparing versions").print_message(MessageKind::Warning, Some(&format!("Your mirrord plugin/CLI version {} does not match the operator version {}. This can lead to unforeseen issues.", mirrord_version, operator_version)));
                 if operator_version > mirrord_version {
                     progress.subtask("comparing versions").print_message(
                         MessageKind::Warning,
                         Some(
-                            "Consider updating your mirrord version to match the operator version.",
+                            "Consider updating your mirrord plugin/CLI to match the operator version.",
                         ),
                     );
                 } else {
-                    progress.subtask("comparing versions").print_message(MessageKind::Warning, Some("Consider either updating your operator version to match your mirrord version, or downgrading your mirrord version."));
+                    progress.subtask("comparing versions").print_message(MessageKind::Warning, Some("Consider either updating your operator version to match your mirrord plugin/CLI version, or downgrading your mirrord plugin/CLI."));
                 }
             }
             operator_api.connect_target(target).await.map(Some)
