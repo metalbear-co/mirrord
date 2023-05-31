ec6348d6bca9b85ed6d4713411e8c043a82368a1
Aviram Hassan <aviram@metalbear.co>
2023-05-31 12:22:47 +0300

    Add `/Volumes` to default local on macOS

--- mirrord/layer/src/file/filter.rs
+++ mirrord/layer/src/file/filter.rs
@@ -59,9 +59,6 @@ fn generate_local_set() -> RegexSet {
         r"^/dev/.*$",
         r"^/opt/.*$",
         r"^/home/.*$",
-        // don't use `/.*$` so lstat on the directory will also be local
-        r"^/private(/|$)",
-        r"^/var/folders(/|$)",
         r"^/tmp(/|$)",
         r"^/snap/.*$",
         // support for nixOS.
@@ -83,10 +80,21 @@ fn generate_local_set() -> RegexSet {
         // asdf
         r".*/\.tool-versions$",
         // macOS
+        #[cfg(target_os = "macos")]
+        "^/Volumes(/|$)",
+        #[cfg(target_os = "macos")]
+        r"^/private(/|$)",
+        #[cfg(target_os = "macos")]
+        r"^/var/folders(/|$)",
+        #[cfg(target_os = "macos")]
         r"^/Users",
+        #[cfg(target_os = "macos")]
         r"^/Library",
+        #[cfg(target_os = "macos")]
         r"^/Applications",
+        #[cfg(target_os = "macos")]
         r"^/System",
+        #[cfg(target_os = "macos")]
         r"^/var/run/com.apple",
         &format!("^{}", temp_dir.to_string_lossy()),
         &format!("^.*{}.*$", current_dir.to_string_lossy()),
