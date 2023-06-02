3af22277acbedc8ecec79e22f83a2472810959db
Michał Smolarek <34063647+Razz4780@users.noreply.github.com>
2023-06-02 19:31:55 +0200

    Rider support added (#1439)
    * Rider support added
    
    * Changelog entry
    
    * Idea version bumped
    
    * Disabled missing dependencies check
    
    * Gradle version bump

--- intellij-ext/settings.gradle.kts
+++ intellij-ext/settings.gradle.kts
@@ -8,6 +8,7 @@ include (
     "modules/products/pycharm",
     "modules/products/rubymine",
     "modules/products/nodejs",
+    "modules/products/rider",
 )
 
 // Rename modules to mirrord-<module>, I think this is required IntelliJ wise.
