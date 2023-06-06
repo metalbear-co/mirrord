a6d0aa478c20835718a254f94636335b97161fc6
infiniteregrets <aroram18@mcmaster.ca>
2023-06-06 01:48:18 -0400

    .

--- mirrord/layer/tests/recv_from.rs
+++ mirrord/layer/tests/recv_from.rs
@@ -1,11 +1,19 @@
 #![feature(assert_matches)]
 #![warn(clippy::indexing_slicing)]
 
+use std::{net::SocketAddr, path::PathBuf, time::Duration};
+
+use futures::{SinkExt, TryStreamExt};
+use mirrord_protocol::{
+    outgoing::{
+        udp::{DaemonUdpOutgoing, LayerUdpOutgoing},
+        DaemonConnect, DaemonRead, LayerConnect, LayerWrite, SocketAddress,
+    },
+    ClientMessage, DaemonMessage,
+};
 use rstest::rstest;
 
 mod common;
-
-use std::{path::PathBuf, time::Duration};
 
 pub use common::*;
 
@@ -16,11 +24,41 @@ async fn recv_from(
     #[values(Application::RustRecvFrom)] application: Application,
     dylib_path: &PathBuf,
 ) {
-    let (mut test_process, mut layer_connection) = application
+    let (mut test_process, layer_connection) = application
         .start_process_with_layer(dylib_path, vec![], None)
         .await;
 
-    assert!(layer_connection.is_ended().await);
+    let mut conn = layer_connection.codec;
+    let msg = conn.try_next().await.unwrap().unwrap();
+
+    let ClientMessage::UdpOutgoing(LayerUdpOutgoing::Connect(LayerConnect { remote_address: SocketAddress::Ip(addr) })) = msg else {
+        panic!("Invalid message received from layer: {msg:?}");
+    };
+    conn.send(DaemonMessage::UdpOutgoing(DaemonUdpOutgoing::Connect(Ok(
+        DaemonConnect {
+            connection_id: 0,
+            remote_address: addr.into(),
+            local_address: RUST_OUTGOING_LOCAL.parse::<SocketAddr>().unwrap().into(),
+        },
+    ))))
+    .await
+    .unwrap();
+
+    let msg = conn.try_next().await.unwrap().unwrap();
+
+    let ClientMessage::UdpOutgoing(LayerUdpOutgoing::Write(LayerWrite { connection_id: 0, bytes: _ })) = msg else {
+        panic!("Invalid message received from layer: {msg:?}");
+    };
+
+    conn.send(DaemonMessage::UdpOutgoing(DaemonUdpOutgoing::Read(Ok(
+        DaemonRead {
+            connection_id: 0,
+            bytes: vec![],
+        },
+    ))))
+    .await
+    .unwrap();
+
     test_process.wait_assert_success().await;
     test_process.assert_no_error_in_stderr();
     test_process.assert_no_error_in_stdout();
