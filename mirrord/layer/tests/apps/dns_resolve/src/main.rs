b8a39c0691a4fe3277a46c32a1805f58125b6941
infiniteregrets <aroram18@mcmaster.ca>
2023-06-07 14:53:15 -0400

    .

--- mirrord/layer/tests/apps/dns_resolve/src/main.rs
+++ mirrord/layer/tests/apps/dns_resolve/src/main.rs
@@ -1,45 +1,12 @@
-use std::{ffi::CString, ptr, str};
-
-fn get_ip_addresses(node: &str) -> (Vec<String>, i32) {
-    let node = CString::new(node).unwrap();
-    let service = ptr::null_mut::<libc::c_char>();
-
-    let hints: libc::addrinfo = unsafe { std::mem::zeroed() };
-
-    let mut res: *mut libc::addrinfo = ptr::null_mut();
-    let result = unsafe { libc::getaddrinfo(node.as_ptr(), service, &hints, &mut res) };
-    let mut current = res;
-    let mut addresses = Vec::new();
-    while !current.is_null() {
-        unsafe {
-            let sockaddr = (*current).ai_addr;
-            let sockaddr_in = sockaddr as *const libc::sockaddr_in;
-            let ip = (*sockaddr_in).sin_addr.s_addr;
-
-            let ipv4_addr = std::net::Ipv4Addr::from(ip);
-            let addr_str = ipv4_addr.to_string();
-
-            addresses.push(addr_str);
-        }
-        current = (unsafe { *current }).ai_next;
-    }
-    unsafe { libc::freeaddrinfo(res) };
-    (addresses, result)
-}
-
-fn test_getaddrinfo() {
-    let domain_x = "example.com";
-    let domain_y = "nonexistent-domain";
-
-    let (addresses_x, result_x) = get_ip_addresses(domain_x);
-    assert!(!addresses_x.is_empty(), "Expected addresses for domain X");
-    assert!(result_x == 0, "Expected success for domain X");
-
-    let (addresses_y, result_y) = get_ip_addresses(domain_y);
-    assert!(addresses_y.is_empty(), "Expected no addresses for domain Y");
-    assert!(result_y == libc::EAI_FAIL, "Expected failure for domain Y");
-}
+use std::net::ToSocketAddrs;
 
 fn main() {
-    test_getaddrinfo();
+    let domain_x_addrs = ("github.com", 0).to_socket_addrs();
+    assert!(domain_x_addrs.is_ok());
+
+    let domain_y_addrs = ("nonexistent-domain", 0).to_socket_addrs();
+    assert_eq!(
+        domain_y_addrs.unwrap_err().to_string(),
+        "failed to lookup address information: Non-recoverable failure in name resolution"
+    );
 }
