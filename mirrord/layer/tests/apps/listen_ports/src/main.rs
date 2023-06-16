9204080aed3dde883a1b088cb2138fda13e4ad73
Aviram Hassan <aviram@metalbear.co>
2023-06-16 23:23:34 +0300

    wtf

--- mirrord/layer/tests/apps/listen_ports/src/main.rs
+++ mirrord/layer/tests/apps/listen_ports/src/main.rs
@@ -1,14 +1,14 @@
 use std::net::{SocketAddr, TcpListener};
 
 fn main() {
-    let addr: SocketAddr = "127.0.0.1.80".parse().unwrap();
+    let addr: SocketAddr = "127.0.0.1:80".parse().unwrap();
     // Check `listen_ports` work
     let listener = TcpListener::bind(addr).unwrap();
     // verify user could connect on port 51222
     listener.accept().unwrap();
 
     // Check specific port but not in listen_ports works
-    let addr: SocketAddr = "127.0.0.1.40000".parse().unwrap();
+    let addr: SocketAddr = "127.0.0.1:40000".parse().unwrap();
     let listener = TcpListener::bind(addr).unwrap();
     // verify user could connect on port 40000
     listener.accept().unwrap();
--- mirrord/layer/tests/listen_ports.rs
+++ mirrord/layer/tests/listen_ports.rs
@@ -1,14 +1,19 @@
-#![cfg(target_os = "linux")]
 #![feature(assert_matches)]
 #![warn(clippy::indexing_slicing)]
 
-use std::{path::PathBuf, time::Duration};
+use std::{assert_matches::assert_matches, path::PathBuf, time::Duration};
 
+use futures::StreamExt;
+use mirrord_protocol::{
+    tcp::{LayerTcpSteal, StealType},
+    ClientMessage,
+};
 use rstest::rstest;
 
 mod common;
 
 pub use common::*;
+use tokio::net::TcpStream;
 
 /// Start an application (and load the layer into it) that listens on a port that is configured to
 /// be ignored, and verify that no messages are sent to the agent.
@@ -16,7 +21,7 @@ pub use common::*;
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 #[timeout(Duration::from_secs(60))]
 async fn listen_ports(
-    #[values(Application::PythonListen)] application: Application,
+    #[values(Application::RustListenPorts)] application: Application,
     dylib_path: &PathBuf,
     config_dir: &PathBuf,
 ) {
@@ -32,15 +37,18 @@ async fn listen_ports(
 
     assert_matches!(
         layer_connection.codec.next().await.unwrap().unwrap(),
-        ClientMessage::Tcp(LayerTcp::PortSubscribe(80))
+        ClientMessage::TcpSteal(LayerTcpSteal::PortSubscribe(StealType::All(80)))
     );
+
+    TcpStream::connect("127.0.0.1:51222").await.unwrap();
 
     assert_matches!(
         layer_connection.codec.next().await.unwrap().unwrap(),
-        ClientMessage::Tcp(LayerTcp::PortSubscribe(40000))
+        ClientMessage::TcpSteal(LayerTcpSteal::PortSubscribe(StealType::All(40000)))
     );
 
-    // Make sure no listen request was made.
+    TcpStream::connect("127.0.0.1:40000").await.unwrap();
+
     assert!(layer_connection.is_ended().await);
     test_process.wait_assert_success().await;
     test_process.assert_no_error_in_stderr();
