fe9f79f5a3ac5a6ad44e4d2dc7767354bae3707f
Aviram Hassan <aviram@metalbear.co>
2023-07-06 10:48:05 +0300

    wait for child

--- mirrord/layer/tests/apps/fork/fork.c
+++ mirrord/layer/tests/apps/fork/fork.c
@@ -5,8 +5,10 @@
 /// This program forks, and the child process calls `socket`.
 /// It is used to verify that hooks are handled in a child socket after a fork.
 int main() {
-    if (!fork()) {
+    pid_t pid = fork();
+    if (!pid) {
         const char path[] = "/path/to/some/file";
         open(path, 0);
     }
+    waitpid(pid, NULL, 0);
 }
