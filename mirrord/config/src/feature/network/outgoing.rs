156c1c3b371327d19bf7ea0456229c8647312e3c
meowjesty <meowjesty_nyan@outlook.com>
2023-07-13 12:52:04 -0300

    Remove outdated file.

--- mirrord/config/src/feature/network/outgoing.rs
+++ mirrord/config/src/feature/network/outgoing.rs
@@ -7,16 +7,50 @@ use schemars::JsonSchema;
 use serde::Deserialize;
 use thiserror::Error;
 
-// use self::filter::{OutgoingFilterConfig, OutgoingFilterFileConfig};
 use crate::{
     config::{from_env::FromEnv, source::MirrordConfigSource, ConfigError},
     util::{MirrordToggleableConfig, VecOrSingle},
 };
 
-// pub mod filter;
-
-/// <--!{$internal}-->
-/// The 3 possible variants for the outgoing traffic filter.
+/// List of addresses/ports/subnets that should be sent through either the remote pod or local app,
+/// depending how you set this up with either `remote` or `local`.
+///
+/// You may use this option to specify when outgoing traffic is sent from the remote pod (which
+/// is the default behavior when you enable outgoing traffic), or from the local app (default when
+/// you have outgoing traffic disabled).
+///
+/// Takes a list of values, such as:
+///
+/// - Only UDP traffic on subnet `1.1.1.0/24` on port 1337 will go throuh the remote pod.
+///
+/// ```json
+/// {
+///   "remote": ["udp://1.1.1.0/24:1337"]
+/// }
+///
+/// - Only UDP and TCP traffic on resolved address of `google.com` on port `1337` and `7331`
+/// will go through the remote pod.
+/// ```json
+/// {
+///   "remote": ["google.com:1337", "google.com:7331"]
+/// }
+/// ```
+/// 
+/// - Only TCP traffic on `localhost` on port 1337 will go through the local app, the rest will
+///   be emmited remotely in the cluster.
+/// ```json
+/// {
+///   "local": ["tcp://localhost:1337"]
+/// }
+///
+/// - Only outgoing traffic on port `1337` and `7331` will go through the local app.
+/// ```json
+/// {
+///   "local": [":1337", ":7331"]
+/// }
+/// ```
+///
+/// Valid values follow this pattern: `[protocol]://[name|address|subnet/mask]:[port]`.
 #[derive(Deserialize, PartialEq, Eq, Clone, Debug, JsonSchema)]
 #[serde(untagged, rename_all = "lowercase")]
 pub enum OutgoingFilterConfig {
