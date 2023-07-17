467e16dec502438ddb68b7fd8b22e4ceac0231fc
Aviram Hassan <aviram@metalbear.co>
2023-07-17 13:17:45 +0300

    ..

--- mirrord/layer/src/error.rs
+++ mirrord/layer/src/error.rs
@@ -159,9 +159,6 @@ pub(crate) enum LayerError {
     )]
     NewConnectionAfterSocketClose(ConnectionId),
 
-    #[error("mirrord-layer: Unmatched pong!")]
-    UnmatchedPong,
-
     #[error("mirrord-layer: JSON convert error")]
     JSONConvertError(#[from] serde_json::Error),
 
