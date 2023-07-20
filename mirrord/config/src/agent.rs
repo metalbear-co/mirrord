cbe952c02116a8c24466bc6b29b1c23664247808
Dmitry Dodzin <d.dodzin@gmail.com>
2023-07-20 14:15:10 +0300

    Update

--- mirrord/config/src/agent.rs
+++ mirrord/config/src/agent.rs
@@ -201,7 +201,7 @@ pub struct AgentConfig {
 
     /// ### agent.tolerations {#agent-tolerations}
     ///
-    /// Add extra pod tolerations. (not with ephemeral agents)
+    /// Set pod tolerations. (not with ephemeral agents)
     pub tolerations: Option<Vec<Toleration>>,
 
     /// <!--${internal}-->
