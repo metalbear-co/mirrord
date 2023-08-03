bd7538623680b83c7a1ac4f9effb6a1eb1b1017a
Dmitry Dodzin <d.dodzin@gmail.com>
2023-08-03 18:45:27 +0300

    Backfill

--- mirrord/protocol/src/features.rs
+++ mirrord/protocol/src/features.rs
@@ -1,12 +1,29 @@
-use std::cell::RefCell;
+use std::{cell::RefCell, fmt, str::FromStr};
 
 use bitflags::bitflags;
+use bitflags_serde_shim::impl_serde_for_bitflags;
 use serde::{Deserialize, Serialize};
 
 bitflags! {
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
     pub struct Features: u32 {
         const KeepHttpFrames = 0b00000001;
+    }
+}
+
+impl_serde_for_bitflags!(Features);
+
+impl fmt::Display for Features {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Display::fmt(&self.0, f)
+    }
+}
+
+impl FromStr for Features {
+    type Err = bitflags::parser::ParseError;
+
+    fn from_str(flags: &str) -> Result<Self, Self::Err> {
+        Ok(Self(flags.parse()?))
     }
 }
 
