19087f786d2fe78098c3416d2c1ee20ae4b4fd41
Aviram Hassan <aviram@metalbear.co>
2023-08-09 12:49:48 +0300

    wip

--- mirrord/progress/src/lib.rs
+++ mirrord/progress/src/lib.rs
@@ -9,84 +9,200 @@ use std::{
     time::Duration,
 };
 
+use enum_dispatch::enum_dispatch;
+use indicatif::ProgressBar;
 use serde::Serialize;
 use serde_json::to_string;
-use indicatif::ProgressBar;
 
 /// The environment variable name that is used
 /// to determine the mode of progress reporting
 pub const MIRRORD_PROGRESS_ENV: &str = "MIRRORD_PROGRESS_MODE";
 
+#[enum_dispatch]
+pub trait Progress: Sized {
+    /// Create a subtask report from this task.
+    fn subtask(&self, text: &str) -> Self;
+
+    /// When task is done successfully
+    fn success(&mut self, msg: Option<&str>);
+
+    /// When task is done with failure
+    fn failure(&mut self, msg: Option<&str>);
+
+    /// When you want to issue a warning on current task
+    fn warning(&self, msg: &str);
+
+    /// When you want to print a message, cli only.
+    fn print(&self, msg: &str);
+
+    /// Control if drop without calling succes is considered failure.
+    fn set_fail_on_drop(&mut self, fail: bool);
+}
+
 /// `ProgressMode` specifies the way progress is reported
 /// by [`TaskProgress`].
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum ProgressMode {
-    /// Display dynamic progress with spinners.
-    Standard(ProgressBar),
+#[derive(Debug)]
+#[enum_dispatch(Progress)]
+pub enum ProgressTracker {
+    // /// Display dynamic progress with spinners.
+    // Standard(ProgressBar),
     /// Display simple human-readable messages in new lines.
-    Simple,
-    /// Output progress messages in JSON format for programmatic use.
-    Json,
+    SimpleProgress(SimpleProgress),
+    // /// Output progress messages in JSON format for programmatic use.
+    JsonProgress(JsonProgress),
     /// Do not output progress.
-    Off,
+    NullProgress(NullProgress),
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum MessageKind {
-    Success,
-    Failure,
-    Warning,
-    CliOnly,
+#[derive(Debug)]
+pub struct NullProgress;
+
+impl Progress for NullProgress {
+    fn subtask(&self, _: &str) -> NullProgress {
+        NullProgress
+    }
+
+    fn set_fail_on_drop(&mut self, _: bool) {}
+
+    fn success(&mut self, _: Option<&str>) {}
+
+    fn failure(&mut self, _: Option<&str>) {}
+
+    fn warning(&self, _: &str) {}
+
+    fn print(&self, _: &str) {}
 }
 
-/// Initialize progress reporting based on the [`MIRRORD_PROGRESS_ENV`]
-/// environment variable.
-///
-/// If the variable does not exist, the provided fallback
-/// progress mode is used.
-///
-/// This is automatically called with the default
-/// progress mode whenever a [`TaskProgress`] is created.
-pub fn init_from_env(fallback: ProgressMode) {
-    PROGRESS.get_or_init(|| ProgressReport::from_env(fallback));
+#[derive(Debug)]
+pub struct JsonProgress {
+    parent: Option<String>,
+    name: String,
+    done: bool,
+    fail_on_drop: bool,
 }
 
-static PROGRESS: OnceLock<ProgressReport> = OnceLock::new();
+impl JsonProgress {
+    fn new(text: &str) -> JsonProgress {
+        let progress = JsonProgress {
+            parent: None,
+            name: text.to_string(),
+            done: false,
+            fail_on_drop: true,
+        };
+        progress.print_new_task();
+        progress
+    }
 
-fn get_report() -> &'static ProgressReport {
-    PROGRESS.get_or_init(|| {
-        ProgressReport::from_env(if atty::is(atty::Stream::Stdout) {
-            ProgressMode::Standard(ProgressBar::new_spinner())
-        } else {
-            ProgressMode::Simple
-        })
-    })
+    fn print_new_task(&self) {
+        let message = ProgressMessage::NewTask(NewTaskMessage {
+            name: self.name.clone(),
+            parent: self.parent.clone(),
+        });
+        message.print();
+    }
+
+    fn print_finished_task(&self, success: bool, msg: Option<&str>) {
+        let message = ProgressMessage::FinishedTask(FinishedTaskMessage {
+            name: self.name.clone(),
+            message: msg.map(|s| s.to_string()),
+            success,
+        });
+        message.print();
+    }
 }
 
-/// Global progress state.
-struct ProgressReport {
-    mode: ProgressMode,
+impl Progress for JsonProgress {
+    fn subtask(&self, text: &str) -> JsonProgress {
+        let task = JsonProgress {
+            parent: Some(self.name.clone()),
+            name: text.to_string(),
+            done: false,
+            fail_on_drop: false,
+        };
+        task.print_new_task();
+        task
+    }
+
+    fn print(&self, _: &str) {}
+
+    fn warning(&self, msg: &str) {
+        let message = ProgressMessage::Warning(WarningMessage {
+            message: msg.to_string(),
+        });
+        message.print();
+    }
+    fn failure(&mut self, msg: Option<&str>) {
+        self.done = true;
+        self.print_finished_task(false, msg)
+    }
+
+    fn success(&mut self, msg: Option<&str>) {
+        self.done = true;
+        self.print_finished_task(true, msg)
+    }
+
+    fn set_fail_on_drop(&mut self, fail: bool) {
+        self.fail_on_drop = fail;
+    }
 }
 
-impl ProgressReport {
-    fn new(mode: ProgressMode) -> Self {
-        Self {
-            mode,
+impl Drop for JsonProgress {
+    fn drop(&mut self) {
+        if !self.done {
+            if self.fail_on_drop {
+                self.failure(None);
+            } else {
+                self.success(None);
+            }
         }
     }
+}
 
-    fn from_env(fallback: ProgressMode) -> Self {
-        Self::new(match std::env::var(MIRRORD_PROGRESS_ENV).as_deref() {
-            Ok("std" | "standard") => ProgressMode::Standard(ProgressBar::new_spinner()),
-            Ok("dumb" | "simple") => ProgressMode::Simple,
-            Ok("json") => ProgressMode::Json,
-            Ok("off") => ProgressMode::Off,
-            _ => fallback,
-        })
+#[derive(Debug)]
+pub struct SimpleProgress;
+
+impl SimpleProgress {
+    fn new(text: &str) -> SimpleProgress {
+        println!("{text}");
+        SimpleProgress {}
     }
 }
 
-static TASK_COUNT: AtomicUsize = AtomicUsize::new(0);
+impl Progress for SimpleProgress {
+    fn subtask(&self, text: &str) -> SimpleProgress {
+        println!("{text}");
+        SimpleProgress {}
+    }
+
+    fn print(&self, text: &str) {
+        println!("{text}");
+    }
+
+    fn warning(&self, msg: &str) {
+        println!("{msg}");
+    }
+    fn failure(&mut self, msg: Option<&str>) {
+        println!("{msg:?}");
+    }
+
+    fn success(&mut self, msg: Option<&str>) {
+        println!("{msg:?}");
+    }
+
+    fn set_fail_on_drop(&mut self, _: bool) {}
+}
+
+impl ProgressTracker {
+    pub fn from_env(fallback: ProgressTracker, text: &str) -> Self {
+        match std::env::var(MIRRORD_PROGRESS_ENV).as_deref() {
+            // Ok("std" | "standard") => ProgressMode::Standard(ProgressBar::new_spinner()),
+            Ok("dumb" | "simple") => SimpleProgress::new(text).into(),
+            Ok("json") => JsonProgress::new(text).into(),
+            Ok("off") => NullProgress.into(),
+            _ => fallback,
+        }
+    }
+}
 
 /// Message sent when a new task is created using subtask/new
 #[derive(Serialize, Debug, Clone, Default)]
@@ -126,229 +242,5 @@ enum ProgressMessage {
 impl ProgressMessage {
     pub(crate) fn print(&self) {
         println!("{}", to_string(self).unwrap());
-    }
-}
-
-pub trait Progress: Sized {
-    /// Create a subtask report from this task.
-    fn subtask(&self, text: &str) -> Self;
-
-    fn done(mut self) {
-        self.set_done(None, false);
-    }
-
-    /// Finish the task with the given message.
-    fn done_with(mut self, text: &str) {
-        self.set_done(Some(text), false);
-    }
-
-    /// Fail the task without changing the description.
-    fn fail(mut self) {
-        self.set_done(None, true);
-    }
-
-    /// Fail the task with the given message.
-    fn fail_with(mut self, text: &str) {
-        self.set_done(Some(text), true);
-    }
-
-    fn set_done(&mut self, msg: Option<&str>, failure: bool);
-    fn print_message(&self, kind: MessageKind, msg: Option<&str>);
-}
-
-pub struct NoProgress;
-
-impl Progress for NoProgress {
-    fn subtask(&self, _: &str) -> NoProgress {
-        NoProgress
-    }
-
-    fn set_done(&mut self, _: Option<&str>, _: bool) {}
-    fn print_message(&self, _: MessageKind, _: Option<&str>) {}
-}
-
-/// Progress report for a single task.
-pub struct TaskProgress {
-    group: SharedFrames<Group>,
-    line: SharedFrames<Line>,
-    indent: usize,
-    done: bool,
-    fail_on_drop: bool,
-    name: String,
-}
-
-impl TaskProgress {
-    /// Report the progress of a new task with the given description.
-    ///
-    /// By default the task is considered failed if it goes out of
-    /// scope without `done` being called.
-    pub fn new(text: &str) -> Self {
-        let report = get_report();
-
-        let mut group = Group::new();
-        let line = Line::new(dots()).with_text(text).shared();
-
-        group.push(line.clone());
-
-        let group = group.shared();
-
-        match report.mode {
-            ProgressMode::Standard => {
-                report.spinner_loop.inner().lock().push(group.clone());
-                report.spinner_loop.spawn_stream(stdout());
-            }
-            ProgressMode::Simple => {
-                println!("{text}");
-            }
-            ProgressMode::Json => {
-                let message = ProgressMessage::NewTask(NewTaskMessage {
-                    name: text.to_string(),
-                    ..Default::default()
-                });
-                message.print();
-            }
-            ProgressMode::Off => {}
-        }
-
-        TASK_COUNT.fetch_add(1, Ordering::Relaxed);
-
-        Self {
-            group,
-            line,
-            indent: 0,
-            done: false,
-            fail_on_drop: true,
-            name: text.to_string(),
-        }
-    }
-
-    /// If set to `true` (default), the task is considered failed
-    /// if it gets dropped.
-    ///
-    /// Set this to `false` if the task cannot fail, or failure
-    /// is not important.
-    pub fn fail_on_drop(mut self, fail: bool) -> Self {
-        self.fail_on_drop = fail;
-        self
-    }
-}
-
-impl Progress for TaskProgress {
-    /// Create a subtask report from this task.
-    fn subtask(&self, text: &str) -> TaskProgress {
-        let report = get_report();
-        let sub_indent = self.indent + 1;
-
-        let mut group = Group::new().with_indent(sub_indent);
-        let line = Line::new(dots()).with_text(text).shared();
-
-        group.push(line.clone());
-
-        let group = group.shared();
-
-        self.group.lock().push(group.clone());
-
-        match report.mode {
-            ProgressMode::Standard | ProgressMode::Off => {}
-            ProgressMode::Simple => {
-                println!("{text}");
-            }
-            ProgressMode::Json => {
-                let message = ProgressMessage::NewTask(NewTaskMessage {
-                    name: text.to_string(),
-                    parent: Some(self.name.clone()),
-                });
-                message.print();
-            }
-        }
-
-        TASK_COUNT.fetch_add(1, Ordering::Relaxed);
-
-        TaskProgress {
-            group,
-            line,
-            indent: sub_indent,
-            done: false,
-            fail_on_drop: true,
-            name: text.to_string(),
-        }
-    }
-
-    fn set_done(&mut self, msg: Option<&str>, failure: bool) {
-        self.done = true;
-        let kind = if failure {
-            MessageKind::Failure
-        } else {
-            MessageKind::Success
-        };
-        self.print_message(kind, msg);
-    }
-    fn print_message(&self, kind: MessageKind, msg: Option<&str>) {
-        let report = get_report();
-
-        match report.mode {
-            ProgressMode::Standard => {
-                let mut line = self.line.lock();
-
-                line.set_spinner_visible(false);
-
-                let marker = match kind {
-                    MessageKind::Success => '✓',
-                    MessageKind::Failure => 'x',
-                    MessageKind::Warning => '!',
-                    MessageKind::CliOnly => '#',
-                };
-
-                if let Some(msg) = msg {
-                    line.set_text(&format!("{marker} {msg}"));
-                } else {
-                    let msg = format!("{marker} {}", line.text());
-                    line.set_text(&msg);
-                }
-            }
-            ProgressMode::Simple => {
-                if let Some(msg) = msg {
-                    println!("{msg}");
-                }
-            }
-            ProgressMode::Json => {
-                match kind {
-                    MessageKind::Warning => {
-                        let message = ProgressMessage::Warning(WarningMessage {
-                            message: msg.unwrap().to_string(),
-                        });
-                        message.print();
-                    }
-                    MessageKind::Success | MessageKind::Failure => {
-                        let message = ProgressMessage::FinishedTask(FinishedTaskMessage {
-                            name: self.name.clone(),
-                            success: kind == MessageKind::Success,
-                            message: msg.map(|s| s.to_string()),
-                        });
-                        message.print();
-                    }
-                    // ignore cli only messages
-                    MessageKind::CliOnly => {}
-                }
-            }
-            ProgressMode::Off => {}
-        }
-    }
-}
-
-impl Drop for TaskProgress {
-    fn drop(&mut self) {
-        let count = TASK_COUNT.fetch_sub(1, Ordering::Relaxed);
-
-        if !self.done {
-            self.set_done(None, self.fail_on_drop);
-        }
-
-        if count == 1 {
-            let report = get_report();
-            report.spinner_loop.stop();
-
-            report.spinner_loop.inner().lock().retain(|_| false);
-        }
     }
 }
