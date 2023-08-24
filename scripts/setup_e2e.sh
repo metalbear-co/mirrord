cbfdd7da3978bd68e85aa1f64dcb7e87dd0ee351
Aviram Hassan <aviram@metalbear.co>
2023-08-24 12:26:10 +0300

    lets go

--- scripts/setup_e2e.sh
+++ scripts/setup_e2e.sh
@@ -1,43 +1,16 @@
 #!/bin/bash
-
-sudo apt-get update && sudo apt-get install -y curl
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo sh -s -- -y
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
-
-nvm install 14
-nvm use 14
-
-npm install express
-
-pip3 install flask
-pip3 install fastapi
-pip3 install uvicorn[standard]
-
-sudo apt-get install bison
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-
-source /home/runner/.gvm/scripts/gvm
-
-gvm install go1.18
 gvm use go1.18
 
 go version
 
 bash ./scripts/build_go_apps.sh 18
 
-gvm install go1.19
 gvm use go1.19
 
 go version
 
 bash ./scripts/build_go_apps.sh 19
 
-gvm install go1.20
 gvm use go1.20
 
 go version
