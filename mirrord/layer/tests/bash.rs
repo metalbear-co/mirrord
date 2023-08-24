f7a308fff78d3ccdc6417c26c2adbdab869aab86
Aviram Hassan <aviram@metalbear.co>
2023-08-24 22:57:22 +0300

    ..

--- mirrord/layer/tests/bash.rs
+++ mirrord/layer/tests/bash.rs
@@ -1,7 +1,10 @@
 #![feature(assert_matches)]
 #![warn(clippy::indexing_slicing)]
 
-use std::{path::{Path, PathBuf}, time::Duration};
+use std::{
+    path::{Path, PathBuf},
+    time::Duration,
+};
 
 #[cfg(not(target_os = "macos"))]
 use futures::SinkExt;
