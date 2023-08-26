af6b975f31a9517526e1be98a6ae345e6968bb4a
Aviram Hassan <aviram@metalbear.co>
2023-08-26 13:11:36 +0300

    ..

--- mirrord/sip/src/codesign.rs
+++ mirrord/sip/src/codesign.rs
@@ -1,13 +1,15 @@
-use std::{os::unix::process::ExitStatusExt, path::Path, process::Command};
+use std::{collections::HashMap, os::unix::process::ExitStatusExt, path::Path, process::Command};
 
 use crate::error::{Result, SipError};
 
 pub(crate) fn sign<P: AsRef<Path>>(path: P) -> Result<()> {
+    // don't load mirrord into the codesign binary
     let output = Command::new("codesign")
         .arg("-s") // sign with identity
         .arg("-") // adhoc identity
         .arg("-f") // force (might have a signature already)
         .arg(path.as_ref())
+        .env_remove("DYLD_INSERT_LIBRARIES")
         .output()?;
     if output.status.success() {
         Ok(())
