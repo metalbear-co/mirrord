eca6a897238d55d1f69b0e87c5fe603a6771d6b0
meowjesty <meowjesty_nyan@outlook.com>
2023-09-11 17:35:12 -0300

    more docs for ci // increase timeout

--- tests/src/issue1317.rs
+++ tests/src/issue1317.rs
@@ -99,8 +99,10 @@ mod issue1317 {
         assert!(String::from_utf8_lossy(&body.to_bytes()[..]).contains("Echo [remote]"));
 
         process
-            .wait_for_line(Duration::from_secs(10), "Echo [local]: GET 2")
+            .wait_for_line(Duration::from_secs(30), "Echo [local]: GET 2")
             .await;
+
+        drop(request_sender);
 
         timeout(Duration::from_secs(40), process.wait())
             .await
