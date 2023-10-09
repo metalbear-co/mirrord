0a2ba1223c409bc0cb2333863da88b22b6f98a52
Razz4780 <msmolarekg@gmail.com>
2023-10-09 12:15:25 +0200

    PingPong docs

--- mirrord/intproxy/src/ping_pong.rs
+++ mirrord/intproxy/src/ping_pong.rs
@@ -1,3 +1,10 @@
+//! Ping pong mechanism implementation on the internal proxy side.
+//! This mechanism exists to keep the `proxy <-> agent` connection alive when there are no requests
+//! from the layer.
+//!
+//! Realized using the [`DaemonMessage::Pong`](mirrord_protocol::codec::DaemonMessage::Pong) and
+//! [`ClientMessage::Ping`] messages.
+
 use std::time::Duration;
 
 use mirrord_protocol::ClientMessage;
@@ -9,29 +16,39 @@ use crate::{
     ProxyMessage,
 };
 
+/// Errors that can occur when handling ping pong.
 #[derive(Error, Debug)]
 pub enum PingPongError {
+    /// Agent sent pong but the proxy was not expecting one.
     #[error("received an unexpected pong from the agent")]
     UnmatchedPong,
+    /// Agent did not send ping in time.
     #[error("agent did not respond to ping in time")]
     PongTimeout,
-    #[error("a ping message was sent while already waiting for a pong")]
-    DuplicatePing,
 }
 
+/// Notification about a message received from the agent.
 pub struct AgentMessageNotification {
+    /// Whether the message was
+    /// [`DaemonMessage::Pong`](mirrord_protocol::codec::DaemonMessage::Pong).
     pub pong: bool,
 }
 
-/// Handles ping pong mechanism on the proxy side.
-/// This mechanism exists to keep the `proxy <-> agent` connection alive
-/// when there are no requests from the layer.
+/// Encapsulates logic of the ping pong mechanism on the proxy side.
+/// Run as a [`BackgroundTask`] by each [`ProxySession`](crate::session::ProxySession).
 pub struct PingPong {
+    /// How long can the `proxy <-> agent` connection remain silent.
     ticker: Interval,
+    /// Whether this struct awaits for a pong from the agent.
     awaiting_pong: bool,
 }
 
 impl PingPong {
+    /// Creates a new instance of this struct.
+    ///
+    /// # Arguments
+    ///
+    /// * frequency - how long can the `proxy <-> agent` connection remain silent
     pub fn new(frequency: Duration) -> Self {
         let mut ticker = time::interval(frequency);
         ticker.set_missed_tick_behavior(MissedTickBehavior::Skip);
@@ -53,8 +70,10 @@ impl BackgroundTask for PingPong {
             tokio::select! {
                 _ = self.ticker.tick() => {
                     if self.awaiting_pong {
+                        tracing::error!("pong timeout");
                         break Err(PingPongError::PongTimeout);
                     } else {
+                        tracing::trace!("sending ping");
                         let _ = message_bus.send(ProxyMessage::ToAgent(ClientMessage::Ping)).await;
                         self.ticker.reset();
                         self.awaiting_pong = true;
@@ -62,13 +81,22 @@ impl BackgroundTask for PingPong {
                 },
 
                 msg = message_bus.recv() => match (msg, self.awaiting_pong) {
-                    (None, _) => break Ok(()),
+                    (None, _) => {
+                        tracing::trace!("no more messages, exiting");
+                        break Ok(())
+                    },
                     (Some(AgentMessageNotification { pong: true }), true) => {
+                        tracing::trace!("agent responded to ping");
                         self.awaiting_pong = false;
                         self.ticker.reset();
                     },
-                    (Some(AgentMessageNotification { pong: false }), true) => {},
-                    (Some(AgentMessageNotification { pong: true }), false) => break Err(PingPongError::UnmatchedPong),
+                    (Some(AgentMessageNotification { pong: false }), true) => {
+                        tracing::trace!("agent sent message, still waiting for pong")
+                    },
+                    (Some(AgentMessageNotification { pong: true }), false) => {
+                        tracing::error!("agent sent an unexpected pong");
+                        break Err(PingPongError::UnmatchedPong)
+                    },
                     (Some(AgentMessageNotification { pong: false }), false) => {
                         self.ticker.reset();
                     }
