dc704334ae695b4e81c9439d85489fbb3bf7c5aa
Razz4780 <msmolarekg@gmail.com>
2023-10-09 13:54:31 +0200

    agent_conn docs

--- mirrord/intproxy/src/agent_conn.rs
+++ mirrord/intproxy/src/agent_conn.rs
@@ -1,3 +1,6 @@
+//! Implementation of `proxy <-> agent` connection through [`mpsc`](tokio::sync::mpsc) channels
+//! created in different mirrord crates.
+
 use std::io;
 
 use mirrord_config::LayerConfig;
@@ -22,18 +25,24 @@ use crate::{
     ProxyMessage,
 };
 
+/// Errors that can occur when the internal proxy tries to establish a connection with the agent.
 #[derive(Error, Debug)]
 pub enum AgentConnectionError {
+    /// IO failed.
     #[error("{0}")]
     Io(#[from] io::Error),
+    /// mirrord operator API failed.
     #[error("{0}")]
     Operator(#[from] OperatorApiError),
+    /// mirrord kube API failed.
     #[error("{0}")]
     Kube(#[from] KubeApiError),
+    /// The proxy failed to find a connection method in the provided [LayerConfig].
     #[error("invalid configuration, could not find method for connection")]
     NoConnectionMethod,
 }
 
+/// Directive for the proxy on how to connect to the agent.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum AgentConnectInfo {
     /// Connect to the agent through the operator.
@@ -42,12 +51,23 @@ pub enum AgentConnectInfo {
     DirectKubernetes(AgentKubernetesConnectInfo),
 }
 
+/// Handles logic of the `proxy <-> agent` connection.
+/// Run as a [`BackgroundTask`] by each [`ProxySession`](crate::session::ProxySession).
+///
+/// # Note
+/// The raw IO is managed in a separate [`tokio::task`] created in a different mirrord crate.
+/// This differs from the [`LayerConnection`](crate::layer_conn::LayerConnection) implementation,
+/// but this logic was already implemented elsewhere. This struct simply wraps the
+/// [`mpsc`](tokio::sync::mpsc) channels returned from other functions and implements the
+/// [`BackgroundTask`] trait.
 pub struct AgentConnection {
     agent_tx: Sender<ClientMessage>,
     agent_rx: Receiver<DaemonMessage>,
 }
 
 impl AgentConnection {
+    /// Creates a new agent connection based on the provided [`LayerConfig`] and optional
+    /// [`AgentConnectInfo`].
     pub async fn new(
         config: &LayerConfig,
         connect_info: Option<&AgentConnectInfo>,
@@ -84,6 +104,8 @@ impl AgentConnection {
     }
 }
 
+/// This error occurs when the [`AgentConnection`] fails to communicate with the inner
+/// [`tokio::task`], which handles raw IO. The original (e.g. some IO error) is not available.
 #[derive(Error, Debug)]
 #[error("agent unexpectedly closed connection")]
 pub struct AgentClosedConnection;
@@ -97,12 +119,23 @@ impl BackgroundTask for AgentConnection {
         loop {
             tokio::select! {
                 msg = message_bus.recv() => match msg {
-                    None => break Ok(()),
-                    Some(msg) => self.agent_tx.send(msg).await.map_err(|_| AgentClosedConnection)?,
+                    None => {
+                        tracing::trace!("no more messages from the agent, exiting");
+                        break Ok(());
+                    },
+                    Some(msg) => {
+                        if self.agent_tx.send(msg).await.is_err() {
+                            tracing::error!("failed to send message to the agent, inner task down");
+                            break Err(AgentClosedConnection);
+                        }
+                    }
                 },
 
                 msg = self.agent_rx.recv() => match msg {
-                    None => break Err(AgentClosedConnection),
+                    None => {
+                        tracing::error!("failed to receive message from the agent, inner task down");
+                        break Err(AgentClosedConnection);
+                    }
                     Some(msg) => message_bus.send(ProxyMessage::FromAgent(msg)).await,
                 }
             }
