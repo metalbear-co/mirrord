9bb08e01fe362b425adb5d5b00b9770281795627
Razz4780 <msmolarekg@gmail.com>
2023-10-31 15:44:14 +0100

    Removed unnecessary agent changes

--- mirrord/agent/tests/blackbox.rs
+++ mirrord/agent/tests/blackbox.rs
@@ -26,7 +26,7 @@ mod tests {
             .arg("2")
             .arg("-i")
             .arg("lo")
-            .arg("blackbox")
+            .arg("blackbox-test")
             .spawn()
             .expect("mirrord-agent failed to start");
         // Wait for agent to listen
