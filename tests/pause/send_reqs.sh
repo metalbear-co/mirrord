07e472413374cc9d905fbb15f6a3554428309e8f
Aviram Hassan <aviram@metalbear.co>
2024-02-13 09:00:37 +0200

    remove pause tests (#2229)
    * remove pause tests
    
    * changelog

--- tests/pause/send_reqs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-curl mirrord-tests-http-logger/log/hi-from-local-app
-sleep 1 # if the deployed app was not paused, it will send requests while we're sleeping.
-curl mirrord-tests-http-logger/log/hi-again-from-local-app
