80cfd7618adfb7be1de95fd2a58d1d4e6a6913eb
Aviram Hassan <aviram@metalbear.co>
2024-03-28 13:03:17 +0200

    use proxy set empty

--- mirrord/cli/src/util.rs
+++ mirrord/cli/src/util.rs
@@ -3,7 +3,8 @@ pub(crate) fn remove_proxy_env() {
     for (key, _val) in std::env::vars() {
         let lower_key = key.to_lowercase();
         if lower_key == "http_proxy" || lower_key == "https_proxy" {
-            std::env::remove_var(key)
+            // we set instead of unset since this way extension will be able to propogate it as well.
+            std::env::set_var(key, "")
         }
     }
 }
