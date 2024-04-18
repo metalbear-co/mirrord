08ed76de88581b257e7ead914e752613f9ca6d4d
Razz4780 <msmolarekg@gmail.com>
2024-04-18 10:20:11 +0200

    Format

--- mirrord/kube/src/api.rs
+++ mirrord/kube/src/api.rs
@@ -21,45 +21,48 @@ pub fn wrap_raw_connection(
     let (in_tx, mut in_rx) = mpsc::channel(CONNECTION_CHANNEL_SIZE);
     let (out_tx, out_rx) = mpsc::channel(CONNECTION_CHANNEL_SIZE);
 
-    tokio::spawn(async move {
-        // Generally, this loop should not be exited early with any `return` statement.
-        // We want the `close` below to happen.
-        loop {
-            tokio::select! {
-                msg = in_rx.recv() => match msg {
-                    Some(msg) => {
-                        if let Err(error) = codec.send(msg).await {
-                            tracing::error!(?error, "Failed to send client message");
+    tokio::spawn(
+        async move {
+            // Generally, this loop should not be exited early with any `return` statement.
+            // We want the `close` below to happen.
+            loop {
+                tokio::select! {
+                    msg = in_rx.recv() => match msg {
+                        Some(msg) => {
+                            if let Err(error) = codec.send(msg).await {
+                                tracing::error!(?error, "Failed to send client message");
+                                break;
+                            }
+                        }
+                        None => {
+                            tracing::trace!("No more client messages, disconnecting");
                             break;
                         }
-                    }
-                    None => {
-                        tracing::trace!("No more client messages, disconnecting");
-                        break;
-                    }
-                },
+                    },
 
-                msg = codec.next() => match msg {
-                    Some(Ok(msg)) => {
-                        if let Err(error) = out_tx.send(msg).await {
-                            tracing::error!(?error, "Failed to send agent message");
+                    msg = codec.next() => match msg {
+                        Some(Ok(msg)) => {
+                            if let Err(error) = out_tx.send(msg).await {
+                                tracing::error!(?error, "Failed to send agent message");
+                                break;
+                            }
+                        }
+                        Some(Err(error)) => {
+                            tracing::error!(?error, "Failed to receive agent message");
                             break;
                         }
-                    }
-                    Some(Err(error)) => {
-                        tracing::error!(?error, "Failed to receive agent message");
-                        break;
-                    }
-                    None => {
-                        tracing::trace!("No more agent messages, disconnecting");
-                        break;
-                    }
-                },
+                        None => {
+                            tracing::trace!("No more agent messages, disconnecting");
+                            break;
+                        }
+                    },
+                }
             }
-        }
 
-        let _ = codec.close().await;
-    }.in_current_span());
+            let _ = codec.close().await;
+        }
+        .in_current_span(),
+    );
 
     (in_tx, out_rx)
 }
