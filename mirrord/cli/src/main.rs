4cad1711f35a4430b29122a27fd49d7662401293
t4lz <t4lz.git@gmail.com>
2024-06-02 00:35:00 +0200

    display ports on start when filter set

--- mirrord/cli/src/main.rs
+++ mirrord/cli/src/main.rs
@@ -202,6 +202,56 @@ fn print_config<P>(
         incoming_info
     ));
 
+    // When the http filter is set, the rules of what ports get stolen are different, so make it
+    // clear to users in that case which ports are stolen.
+    if config.feature.network.incoming.is_steal()
+        && config.feature.network.incoming.http_filter.is_filter_set()
+    {
+        let filtered_ports_str = config
+            .feature
+            .network
+            .incoming
+            .http_filter
+            .get_filtered_ports()
+            .and_then(|filtered_ports| match filtered_ports.len() {
+                0 => None,
+                #[allow(clippy::indexing_slicing)]
+                1 => Some(format!("port {} (filtered)", filtered_ports[0])),
+                _ => Some(format!("ports {filtered_ports:?} (filtered)")),
+            });
+        let unfiltered_ports_str =
+            config
+                .feature
+                .network
+                .incoming
+                .ports
+                .as_ref()
+                .and_then(|ports| match ports.len() {
+                    0 => None,
+                    1 => Some(format!(
+                        "port {} (unfiltered)",
+                        ports.iter().next().unwrap()
+                    )),
+                    _ => Some(format!(
+                        "ports [{}] (unfiltered)",
+                        ports
+                            .iter()
+                            .copied()
+                            .map(|n| n.to_string())
+                            .collect::<Vec<String>>()
+                            .join(", ")
+                    )),
+                });
+        let and = if filtered_ports_str.is_some() && unfiltered_ports_str.is_some() {
+            " and "
+        } else {
+            ""
+        };
+        let filtered_port_str = filtered_ports_str.unwrap_or_default();
+        let unfiltered_ports_str = unfiltered_ports_str.unwrap_or_default();
+        messages.push(format!("incoming: traffic will only be stolen from {filtered_port_str}{and}{unfiltered_ports_str}"));
+    }
+
     let outgoing_info = match (
         config.feature.network.outgoing.tcp,
         config.feature.network.outgoing.udp,
