0779e993588f1c0e6d8dc524cfc61c1d12c0fb43
meowjesty <meowjesty_nyan@outlook.com>
2024-06-10 17:00:39 -0300

    docs and schema

--- mirrord/config/src/target.rs
+++ mirrord/config/src/target.rs
@@ -186,6 +186,7 @@ mirrord-layer failed to parse the provided target!
     >> deploy/<deployment-name>[/container/container-name]
     >> pod/<pod-name>[/container/container-name]
     >> job/<job-name>[/container/container-name]
+    >> cronjob/<cronjob-name>[/container/container-name]
 
 - Note:
     >> specifying container name is optional, defaults to the first container in the provided pod/deployment target.
@@ -209,6 +210,7 @@ mirrord-layer failed to parse the provided target!
 /// - `container/{sample-container}`;
 /// - `containername/{sample-container}`.
 /// - `job/{sample-job}`;
+/// - `cronjob/{sample-job}`;
 #[derive(Serialize, Deserialize, Clone, Eq, PartialEq, Hash, Debug, JsonSchema)]
 #[serde(untagged, deny_unknown_fields)]
 pub enum Target {
