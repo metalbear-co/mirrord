865ada7b9fde45915c12edec2cb00b3c7467337f
Aviram Hassan <aviram@metalbear.co>
2024-06-10 13:24:29 +0300

    ..

--- mirrord/agent/src/entrypoint.rs
+++ mirrord/agent/src/entrypoint.rs
@@ -449,7 +449,7 @@ impl ClientConnectionHandler {
                 .await?;
             }
             ClientMessage::SwitchProtocolVersion(client_version) => {
-                let settled_version = &client_version.min(&(*mirrord_protocol::VERSION));
+                let settled_version = (&client_version.min(&(*mirrord_protocol::VERSION))).clone();
                 if let Some(tcp_stealer_api) = self.tcp_stealer_api.as_mut() {
                     tcp_stealer_api
                         .switch_protocol_version(settled_version.clone())
