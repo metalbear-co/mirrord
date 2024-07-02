40a7ddeadb2c61630f06a071d4e2cc4eb90db18b
Gemma Tipper <gemmat@metalbear.co>
2024-07-02 14:37:05 +0100

    Remove PathBuf from Detour::Bypass variant, replace with PathBuf length

--- mirrord/layer/src/file/hooks.rs
+++ mirrord/layer/src/file/hooks.rs
@@ -62,9 +62,8 @@ fn update_ptr_from_bypass(ptr: *const c_char, bypass: Bypass) -> *const c_char {
         // inside mirrord's temp bin dir. The detour has returned us the original path of the file
         // (stripped mirrord's dir path), so now we carry out the operation locally, on the stripped
         // path.
-        Bypass::FileOperationInMirrordBinTempDir((prefix_len, path_buf)) => {
+        Bypass::FileOperationInMirrordBinTempDir((prefix_len, _)) => {
             // add length of path to strip to ptr to obtain new pointer
-            dbg!("{:?}", path_buf.clone());
             ptr.wrapping_add(prefix_len)
         }
         _ => ptr,
