67d840eaf5f5fc86a5b275a455579684676579a9
Michał Smolarek <34063647+Razz4780@users.noreply.github.com>
2024-07-08 17:30:59 +0200

    Key pair compat (#2577)
    * Yay
    
    * More doc
    
    * Cert deserialize test
    
    * Changelog entry
    
    * Doc fix + clippy
    
    * Cert request test
    
    * unwrap -> expect

--- mirrord/auth/src/error.rs
+++ mirrord/auth/src/error.rs
@@ -1,47 +1,26 @@
 use thiserror::Error;
 use x509_certificate::X509CertificateError;
 
-/// Wrapper error for errors in mirrord-auth library
-#[derive(Debug, Error)]
-pub enum AuthenticationError {
-    /// Error from parsing `pem` wrapped certificate/key-pair
-    #[error(transparent)]
-    Pem(std::io::Error),
-
-    /// Error from from generating sha256 fingerprint for certificate/key-pair
-    #[error(transparent)]
-    Fingerprint(std::io::Error),
-
-    /// Error from `x509_certificate` library
-    #[error(transparent)]
-    X509Certificate(#[from] X509CertificateError),
-
-    #[cfg(feature = "client")]
-    #[error(transparent)]
-    CertificateStore(#[from] CertificateStoreError),
-
-    #[cfg(feature = "client")]
-    #[error(transparent)]
-    Kube(#[from] kube::Error),
-
-    /// Failed to generate key pair
-    #[error("Failed to generate key pair")]
-    KeyGenerationError,
-}
-
-/// Error from CredentialStore operations
+/// Errors from [`CredentialStore`](crate::credential_store::CredentialStore) and
+/// [`CredentialStoreSync`](crate::credential_store::CredentialStoreSync) operations
 #[cfg(feature = "client")]
 #[derive(Debug, Error)]
-pub enum CertificateStoreError {
-    #[error("Unable to save/load CertificateStore: {0}")]
-    Io(#[from] std::io::Error),
+pub enum CredentialStoreError {
+    #[error("failed to parent directory for credential store file: {0}")]
+    ParentDir(std::io::Error),
 
-    #[error("Unable to create CertificateStore lockfile: {0}")]
+    #[error("IO on credential store file failed: {0}")]
+    FileAccess(std::io::Error),
+
+    #[error("failed to lock/unlock credential store file: {0}")]
     Lockfile(std::io::Error),
 
-    #[error("Unable serialize/deserialize CertificateStore: {0}")]
+    #[error("failed to serialize/deserialize credentials: {0}")]
     Yaml(#[from] serde_yaml::Error),
-}
 
-/// `Result` with `AuthenticationError` as default error
-pub type Result<T, E = AuthenticationError> = std::result::Result<T, E>;
+    #[error("x509 certificate error: {0}")]
+    X509Certificate(#[from] X509CertificateError),
+
+    #[error("certification request failed: {0}")]
+    Kube(#[from] kube::Error),
+}
