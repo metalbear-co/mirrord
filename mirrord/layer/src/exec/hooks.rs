6ece064d232e13430c244ecec41216c0be43adb8
meowjesty <meowjesty_nyan@outlook.com>
2024-07-17 14:26:34 -0300

    changes

--- mirrord/layer/src/exec/hooks.rs
+++ mirrord/layer/src/exec/hooks.rs
@@ -1,8 +1,13 @@
-use std::{ffi::CString, ops::Not, os::unix::process::parent_id};
+use std::{
+    ffi::{CStr, CString},
+    ops::Not,
+    os::unix::process::parent_id,
+};
 
 use base64::prelude::*;
 use libc::{c_char, c_int, FD_CLOEXEC};
 use mirrord_layer_macro::hook_guard_fn;
+use null_terminated::Nul;
 use tracing::Level;
 
 use super::Argv;
@@ -21,10 +26,11 @@ fn shared_sockets() -> Vec<(i32, UserSocket)> {
     SOCKETS
         .iter()
         .filter_map(|inner| {
-            let is_cloexec = unsafe { FN_FCNTL(*inner.key(), libc::F_GETFD) } & FD_CLOEXEC > 0;
-            is_cloexec
-                .not()
-                .then(|| (*inner.key(), UserSocket::clone(inner.value())))
+            // let is_cloexec = unsafe { FN_FCNTL(*inner.key(), libc::F_GETFD) } & FD_CLOEXEC > 0;
+            // is_cloexec
+            //     .not()
+            //     .then(|| (*inner.key(), UserSocket::clone(inner.value())))
+            Some((*inner.key(), UserSocket::clone(inner.value())))
         })
         .collect::<Vec<_>>()
 }
@@ -55,6 +61,63 @@ unsafe extern "C" fn execv_detour(path: *const c_char, argv: *const *const c_cha
     }
 }
 
+#[mirrord_layer_macro::instrument(level = Level::INFO, skip(rawish_envp), ret)]
+pub(super) fn old_execve(rawish_envp: &Nul<*const c_char>) -> Detour<Argv> {
+    let shared_sockets = SOCKETS
+        .iter()
+        .filter_map(|inner| {
+            let cloned = UserSocket::clone(inner.value());
+            Some((*inner.key(), cloned))
+        })
+        .collect::<Vec<_>>();
+
+    let encoded = bincode::encode_to_vec(shared_sockets, bincode::config::standard())
+        .map(|bytes| BASE64_URL_SAFE.encode(bytes))?;
+
+    let mut env_vars = rawish_envp
+        .iter()
+        .take_while(|raw_env| !(*raw_env).is_null())
+        .map(|raw_env_var| unsafe { CStr::from_ptr(*raw_env_var) }.to_owned())
+        .collect::<Argv>();
+
+    env_vars.push(CString::new(format!("MIRRORD_SHARED_SOCKETS={}", encoded))?);
+
+    Detour::Success(env_vars)
+}
+
+#[cfg(not(target_os = "macos"))]
+#[hook_guard_fn]
+pub(crate) unsafe extern "C" fn execve_detour(
+    path: *const c_char,
+    argv: *const *const c_char,
+    envp: *const *const c_char,
+) -> c_int {
+    use null_terminated::Nul;
+
+    tracing::info!("execve called");
+    // if let Detour::Success(env_vars) = execve() {
+    //     FN_EXECVE(path, argv, env_vars.null_vec().as_ptr() as *const *const _)
+    // } else {
+    //     FN_EXECVE(path, argv, envp)
+    // }
+
+    // match execve() {
+    //     Detour::Success(new_envp) => {
+    //         FN_EXECVE(path, argv, new_envp.null_vec().as_ptr() as *const *const _)
+    //     }
+    //     _ => FN_EXECVE(path, argv, envp),
+    // }
+
+    match old_execve(Nul::new_unchecked(envp)) {
+        Detour::Success(new_envp) => {
+            FN_EXECVE(path, argv, new_envp.null_vec().as_ptr() as *const *const _)
+        }
+        _ => FN_EXECVE(path, argv, envp),
+    }
+
+    // FN_EXECVE(path, argv, envp)
+}
+
 #[cfg(target_os = "macos")]
 #[hook_guard_fn]
 unsafe extern "C" fn execv_detour(path: *const c_char, argv: *const *const c_char) -> c_int {
@@ -77,21 +140,6 @@ unsafe extern "C" fn execv_detour(path: *const c_char, argv: *const *const c_cha
         }
     } else {
         FN_EXECV(path, argv)
-    }
-}
-
-#[cfg(not(target_os = "macos"))]
-#[hook_guard_fn]
-pub(crate) unsafe extern "C" fn execve_detour(
-    path: *const c_char,
-    argv: *const *const c_char,
-    envp: *const *const c_char,
-) -> c_int {
-    tracing::info!("execve called");
-    if let Detour::Success(env_vars) = execve() {
-        FN_EXECVE(path, argv, env_vars.null_vec().as_ptr() as *const *const _)
-    } else {
-        FN_EXECVE(path, argv, envp)
     }
 }
 
