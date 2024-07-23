8319b3875e491a33a1d38ba6b4ef05f73a6b3aba
meowjesty <meowjesty_nyan@outlook.com>
2024-07-23 16:44:13 -0300

    docs

--- mirrord/layer/src/exec_hooks.rs
+++ mirrord/layer/src/exec_hooks.rs
@@ -10,18 +10,25 @@ pub(crate) mod hooks;
 pub(crate) struct Argv(Vec<CString>);
 
 impl Argv {
+    /// Turns this list of [`CString`] into a C list of pointers (null-terminated).
+    ///
+    /// We leak the [`CString`]s, so that they may live in C-land.
     pub(crate) fn leak(self) -> *const *const c_char {
+        // Leaks the strings.
         let mut list = self
             .0
             .into_iter()
             .map(|value| value.into_raw().cast_const())
             .collect::<Vec<_>>();
 
+        // Null-terminated.
         list.push(ptr::null());
 
+        // Leaks the list itself.
         list.into_raw_parts().0.cast_const()
     }
 
+    /// Convenience to [`Vec::push`] a new [`CString`].
     pub(crate) fn push(&mut self, item: CString) {
         self.0.push(item);
     }
