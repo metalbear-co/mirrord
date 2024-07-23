fcb836732b42f639c861c2e7e9ca7cd35e47a56e
meowjesty <meowjesty_nyan@outlook.com>
2024-07-23 18:15:49 -0300

    fix docs

--- mirrord/layer/src/exec_hooks/hooks.rs
+++ mirrord/layer/src/exec_hooks/hooks.rs
@@ -76,17 +76,20 @@ unsafe extern "C" fn execv_detour(path: *const c_char, argv: *const *const c_cha
 /// - #[cfg(target_os = "macos")]
 ///
 /// We change 3 arguments and then call the original functions:
+///
 /// 1. The executable path - we check it for SIP, create a patched binary and use the path to the
-///    new path instead of the original path. If there is no SIP, we use a new string with the same
-///    path.
-/// 2. argv - we strip mirrord's temporary directory from the start of arguments. So if argv[1] is
-///    "/var/folders/1337/mirrord-bin/opt/homebrew/bin/npx" Switch it to "/opt/homebrew/bin/npx"
-///    Also here we create a new array with pointers to new strings, even if there are no changes
-///    needed (except for the case of an error).
+/// new path instead of the original path. If there is no SIP, we use a new string with the same
+/// path.
+/// 2. argv - we strip mirrord's temporary directory from the start of arguments.
+/// So if `argv[1]` is "/var/folders/1337/mirrord-bin/opt/homebrew/bin/npx", switch it
+/// to "/opt/homebrew/bin/npx". Also here we create a new array with pointers
+/// to new strings, even if there are no changes needed (except for the case of an error).
 /// 3. envp - We found out that Turbopack (Vercel) spawns a clean "Node" instance without env,
-///    basically stripping all of the important mirrord env.
-///    https://github.com/metalbear-co/mirrord/issues/2500
-///    We restore the `DYLD_INSERT_LIBRARIES` environment variable and all env vars starting with `MIRRORD_` if the dyld var can't be found in `envp`.
+/// basically stripping all of the important mirrord env.
+/// https://github.com/metalbear-co/mirrord/issues/2500
+/// We restore the `DYLD_INSERT_LIBRARIES` environment variable and all env vars
+/// starting with `MIRRORD_` if the dyld var can't be found in `envp`.
+///
 /// If there is an error in the detour, we don't exit or anything, we just call the original libc
 /// function with the original passed arguments.
 #[hook_guard_fn]
