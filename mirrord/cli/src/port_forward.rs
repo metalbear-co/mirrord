82c11d332aab988176793fa9353f997b203e9ba8
Gemma Tipper <gemmat@metalbear.co>
2024-08-06 14:15:14 +0100

    Add BiMap for connection IDs and sockets, restructure message type matching

--- mirrord/cli/src/port_forward.rs
+++ mirrord/cli/src/port_forward.rs
@@ -1,6 +1,13 @@
 use std::{collections::HashMap, net::SocketAddr};
 
-use mirrord_protocol::DaemonMessage;
+use futures::future::Either;
+use mirrord_protocol::{
+    outgoing::{
+        tcp::{DaemonTcpOutgoing, LayerTcpOutgoing},
+        LayerClose, LayerConnect, SocketAddress,
+    },
+    ClientMessage, ConnectionId, DaemonMessage, LogLevel, CLIENT_READY_FOR_LOGS,
+};
 use thiserror::Error;
 use tokio::{
     net::{
@@ -24,6 +31,8 @@ pub struct PortForwarder {
     rx_connections: StreamMap<SocketAddr, OwnedReadHalf>,
     // the writing half of the stream received by a listener for sending data back to the user app
     tx_connections: HashMap<SocketAddr, OwnedWriteHalf>,
+    // bijective map for storing connection_ids and sockets
+    sockets_ids_map: BiMap,
 }
 
 impl PortForwarder {
@@ -56,33 +65,96 @@ impl PortForwarder {
             listeners,
             rx_connections: StreamMap::new(),
             tx_connections: HashMap::new(),
+            sockets_ids_map: BiMap {
+                connection_ids: HashMap::new(),
+                sockets: HashMap::new(),
+            },
         })
     }
 
     pub(crate) async fn run(&mut self) -> Result<(), PortForwardError> {
+        // setup agent connection
+        let Ok(_) = self
+            .agent_connection
+            .sender
+            .send(ClientMessage::SwitchProtocolVersion(
+                mirrord_protocol::VERSION.clone(),
+            ))
+            .await
+        else {
+            // failed to send to agent
+            return Err(PortForwardError::AgentSetupError(
+                "Sending 'Switch Protocol Version' Client Message failed".into(),
+            ));
+        };
+        match self.agent_connection.receiver.recv().await {
+            Some(DaemonMessage::SwitchProtocolVersionResponse(version))
+                if CLIENT_READY_FOR_LOGS.matches(&version) =>
+            {
+                let Ok(_) = self
+                    .agent_connection
+                    .sender
+                    .send(ClientMessage::ReadyForLogs)
+                    .await
+                else {
+                    return Err(PortForwardError::AgentSetupError(
+                        "Sending 'Ready for Logs' Client Message failed".into(),
+                    ));
+                };
+            }
+            _ => {
+                return Err(PortForwardError::AgentSetupError(
+                    "Switching protocol version check failed".into(),
+                ))
+            }
+        }
+
         loop {
             select! {
                 message = self.agent_connection.receiver.recv() => match message {
-                    // message incoming from agent
-                    // TODO: are some variants ignored and left for other arm to handle?
                     Some(message) => {
                         match message {
-                            DaemonMessage::Close(_) => break Ok(()),                    // kills intproxy, kills portfwd as well?
-                            DaemonMessage::Tcp(_) => todo!(),                           // pass back to user app with tx_connections
-                            DaemonMessage::TcpSteal(_) => todo!(),                      // pass back to user app with tx_connections
-                            DaemonMessage::TcpOutgoing(_) => todo!(),                   // outgoing?
-                            DaemonMessage::UdpOutgoing(_) => todo!(),                   // udp not supported, ignore or send err?
-                            DaemonMessage::LogMessage(_) => todo!(),                    // log given?
-                            DaemonMessage::File(_) => todo!(),                          // ???
-                            DaemonMessage::Pong => todo!(),                             // do nothing? or part of setup handshake?
-                            DaemonMessage::GetEnvVarsResponse(_) => todo!(),            // ???
-                            DaemonMessage::GetAddrInfoResponse(_) => todo!(),           // ???
-                            DaemonMessage::PauseTarget(_) => todo!(),                   // pause depricated?
-                            DaemonMessage::SwitchProtocolVersionResponse(_) => todo!(), // ???
+                            DaemonMessage::TcpOutgoing(message) => {
+                                match message {
+                                    DaemonTcpOutgoing::Connect(res) => {
+                                        match res {
+                                            Ok(connection) => {
+                                                let SocketAddress::Ip(local_address) = connection.local_address else {
+                                                    return Err(PortForwardError::ConnectionError("Unexpectedly received Unix address for socket during setup".into()));
+                                                };
+                                                self.sockets_ids_map.insert(connection.connection_id, local_address);
+                                                // tokio::spawn(); // TODO: new connection made successfully, spawn new task to proxy data
+                                            },
+                                            Err(error) => return Err(PortForwardError::ConnectionError("{error}".into())), // FIX: string format
+                                        }
+                                    },
+                                    DaemonTcpOutgoing::Read(res) => todo!(), // TODO: response from remote socket? can this happen?
+                                    DaemonTcpOutgoing::Close(connection_id) => {
+                                        if let Some(Either::Right(socket)) = self.sockets_ids_map.get(Either::Left(connection_id)) {
+                                            self.sockets_ids_map.remove(Either::Right(socket));
+                                            self.rx_connections.remove(&socket);
+                                            self.tx_connections.remove(&socket);
+                                        }
+                                    },
+                                }
+                            },
+                            DaemonMessage::LogMessage(log_message) => {
+                                match log_message.level {
+                                    LogLevel::Warn => tracing::warn!(log_message.message),
+                                    LogLevel::Error => tracing::error!(log_message.message),
+                                }
+                            },
+                            DaemonMessage::Pong => todo!(), // TODO: ?
+                            DaemonMessage::Close(error) => {
+                                break Err(PortForwardError::AgentError(error));
+                            },
+                            other@_ => {
+                                break Err(PortForwardError::AgentError("Unexpected message from Agent: {other}".into())); // FIX: string format
+                            },
                         }
                     },
                     None => {
-                        tracing::trace!("None message recieved, connection ended with agent");
+                        tracing::trace!("None message received, connection ended with agent");
                         break Ok(());
                     },
                 },
@@ -95,33 +167,112 @@ impl PortForwarder {
                         self.rx_connections.insert(socket, read);
                         self.tx_connections.insert(socket, write);
                         // get destination socket from mappings
-                        let _destination =  match self.mappings.get(&socket) {
+                        let destination =  match self.mappings.get(&socket) {
                             Some(address) => address,
                             None => return Err(PortForwardError::SocketMappingNotFound(socket)),
                         };
-                        tokio::spawn(async move {
-                            // when first data received: talk 2 agent and make outgoing connection, then proxy data
-                            // TODO: ClientMessage Variant?
-                            // self.agent_connection.sender.send(ClientMessage::GetAddrInfoRequest); // <- send destination info
-                            // let connection = self.agent_connection.receiver.recv(); // <- recv connection to destination
-                            // connection.send(read.recv()); // <- send data to remote port
-                        });
+                        let dest_socket = SocketAddress::Ip(*destination);
+                        let Ok(_) = self.agent_connection.sender.send(ClientMessage::TcpOutgoing(LayerTcpOutgoing::Connect(LayerConnect{remote_address: dest_socket}))).await else {
+                            return Err(PortForwardError::AgentSetupError("Failed to send connection request to agent".into()));
+                        };
                     },
-                    Some((_socket, Err(_error))) => {
+                    Some((socket, Err(error))) => {
                         // error from TcpStream
-                        // TODO: remove stream from rx, tx, send error or Close to agent?
-                        // self.agent_connection.sender.send(ClientMessage);
-                        todo!()
+                        tracing::error!("Error occured while listening to local socket {socket}: {error}");
+                        self.rx_connections.remove(&socket);
+                        self.tx_connections.remove(&socket);
+                        if let Some(Either::Left(connection_id)) = self.sockets_ids_map.get(Either::Right(socket)) {
+                            let _ = self.agent_connection.sender.send(ClientMessage::TcpOutgoing(LayerTcpOutgoing::Close(LayerClose{ connection_id }))).await;
+                        }
+                        self.sockets_ids_map.remove(Either::Right(socket));
                     },
-                    None => todo!(),
+                    None => break Ok(()), // all streams ended
                 }
             }
         }
     }
 }
 
+pub struct BiMap {
+    // connection IDs for successful connections
+    connection_ids: HashMap<SocketAddr, ConnectionId>,
+    // connection IDs for successful connections
+    sockets: HashMap<ConnectionId, SocketAddr>,
+}
+
+impl BiMap {
+    fn socket(&self, connection_id: ConnectionId) -> Option<&SocketAddr> {
+        self.sockets.get(&connection_id)
+    }
+
+    fn connection_id(&self, socket: SocketAddr) -> Option<&ConnectionId> {
+        self.connection_ids.get(&socket)
+    }
+
+    /// retrieve a value from the BiMap given either value
+    /// returns Some() if value was present, otherwise returns None
+    pub fn get(
+        &self,
+        item: Either<ConnectionId, SocketAddr>,
+    ) -> Option<Either<ConnectionId, SocketAddr>> {
+        match item {
+            Either::Left(connection_id) => self.socket(connection_id).cloned().map(Either::Right),
+            Either::Right(socket) => self.connection_id(socket).cloned().map(Either::Left),
+        }
+    }
+
+    /// insert a pair of values into the BiMap, returning None if successful
+    /// if value(s) already present for either item, Some() is returned and contains the existing
+    /// value(s)
+    pub fn insert(
+        &mut self,
+        connection_id: ConnectionId,
+        socket: SocketAddr,
+    ) -> Option<(Option<SocketAddr>, Option<ConnectionId>)> {
+        let res_s = self.sockets.insert(connection_id, socket);
+        let res_c = self.connection_ids.insert(socket, connection_id);
+        match (res_s, res_c) {
+            (None, None) => return None,
+            _ => return Some((res_s, res_c)),
+        }
+    }
+
+    /// remove a pair of values from the BiMap given either value
+    /// returns Some() if both values were present, otherwise returns None
+    /// note that if None is returned but one value was present, it has been removed
+    pub fn remove(&mut self, item: Either<ConnectionId, SocketAddr>) -> Option<()> {
+        let (connection_id, socket) = match item {
+            Either::Left(connection_id) => {
+                (Some(connection_id), self.socket(connection_id).cloned())
+            }
+            Either::Right(socket) => (self.connection_id(socket).cloned(), Some(socket)),
+        };
+
+        let res_s = if let Some(inner) = connection_id {
+            self.sockets.remove(&inner)
+        } else {
+            None
+        };
+        let res_c = if let Some(inner) = socket {
+            self.connection_ids.remove(&inner)
+        } else {
+            None
+        };
+        match (res_s, res_c) {
+            (Some(_), Some(_)) => return Some(()),
+            _ => return None,
+        }
+    }
+}
+
 #[derive(Debug, Error)]
 pub enum PortForwardError {
+    #[error("Failed to setup connection with Agent with error: `{0}`")]
+    AgentSetupError(String),
+
+    #[error("Error occurred in the Agent: `{0}`")]
+    AgentError(String),
+
     #[error("Multiple port forwarding mappings found for local address `{0}`")]
     PortMapSetupError(SocketAddr),
 
@@ -130,6 +281,9 @@ pub enum PortForwardError {
 
     #[error("No destination address found for local address `{0}`")]
     SocketMappingNotFound(SocketAddr),
+
+    #[error("Failed to establish connection with remote process: `{0}`")]
+    ConnectionError(String),
 }
 
 impl From<PortForwardError> for CliError {
