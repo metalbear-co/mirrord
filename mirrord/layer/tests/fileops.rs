a02971d12ef5f7b3a71053fbaeef7f345de19fb3
meowjesty <meowjesty_nyan@outlook.com>
2024-08-13 18:59:07 -0300

    add expect close

--- mirrord/layer/tests/fileops.rs
+++ mirrord/layer/tests/fileops.rs
@@ -424,6 +424,8 @@ async fn go_dir(
         ClientMessage::FileRequest(FileRequest::CloseDir(CloseDirRequest { remote_fd: 2 }))
     );
 
+    intproxy.expect_file_close(fd).await;
+
     test_process.wait_assert_success().await;
     test_process.assert_no_error_in_stderr().await;
 }
