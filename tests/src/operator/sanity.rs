7133cb186944582b523977b0865050e647a74f88
meowjesty <meowjesty_nyan@outlook.com>
2024-08-28 11:40:23 -0300

    no more 15 tests for mirrord_ls

--- tests/src/operator/sanity.rs
+++ tests/src/operator/sanity.rs
@@ -16,40 +16,25 @@ pub async fn mirrord_ls(#[future] service_for_mirrord_ls: KubeService) {
     assert!(res.success());
     let stdout = process.get_stdout().await;
     let targets: Vec<String> = serde_json::from_str(&stdout).unwrap();
-    let re = Regex::new(r"^(pod|deployment|statefulset|cronjob|job)/.+(/container/.+)?$").unwrap();
+    let re =
+        Regex::new(r"^(pod|deployment|stateful_set|cron_job|job)/.+(/container/.+)?$").unwrap();
     targets
         .iter()
         .for_each(|output| assert!(re.is_match(output)));
-    assert!(targets
-        .iter()
-        .any(|output| output.starts_with(&format!("pod/{}", service.name))));
-}
-
-#[rstest]
-#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
-pub async fn mirrord_ls_targets(
-    #[future] service_for_mirrord_ls: KubeService,
-    #[values(
-        r"^pod/.+(/container/.+)?$",
-        r"^deployment/.+(/container/.+)?$",
-        r"^stateful_set/.+(/container/.+)?$",
-        r"^cron_job/.+(/container/.+)?$",
-        r"^job/.+(/container/.+)?$"
-    )]
-    target_string: &str,
-) {
-    let service = service_for_mirrord_ls.await;
-    let mut process = run_ls::<true>(None, None).await;
-    let res = process.wait().await;
-    assert!(res.success());
-    let stdout = process.get_stdout().await;
-    let targets: Vec<String> = serde_json::from_str(&stdout).unwrap();
-    let re = Regex::new(target_string).unwrap();
-
-    let has_target = targets.iter().any(|output| re.is_match(output));
-    assert!(has_target, "targets {:?}", targets);
 
     assert!(targets
         .iter()
         .any(|output| output.starts_with(&format!("pod/{}", service.name))));
+    assert!(targets
+        .iter()
+        .any(|output| output.starts_with(&format!("deployment/{}", service.name))));
+    assert!(targets
+        .iter()
+        .any(|output| output.starts_with(&format!("stateful_set/{}", service.name))));
+    assert!(targets
+        .iter()
+        .any(|output| output.starts_with(&format!("cron_job/{}", service.name))));
+    assert!(targets
+        .iter()
+        .any(|output| output.starts_with(&format!("job/{}", service.name))));
 }
