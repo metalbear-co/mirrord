3658bed4e55f23f129b2e57029b284929f55ff41
Razz4780 <msmolarekg@gmail.com>
2024-09-19 20:27:22 +0200

    External change CRD

--- mirrord/operator/src/crd.rs
+++ mirrord/operator/src/crd.rs
@@ -659,3 +659,18 @@ pub struct MirrordSqsSessionSpec {
     // so we save that field as a (HEX) string even though its source is a u64
     pub session_id: String,
 }
+
+/// Properties to use when creating operator's Kafka client.
+/// Resources of this kind should live in the operator's namespace.
+#[derive(CustomResource, Clone, Debug, Deserialize, Serialize, JsonSchema)]
+#[kube(
+    group = "operator.metalbear.co",
+    version = "v1",
+    kind = "MirrordExternalChange",
+    namespaced
+)]
+#[serde(rename_all = "camelCase")]
+pub enum MirrordExternalChangeSpec {
+    #[serde(rename_all = "camelCase")]
+    TemporaryKafkaTopic { name: String },
+}
