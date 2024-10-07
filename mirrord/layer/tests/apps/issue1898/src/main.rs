2d232c2cd98aba345f6e7051d8e4cfb97f972b8e
Dmitry Dodzin <d.dodzin@gmail.com>
2024-10-07 11:20:51 +0300

    Add Small Delay to test

--- mirrord/layer/tests/apps/issue1898/src/main.rs
+++ mirrord/layer/tests/apps/issue1898/src/main.rs
@@ -1,4 +1,7 @@
-use std::net::{Shutdown, SocketAddr, TcpStream as SyncTcpStream};
+use std::{
+    net::{Shutdown, SocketAddr, TcpStream as SyncTcpStream},
+    time::Duration,
+};
 
 use async_std::net::TcpStream as AsyncTcpStream;
 use thread_async::ThreadFutureExt;
@@ -10,6 +13,10 @@ fn main() {
     let second_socket_addr: SocketAddr = "2.3.4.5:80".parse().unwrap();
 
     let async_stream = AsyncTcpStream::connect(socket_addr).spawn_thread_await();
+
+    assert!(!async_stream.is_finished());
+
+    std::thread::sleep(Duration::from_millis(10));
 
     let stream =
         SyncTcpStream::connect(second_socket_addr).expect("sync tcp stream was not created");
