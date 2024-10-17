2825a8e828e43daff5725321e7a720db8bada3b7
t4lz <t4lz.git@gmail.com>
2024-10-17 11:15:41 +0200

    No arm64e in fat layer

--- scripts/build_fat_mac.sh
+++ scripts/build_fat_mac.sh
@@ -7,12 +7,10 @@
 set -e
 cargo build -p mirrord-layer --target=x86_64-apple-darwin
 cargo build -p mirrord-layer --target=aarch64-apple-darwin
-cp target/aarch64-apple-darwin/debug/libmirrord_layer.dylib target/aarch64-apple-darwin/debug/libmirrord_layer_arm64e.dylib
-printf '\x02' | dd of=target/aarch64-apple-darwin/debug/libmirrord_layer_arm64e.dylib bs=1 seek=8 count=1 conv=notrunc
-codesign -f -s - target/aarch64-apple-darwin/debug/libmirrord_layer_arm64e.dylib
+codesign -f -s - target/aarch64-apple-darwin/debug/libmirrord_layer.dylib
 codesign -f -s - target/x86_64-apple-darwin/debug/libmirrord_layer.dylib
 mkdir -p target/universal-apple-darwin/debug
-lipo -create -output target/universal-apple-darwin/debug/libmirrord_layer.dylib target/aarch64-apple-darwin/debug/libmirrord_layer.dylib target/x86_64-apple-darwin/debug/libmirrord_layer.dylib target/aarch64-apple-darwin/debug/libmirrord_layer_arm64e.dylib
+lipo -create -output target/universal-apple-darwin/debug/libmirrord_layer.dylib target/aarch64-apple-darwin/debug/libmirrord_layer.dylib target/x86_64-apple-darwin/debug/libmirrord_layer.dylib
 codesign -f -s - target/universal-apple-darwin/debug/libmirrord_layer.dylib
 MIRRORD_LAYER_FILE=../../../target/universal-apple-darwin/debug/libmirrord_layer.dylib cargo build -p mirrord --target=aarch64-apple-darwin
 MIRRORD_LAYER_FILE=../../../target/universal-apple-darwin/debug/libmirrord_layer.dylib cargo build -p mirrord --target=x86_64-apple-darwin
