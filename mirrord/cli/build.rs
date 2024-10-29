92adb735b4842392aa889d44300c498562f3582d
Gemma Tipper <gemmat@metalbear.co>
2024-10-29 12:11:32 +0000

    Change build script to correctly check target arch

--- mirrord/cli/build.rs
+++ mirrord/cli/build.rs
@@ -1,4 +1,3 @@
-#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 use std::process::exit;
 
 fn main() {
@@ -10,8 +9,12 @@ fn main() {
         );
     };
 
-    #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
-    if std::env::var("MIRRORD_LAYER_FILE_MACOS_ARM64").is_err() {
+    // this check uses cargo env vars instead of conditional compilation due to cfg! not respecting
+    // the target flag on a build
+    if std::env::var("MIRRORD_LAYER_FILE_MACOS_ARM64").is_err()
+        && std::env::var("CARGO_CFG_TARGET_ARCH").is_ok_and(|t| t.eq("aarch64"))
+        && std::env::var("CARGO_CFG_TARGET_OS").is_ok_and(|t| t.eq("macos"))
+    {
         println!("cargo::warning=No environment variable 'MIRRORD_LAYER_FILE_MACOS_ARM64' found - it should contain the path to the mirrord layer compiled for the `aarch64-apple-darwin` target");
         exit(1);
     };
