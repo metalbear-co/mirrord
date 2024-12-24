2048eb54befe7915996f0489a5c45344e86c4334
Razz4780 <msmolarekg@gmail.com>
2024-12-24 11:44:56 +0100

    mirrord-console

--- mirrord/console/src/lib.rs
+++ mirrord/console/src/lib.rs
@@ -1,4 +1,5 @@
 #![warn(clippy::indexing_slicing)]
+#![deny(unused_crate_dependencies)]
 
 #[cfg(feature = "async-logger")]
 pub mod async_logger;
@@ -9,3 +10,13 @@ pub mod protocol;
 #[cfg(feature = "async-logger")]
 pub use async_logger::init_async_logger;
 pub use logger::init_logger;
+
+/// Silences `deny(unused_crate_dependencies)`.
+/// 
+/// These dependencies are only used in the console binary.
+#[cfg(feature = "binary")]
+mod binary_deps {
+    use tokio as _;
+    use tracing as _;
+    use tracing_subscriber as _;
+}
