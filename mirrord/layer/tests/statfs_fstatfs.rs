a99ef263dd5a4fa016440f04cd647a6afe2e2456
Facundo Poblete <facundoepoblete@gmail.com>
2025-01-14 08:40:42 -0300

    Fix statfs_fstatfs.rs

--- mirrord/layer/tests/statfs_fstatfs.rs
+++ mirrord/layer/tests/statfs_fstatfs.rs
@@ -17,22 +17,25 @@ async fn mkdir(dylib_path: &Path) {
         .start_process_with_layer(dylib_path, Default::default(), None)
         .await;
 
-    println!("waiting for file request.");
+    println!("waiting for file request (mkdir).");
     intproxy
         .expect_make_dir("/statfs_fstatfs_test_path", 0o777)
         .await;
 
-    println!("waiting for file request.");
+    println!("waiting for file request (statfs).");
     intproxy.expect_statfs("/statfs_fstatfs_test_path").await;
 
-    println!("waiting for file request.");
+    println!("waiting for file request (open).");
     let fd: u64 = 1;
     intproxy
         .expect_file_open_for_reading("/statfs_fstatfs_test_path", fd)
         .await;
 
-    println!("waiting for file request.");
+    println!("waiting for file request (fstatfs).");
     intproxy.expect_fstatfs(fd).await;
+
+    println!("waiting for file request (close).");
+    intproxy.expect_file_close(fd).await;
 
     assert_eq!(intproxy.try_recv().await, None);
 
