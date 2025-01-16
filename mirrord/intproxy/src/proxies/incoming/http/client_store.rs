d729c3b391012a87312ffa995743310792ecae56
Razz4780 <msmolarekg@gmail.com>
2025-01-16 18:50:33 +0100

    ClientStore test

--- mirrord/intproxy/src/proxies/incoming/http/client_store.rs
+++ mirrord/intproxy/src/proxies/incoming/http/client_store.rs
@@ -148,3 +148,44 @@ async fn cleanup_task(clients: watch::Sender<Vec<IdleLocalClient>>, idle_client_
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::{convert::Infallible, time::Duration};
+
+    use bytes::Bytes;
+    use http_body_util::Empty;
+    use hyper::{body::Incoming, server::conn::http1, service::service_fn, Request, Response, Version};
+    use hyper_util::rt::TokioIo;
+    use tokio::{net::TcpListener, time};
+
+    use super::ClientStore;
+
+    /// Verifies that [`ClientStore`] cleans up unused connections.
+    #[tokio::test]
+    async fn cleans_up_unused_connections() {
+        let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let addr = listener.local_addr().unwrap();
+
+        tokio::spawn(async move {
+            let service = service_fn(|_req: Request<Incoming>| {
+                std::future::ready(Ok::<_, Infallible>(Response::new(Empty::<Bytes>::new())))
+            });
+
+            let (connection, _) = listener.accept().await.unwrap();
+            std::mem::drop(listener);
+            http1::Builder::new()
+                .serve_connection(TokioIo::new(connection), service)
+                .await
+                .unwrap()
+        });
+
+        let client_store = ClientStore::new_with_timeout(Duration::from_millis(10));
+        let client = client_store.get(addr, Version::HTTP_11).await.unwrap();
+        client_store.push_idle(client);
+
+        time::sleep(Duration::from_millis(100)).await;
+
+        assert!(client_store.0.borrow().is_empty());
+    }
+}
