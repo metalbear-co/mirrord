08bfbe5d75931fd3e19b3c0091348b7c1baac39b
t4lz <t4lz.git@gmail.com>
2025-02-13 12:32:06 +0100

    fix mkdir rmdir test apps - fixup

--- mirrord/layer/tests/apps/mkdir/mkdir.c
+++ mirrord/layer/tests/apps/mkdir/mkdir.c
@@ -11,9 +11,9 @@ int main()
 {
   // Create a directory at a path that is ignored by default by the layer,
   // so that the test can make sure it gets ignored.
-  char *mkdir_test_path = "/tmp/mkdir_test_path";
-  int mkdir_result = mkdir(mkdir_test_path, 0777);
-  assert(mkdir_result == 0);
+  char *ignored_mkdir_path = "/tmp/mkdir_test_path";
+  int local_mkdir_result = mkdir(ignored_mkdir_path, 0777);
+  assert(local_mkdir_result == 0);
 
   char *mkdir_test_path = "/mkdir_test_path";
   int mkdir_result = mkdir(mkdir_test_path, 0777);
@@ -21,9 +21,9 @@ int main()
 
   // Create a directory at a path that is ignored by default by the layer,
   // so that the test can make sure it gets ignored.
-  char *mkdirat_test_path = "/tmp/mkdirat_test_path";
-  int mkdirat_result = mkdirat(AT_FDCWD, mkdirat_test_path, 0777);
-  assert(mkdirat_result == 0);
+  char *ignored_mkdirat_path = "/tmp/mkdirat_test_path";
+  int local_mkdirat_result = mkdirat(AT_FDCWD, ignored_mkdirat_path, 0777);
+  assert(local_mkdirat_result == 0);
 
   char *mkdirat_test_path = "/mkdirat_test_path";
   int mkdirat_result = mkdirat(AT_FDCWD, mkdirat_test_path, 0777);
