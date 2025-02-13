9f56c94bfcf3c633afc62e995334d73f1271df7c
t4lz <t4lz.git@gmail.com>
2025-02-13 11:18:56 +0100

    test ignored faccessat in integration tests

--- mirrord/layer/tests/apps/faccessat_go/main.go
+++ mirrord/layer/tests/apps/faccessat_go/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"C"
 	"syscall"
+	"os"
 )
 
-const TEXT = "Pineapples."
-
 func main() {
+
+	// First make a call that should be bypassed.
+	localHomeDir, homeDirErr := os.UserHomeDir()
+	if homeDirErr != nil {
+		panic(homeDirErr)
+	}
+	localErr := syscall.Access(localHomeDir, syscall.O_RDWR)
+	if localErr != nil {
+		panic(localErr)
+	}
+
 	// Tests: SYS_faccessat
 	// Access calls Faccess with _AT_FDCWD and flags set to 0
 	err := syscall.Access("/app/test.txt", syscall.O_RDWR)
