f0809554c1e842fd48c54753d90c5cce9d85a0c4
t4lz <t4lz.git@gmail.com>
2025-02-13 11:08:06 +0100

    test ignored rmdir in integration tests

--- mirrord/layer/tests/apps/rmdir/rmdir.c
+++ mirrord/layer/tests/apps/rmdir/rmdir.c
@@ -9,6 +9,17 @@
 ///
 int main()
 {
+  // First create and delete a dir that should be created and deleted locally and therefore
+  // bypassed in the layer.
+  // This should not result in any client message.
+  char *test_dir = "/tmp/test_dir";
+  int mkdir_result = mkdir(test_dir, 0777);
+  assert(mkdir_result == 0);
+
+  int rmdir_result = rmdir(test_dir);
+  assert(rmdir_result == 0);
+
+  // This should result in client messages.
   char *test_dir = "/test_dir";
   int mkdir_result = mkdir(test_dir, 0777);
   assert(mkdir_result == 0);
