5ff2ac58ed2e7fd5cb4d36e5b71fa260e91c238a
Gemma Tipper <gemmat@metalbear.co>
2025-02-21 13:53:44 +0000

    Change universal binary build script to insert code shim in x86 macos binary

--- mirrord/cli/build.rs
+++ mirrord/cli/build.rs
@@ -12,7 +12,7 @@ fn main() {
     // this check uses cargo env vars instead of conditional compilation due to cfg! not respecting
     // the target flag on a build
     if std::env::var("MIRRORD_LAYER_FILE_MACOS_ARM64").is_err()
-        && std::env::var("CARGO_CFG_TARGET_ARCH").is_ok_and(|t| t.eq("aarch64"))
+        && std::env::var("CARGO_CFG_TARGET_ARCH").is_ok_and(|t| t.eq("aarch64") || t.eq("x86_64"))
         && std::env::var("CARGO_CFG_TARGET_OS").is_ok_and(|t| t.eq("macos"))
     {
         println!("cargo::warning=No environment variable 'MIRRORD_LAYER_FILE_MACOS_ARM64' found - it should contain the path to the mirrord layer compiled for the `aarch64-apple-darwin` target");
