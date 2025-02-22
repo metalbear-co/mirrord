36708594ed424c43c77c380d1c11125d70e2de0e
Razz4780 <msmolarekg@gmail.com>
2025-02-22 17:53:07 +0100

    Improvements to HasSubjectAlternateNames trait

--- mirrord/tls-util/src/san.rs
+++ mirrord/tls-util/src/san.rs
@@ -9,7 +9,7 @@ use x509_parser::{
 
 use crate::error::GetSanError;
 
-/// Convenience trait for extracing [`ServerName`]s from an x509 certificate.
+/// Convenience trait for extracting [`ServerName`]s from an X509 certificate.
 pub trait HasSubjectAlternateNames {
     fn subject_alternate_names(&self) -> Result<Vec<ServerName<'static>>, GetSanError>;
 }
@@ -17,31 +17,31 @@ pub trait HasSubjectAlternateNames {
 impl HasSubjectAlternateNames for CertificateDer<'_> {
     #[tracing::instrument(level = Level::DEBUG, skip(self), ret, err(level = Level::DEBUG))]
     fn subject_alternate_names(&self) -> Result<Vec<ServerName<'static>>, GetSanError> {
-        let certificate = X509Certificate::from_der(self)?.1;
-        get_san(&certificate)
+        X509Certificate::from_der(self)?.1.subject_alternate_names()
     }
 }
 
 impl HasSubjectAlternateNames for Pem {
     #[tracing::instrument(level = Level::DEBUG, skip(self), ret, err(level = Level::DEBUG))]
     fn subject_alternate_names(&self) -> Result<Vec<ServerName<'static>>, GetSanError> {
-        let certificate = self.parse_x509()?;
-        get_san(&certificate)
+        self.parse_x509()?.subject_alternate_names()
     }
 }
 
-fn get_san(certificate: &X509Certificate<'_>) -> Result<Vec<ServerName<'static>>, GetSanError> {
-    let extension = certificate
-        .subject_alternative_name()
-        .map_err(GetSanError::InvalidSanExtension)?
-        .ok_or(GetSanError::NoSanExtension)?;
+impl HasSubjectAlternateNames for X509Certificate<'_> {
+    #[tracing::instrument(level = Level::DEBUG, skip(self), ret, err(level = Level::DEBUG))]
+    fn subject_alternate_names(&self) -> Result<Vec<ServerName<'static>>, GetSanError> {
+        let extension = self
+            .subject_alternative_name()
+            .map_err(GetSanError::InvalidSanExtension)?
+            .ok_or(GetSanError::NoSanExtension)?;
 
-    let names = extension
-        .value
-        .general_names
-        .iter()
-        .filter_map(|general_name| {
-            match *general_name {
+        let names = extension
+            .value
+            .general_names
+            .iter()
+            .filter_map(|general_name| {
+                match *general_name {
             GeneralName::DNSName(name) => {
                 ServerName::try_from(name)
                     .inspect_err(|error| {
@@ -67,8 +67,9 @@ fn get_san(certificate: &X509Certificate<'_>) -> Result<Vec<ServerName<'static>>
 
             _ => None,
         }
-        })
-        .collect();
+            })
+            .collect();
 
-    Ok(names)
+        Ok(names)
+    }
 }
