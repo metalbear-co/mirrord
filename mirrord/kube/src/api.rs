773af9480784700570f235a6072849c6f4601e71
Razz4780 <msmolarekg@gmail.com>
2025-02-25 10:54:02 +0100

    Fixed span entering logic

--- mirrord/kube/src/api.rs
+++ mirrord/kube/src/api.rs
@@ -2,7 +2,7 @@ use actix_codec::{AsyncRead, AsyncWrite};
 use futures::{SinkExt, StreamExt};
 use mirrord_protocol::{ClientCodec, ClientMessage, DaemonMessage};
 use tokio::sync::mpsc;
-use tracing::Level;
+use tracing::{Instrument, Level};
 
 pub mod container;
 pub mod kubernetes;
@@ -25,48 +25,51 @@ where
     let (in_tx, mut in_rx) = mpsc::channel(CONNECTION_CHANNEL_SIZE);
     let (out_tx, out_rx) = mpsc::channel(CONNECTION_CHANNEL_SIZE);
 
-    tokio::spawn(async move {
-        let span = tracing::span!(Level::TRACE, "mirrord_protocol_connection_wrapper_task");
-        let _entered = span.enter();
-
-        // Generally, this loop should not be exited early with any `return` statement.
-        // We want the `close` below to happen.
-        loop {
-            tokio::select! {
-                msg = in_rx.recv() => match msg {
-                    Some(msg) => {
-                        if let Err(error) = codec.send(msg).await {
-                            tracing::error!(%error, "Failed to send a client message");
+    tokio::spawn(
+        async move {
+            // Generally, this loop should not be exited early with any `return` statement.
+            // We want the `close` below to happen.
+            loop {
+                tokio::select! {
+                    msg = in_rx.recv() => match msg {
+                        Some(msg) => {
+                            if let Err(error) = codec.send(msg).await {
+                                tracing::error!(%error, "Failed to send a client message");
+                                break;
+                            }
+                        }
+                        None => {
+                            tracing::trace!("No more client messages, disconnecting");
                             break;
                         }
-                    }
-                    None => {
-                        tracing::trace!("No more client messages, disconnecting");
-                        break;
-                    }
-                },
+                    },
 
-                msg = codec.next() => match msg {
-                    Some(Ok(msg)) => {
-                        if let Err(error) = out_tx.send(msg).await {
-                            tracing::error!(%error, "Failed to send an agent message");
+                    msg = codec.next() => match msg {
+                        Some(Ok(msg)) => {
+                            if let Err(error) = out_tx.send(msg).await {
+                                tracing::error!(%error, "Failed to send an agent message");
+                                break;
+                            }
+                        }
+                        Some(Err(error)) => {
+                            tracing::error!(%error, "Failed to receive an agent message");
                             break;
                         }
-                    }
-                    Some(Err(error)) => {
-                        tracing::error!(%error, "Failed to receive an agent message");
-                        break;
-                    }
-                    None => {
-                        tracing::trace!("No more agent messages, disconnecting");
-                        break;
-                    }
-                },
+                        None => {
+                            tracing::trace!("No more agent messages, disconnecting");
+                            break;
+                        }
+                    },
+                }
             }
-        }
 
-        let _ = codec.close().await;
-    });
+            let _ = codec.close().await;
+        }
+        .instrument(tracing::span!(
+            Level::TRACE,
+            "mirrord_protocol_connection_wrapper_task"
+        )),
+    );
 
     (in_tx, out_rx)
 }
