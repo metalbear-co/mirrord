6406b7391386adf24e2590acd4b6745201a3e115
Razz4780 <msmolarekg@gmail.com>
2025-03-04 09:22:09 +0100

    Fixity fix

--- tests/src/utils/port_forwarder.rs
+++ tests/src/utils/port_forwarder.rs
@@ -1,7 +1,11 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use k8s_openapi::api::core::v1::{Pod, Service};
-use kube::{api::ListParams, Api, Client};
+use k8s_openapi::api::core::v1::Pod;
+#[cfg(feature = "operator")]
+use k8s_openapi::api::core::v1::Service;
+#[cfg(feature = "operator")]
+use kube::api::ListParams;
+use kube::{Api, Client};
 use tokio::{
     net::TcpListener,
     task::{JoinHandle, JoinSet},
@@ -33,6 +37,7 @@ impl PortForwarder {
         Self { address, handle }
     }
 
+    #[cfg(feature = "operator")]
     pub async fn new_for_service(client: Client, service: &Service, port: u16) -> Self {
         let selector = service
             .spec
