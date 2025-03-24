fa47a8343603a00cf516542e839d498c6cfb3939
Dmitry Dodzin <d.dodzin@gmail.com>
2025-03-24 17:38:59 +0200

    Nvm

--- tests/src/operator/setup.rs
+++ tests/src/operator/setup.rs
@@ -41,7 +41,7 @@ async fn check_install_result(stdout: String) {
     let temp_dir = tempdir().unwrap();
 
     let validate = Command::new("kubectl")
-        .args(vec!["apply", "--dry-run=server", "-f", "-"])
+        .args(vec!["apply", "--dry-run=client", "-f", "-"])
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
@@ -62,7 +62,7 @@ async fn check_install_result(stdout: String) {
 
 async fn check_install_file_result(file_path: String, temp_dir: TempDir) {
     let validate = Command::new("kubectl")
-        .args(vec!["apply", "--dry-run=server", "-f", &file_path])
+        .args(vec!["apply", "--dry-run=client", "-f", &file_path])
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
