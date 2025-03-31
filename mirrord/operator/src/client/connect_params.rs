d0509ca19ca61b3a1874adae95b91a523c9c805f
Razz4780 <msmolarekg@gmail.com>
2025-03-31 17:04:49 +0200

    Added support for Kafka splitting without copying the target

--- mirrord/operator/src/client/connect_params.rs
+++ mirrord/operator/src/client/connect_params.rs
@@ -1,6 +1,9 @@
-use std::fmt;
+use std::{
+    collections::{BTreeMap, HashMap},
+    fmt,
+};
 
-use mirrord_config::feature::network::incoming::ConcurrentSteal;
+use mirrord_config::{feature::network::incoming::ConcurrentSteal, LayerConfig};
 use serde::Serialize;
 
 /// Query params for the operator connect request.
@@ -16,12 +19,24 @@ pub struct ConnectParams<'a> {
     /// Selected mirrord profile.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub profile: Option<&'a str>,
+    #[serde(skip_serializing_if = "HashMap::is_empty")]
+    pub kafka_splits: HashMap<&'a str, &'a BTreeMap<String, String>>,
+}
+
+impl<'a> ConnectParams<'a> {
+    pub fn new(config: &'a LayerConfig) -> Self {
+        Self {
+            connect: true,
+            on_concurrent_steal: config.feature.network.incoming.on_concurrent_steal.into(),
+            profile: config.profile.as_deref(),
+            kafka_splits: config.feature.split_queues.kafka().collect(),
+        }
+    }
 }
 
 impl fmt::Display for ConnectParams<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let as_string =
-            serde_urlencoded::to_string(self).expect("serialization to memory should not fail");
+        let as_string = serde_qs::to_string(self).expect("serialization to memory should not fail");
 
         f.write_str(&as_string)
     }
