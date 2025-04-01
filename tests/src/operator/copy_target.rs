f6d26fa4fa38bc656057070edf434c8e9617cd1d
meowjesty <meowjesty_nyan@outlook.com>
2025-04-01 17:52:43 -0300

    use better app for test

--- tests/src/operator/copy_target.rs
+++ tests/src/operator/copy_target.rs
@@ -1,5 +1,5 @@
 #![cfg(test)]
-// #![cfg(feature = "operator")]
+#![cfg(feature = "operator")]
 //! Test the copy-target features with an operator.
 
 use std::time::Duration;
@@ -7,7 +7,7 @@ use std::time::Duration;
 use rstest::*;
 use tempfile::NamedTempFile;
 
-use crate::utils::{service, Application, KubeService};
+use crate::utils::{service, Application, EnvApp, KubeService};
 
 /// Starts mirrord with the `copy-target` feature just to validate that it can create a
 /// working copy-pod. Should work as a sanity check that the targets (see `target` parameter)
@@ -18,15 +18,14 @@ pub async fn copy_target_starts_a_working_copy(
     #[future]
     #[notrace]
     service: KubeService,
-    #[values(Application::CopyTargetStartsAWorkingCopy)] application: Application,
+    #[values(EnvApp::NodeInclude)] application: EnvApp,
     #[values("pod", "deployment")] target: &str,
 ) {
+    use crate::utils::run_exec_with_target;
+
     let service = service.await;
 
-    // Create a temporary file for the config
     let mut config_file = NamedTempFile::with_suffix(".json").unwrap();
-
-    // Create the config JSON
     let config = serde_json::json!({
         "feature": {
             "copy_target": {
@@ -34,7 +33,6 @@ pub async fn copy_target_starts_a_working_copy(
             }
         }
     });
-    // Write the config to the temporary file
     serde_json::to_writer(config_file.as_file_mut(), &config).unwrap();
 
     let target = match target {
@@ -43,19 +41,15 @@ pub async fn copy_target_starts_a_working_copy(
         other => unimplemented!("Add a new branch to check for this target `{other}` `{target}`!",),
     };
 
-    let mut test_proc = application
-        .run(
-            &target,
-            Some(&service.namespace),
-            Some(vec!["--config-file", config_path.to_str().unwrap()]),
-            None,
-        )
-        .await;
+    let mirrord_args = {
+        let mut args = application.mirrord_args().unwrap_or_default();
+        args.push("--config-file");
+        args.push(config_file.path().to_str().unwrap());
+        Some(args)
+    };
 
-    test_proc
-        .wait_for_line(Duration::from_secs(40), "daemon subscribed")
-        .await;
-
-    test_proc.assert_no_error_in_stdout().await;
-    test_proc.wait_assert_success().await;
+    let mut process =
+        run_exec_with_target(application.command(), &target, None, mirrord_args, None).await;
+    let res = process.wait().await;
+    assert!(res.success());
 }
