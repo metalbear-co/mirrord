6330c98acdeabf1a522ebfc3ef51dfd651b20d0a
yuan <yuanl@metalbear.co>
2025-04-30 12:12:09 -0400

    Fix layer test on macOS aarch64e (#3297)
    * Fix layer test on macOS aarch64e
    
    * Derive arm64 lib location from universal dylib
    
    * Put arm64_dylib_path in OnceLock
    
    * Rename changelog and add contributing guide back
    
    * Fix clippy

--- mirrord/layer/tests/common/mod.rs
+++ mirrord/layer/tests/common/mod.rs
@@ -1,3 +1,5 @@
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+use std::sync::OnceLock;
 use std::{
     assert_matches::assert_matches,
     collections::HashMap,
@@ -40,6 +42,9 @@ use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
 pub const RUST_OUTGOING_PEERS: &str = "1.1.1.1:1111,2.2.2.2:2222,3.3.3.3:3333";
 /// Configuration for [`Application::RustOutgoingTcp`] and [`Application::RustOutgoingUdp`].
 pub const RUST_OUTGOING_LOCAL: &str = "4.4.4.4:4444";
+
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+static MIRRORD_MACOS_ARM64_LIBRARY: OnceLock<PathBuf> = OnceLock::new();
 
 /// Initializes tracing for the current thread, allowing us to have multiple tracing subscribers
 /// writin logs to different files.
@@ -1394,19 +1399,57 @@ pub fn get_env(
             intproxy_addr.to_string(),
         ),
         (
+            LayerConfig::RESOLVED_CONFIG_ENV.to_string(),
+            config.encode().unwrap(),
+        ),
+        #[cfg(target_os = "macos")]
+        (
             "DYLD_INSERT_LIBRARIES".to_string(),
             dylib_path.to_str().unwrap().to_string(),
         ),
+        #[cfg(target_os = "linux")]
         (
             "LD_PRELOAD".to_string(),
             dylib_path.to_str().unwrap().to_string(),
         ),
+        #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
         (
-            LayerConfig::RESOLVED_CONFIG_ENV.to_string(),
-            config.encode().unwrap(),
+            // The universal layer library loads arm64 library from the path specified in this
+            // environment variable when the host runs arm64e.
+            // See `mirorrd/layer/shim.c` for more information.
+            "MIRRORD_MACOS_ARM64_LIBRARY".to_string(),
+            arm64_dylib_path().to_str().unwrap().to_string(),
         ),
     ]
     .into_iter()
     .chain(extra_vars_owned)
     .collect()
+}
+
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+fn arm64_dylib_path() -> &'static Path {
+    MIRRORD_MACOS_ARM64_LIBRARY
+        .get_or_init(|| {
+            if let Ok(path) = std::env::var("MIRRORD_MACOS_ARM64_LIBRARY") {
+                let dylib_path = PathBuf::from(path);
+                println!("Using existing macOS arm64 layer lib from: {dylib_path:?}");
+                assert!(dylib_path.exists());
+                return dylib_path;
+            }
+
+            if let Ok(path) = std::env::var("MIRRORD_TEST_USE_EXISTING_LIB") {
+                let derived_path = path.replace("universal-apple-darwin", "aarch64-apple-darwin");
+                let dylib_path = PathBuf::from(&derived_path);
+                if dylib_path.exists() {
+                    return dylib_path;
+                } else {
+                    println!("Derived arm64 layer lib path does not exist: {derived_path}");
+                }
+            }
+
+            let dylib_path = test_cdylib::build_current_project();
+            println!("Built macOS arm64 layer lib at {dylib_path:?}");
+            dylib_path
+        })
+        .as_path()
 }
