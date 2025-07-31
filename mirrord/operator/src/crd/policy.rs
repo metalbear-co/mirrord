40bdfab3caebdb3392cbf0f3afd04408f47d224d
Yuan Lyu <yuanl@metalbear.co>
2025-07-31 17:35:24 -0400

    Make rule expression recursive

--- mirrord/operator/src/crd/policy.rs
+++ mirrord/operator/src/crd/policy.rs
@@ -1,8 +1,15 @@
 use std::collections::HashSet;
 
 use kube::CustomResource;
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
+use schemars::{
+    schema::{InstanceType, ObjectValidation, Schema, SchemaObject},
+    JsonSchema,
+};
+use serde::{
+    de::{IntoDeserializer, MapAccess, Visitor},
+    ser::SerializeMap,
+    Deserialize, Deserializer, Serialize, Serializer,
+};
 
 use super::label_selector::LabelSelector;
 
@@ -261,9 +268,11 @@ pub struct HttpFilterPolicy {
 #[derive(Clone, Default, Debug, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct SplitQueuesPolicy {
-    /// When set to true, require message filter being used in split queues.
+    /// When set to true, require a message filter to be used in split queues.
+    #[serde(default)]
     pub require_filter: bool,
     /// A set of queue filter policies that need to be satisfied in a mirrord session.
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub filters: Vec<QueueFilterPolicy>,
 }
 
@@ -277,7 +286,7 @@ pub struct QueueFilterPolicy {
     /// The queue type this policy applies to.
     pub queue_type: QueueType,
     /// A set of rules for filters used against the matching queues.
-    pub filter_rules: QueueFilterRules,
+    pub filter_rules: QueueFilterRuleExp,
 }
 
 /// Type of queues supported by queue filter policy.
@@ -289,12 +298,163 @@ pub enum QueueType {
     Kafka,
 }
 
-/// Defines either one or all of the queue filter rules need to be met.
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
-pub enum QueueFilterRules {
-    AllOf(Vec<QueueFilterRule>),
-    AnyOf(Vec<QueueFilterRule>),
+/// A recursive queue filter rule expression.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum QueueFilterRuleExp {
+    AllOf(Vec<QueueFilterRuleExp>),
+    AnyOf(Vec<QueueFilterRuleExp>),
+    Single(QueueFilterRule),
+}
+
+impl Serialize for QueueFilterRuleExp {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            QueueFilterRuleExp::AllOf(exprs) => {
+                let mut map = serializer.serialize_map(Some(1))?;
+                map.serialize_entry("allOf", exprs)?;
+                map.end()
+            }
+            QueueFilterRuleExp::AnyOf(exprs) => {
+                let mut map = serializer.serialize_map(Some(1))?;
+                map.serialize_entry("anyOf", exprs)?;
+                map.end()
+            }
+            QueueFilterRuleExp::Single(rule) => rule.serialize(serializer),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for QueueFilterRuleExp {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct ExpVisitor;
+
+        impl<'de> Visitor<'de> for ExpVisitor {
+            type Value = QueueFilterRuleExp;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("a queue filter expression (allOf, anyOf, or a single rule)")
+            }
+
+            fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'de>,
+            {
+                // Peek the first key
+                let key: Option<String> = map.next_key()?;
+                match key {
+                    Some(ref k) if k == "allOf" => {
+                        let val = map.next_value()?;
+                        Ok(QueueFilterRuleExp::AllOf(val))
+                    }
+                    Some(ref k) if k == "anyOf" => {
+                        let val = map.next_value()?;
+                        Ok(QueueFilterRuleExp::AnyOf(val))
+                    }
+                    Some(k) => {
+                        // Reconstruct the map with the first key replayed
+                        struct MapReplay<'a, M> {
+                            first_key: Option<String>,
+                            map: &'a mut M,
+                        }
+
+                        impl<'de, M> MapAccess<'de> for MapReplay<'_, M>
+                        where
+                            M: MapAccess<'de>,
+                        {
+                            type Error = M::Error;
+
+                            fn next_key_seed<K>(
+                                &mut self,
+                                seed: K,
+                            ) -> Result<Option<K::Value>, M::Error>
+                            where
+                                K: serde::de::DeserializeSeed<'de>,
+                            {
+                                if let Some(k) = self.first_key.take() {
+                                    seed.deserialize(k.into_deserializer()).map(Some)
+                                } else {
+                                    self.map.next_key_seed(seed)
+                                }
+                            }
+
+                            fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value, M::Error>
+                            where
+                                V: serde::de::DeserializeSeed<'de>,
+                            {
+                                self.map.next_value_seed(seed)
+                            }
+                        }
+
+                        let mut replay = MapReplay {
+                            first_key: Some(k),
+                            map: &mut map,
+                        };
+
+                        let rule = QueueFilterRule::deserialize(
+                            serde::de::value::MapAccessDeserializer::new(&mut replay),
+                        )?;
+                        Ok(QueueFilterRuleExp::Single(rule))
+                    }
+                    None => Err(serde::de::Error::custom("expected a non-empty map")),
+                }
+            }
+        }
+
+        deserializer.deserialize_map(ExpVisitor)
+    }
+}
+
+impl JsonSchema for QueueFilterRuleExp {
+    fn schema_name() -> String {
+        "QueueFilterRuleExp".to_string()
+    }
+
+    fn json_schema(gen: &mut schemars::gen::SchemaGenerator) -> Schema {
+        let all_of_schema = {
+            let mut obj = ObjectValidation::default();
+            obj.properties.insert(
+                "allOf".to_string(),
+                gen.subschema_for::<Vec<QueueFilterRuleExp>>(),
+            );
+            obj.required.insert("allOf".to_string());
+            Schema::Object(SchemaObject {
+                instance_type: Some(InstanceType::Object.into()),
+                object: Some(Box::new(obj)),
+                ..Default::default()
+            })
+        };
+
+        let any_of_schema = {
+            let mut obj = ObjectValidation::default();
+            obj.properties.insert(
+                "anyOf".to_string(),
+                gen.subschema_for::<Vec<QueueFilterRuleExp>>(),
+            );
+            obj.required.insert("anyOf".to_string());
+            Schema::Object(SchemaObject {
+                instance_type: Some(InstanceType::Object.into()),
+                object: Some(Box::new(obj)),
+                ..Default::default()
+            })
+        };
+
+        let single_schema = gen.subschema_for::<QueueFilterRule>();
+
+        // Use a oneOf to represent the three mutually exclusive shapes
+        Schema::Object(SchemaObject {
+            subschemas: Some(Box::new(schemars::schema::SubschemaValidation {
+                one_of: Some(vec![all_of_schema, any_of_schema, single_schema]),
+                ..Default::default()
+            })),
+            ..Default::default()
+        })
+    }
 }
 
 /// A single filter rule.
@@ -306,6 +466,7 @@ pub struct QueueFilterRule {
     /// Key of the filter.
     pub key: String,
     /// Regex that's used to match against the filter value.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub value: Option<String>,
 }
 
@@ -314,4 +475,86 @@ fn check_one_api_group() {
     use kube::Resource;
 
     assert_eq!(MirrordPolicy::group(&()), MirrordClusterPolicy::group(&()),)
+}
+
+#[cfg(test)]
+mod test {
+    use super::SplitQueuesPolicy;
+
+    #[test]
+    fn test_queue_filter_policy_serde_roundtrip() {
+        let simple_require_filter_yaml = "requireFilter: true\n";
+        let value: SplitQueuesPolicy = serde_yaml::from_str(simple_require_filter_yaml).unwrap();
+        assert_eq!(
+            simple_require_filter_yaml,
+            serde_yaml::to_string(&value).unwrap()
+        );
+
+        let simple_all_of_yaml = r#"requireFilter: true
+filters:
+- queueId: ^test-queue$
+  queueType: SQS
+  filterRules:
+    allOf:
+    - key: foo
+      value: bar
+    - key: baz
+"#;
+        let value: SplitQueuesPolicy = serde_yaml::from_str(simple_all_of_yaml).unwrap();
+        assert_eq!(simple_all_of_yaml, serde_yaml::to_string(&value).unwrap());
+
+        let simple_any_of_yaml = r#"requireFilter: true
+filters:
+- queueId: ^test-queue$
+  queueType: Kafka
+  filterRules:
+    anyOf:
+    - key: foo
+      value: bar
+    - key: baz
+"#;
+        let value: SplitQueuesPolicy = serde_yaml::from_str(simple_any_of_yaml).unwrap();
+        assert_eq!(simple_any_of_yaml, serde_yaml::to_string(&value).unwrap());
+
+        let simple_one_rule_yaml = r#"requireFilter: true
+filters:
+- queueId: ^test-queue$
+  queueType: Kafka
+  filterRules:
+    key: foo
+    value: bar
+"#;
+        let value: SplitQueuesPolicy = serde_yaml::from_str(simple_one_rule_yaml).unwrap();
+        assert_eq!(simple_one_rule_yaml, serde_yaml::to_string(&value).unwrap());
+
+        let nested_rules_yaml = r#"requireFilter: false
+filters:
+- queueId: ^test-queue$
+  queueType: Kafka
+  filterRules:
+    anyOf:
+    - anyOf:
+      - allOf:
+        - key: nest-us
+          value: ok
+        - key: nessst-us-now
+      - anyOf:
+        - key: did-you-nest-us
+          value: yes-i-did
+        - key: thank-you
+    - allOf:
+      - anyOf:
+        - key: are-you-sure-you-nested-us
+          value: yeah-why
+      - allOf:
+        - anyOf:
+          - allOf:
+            - anyOf:
+              - key: because-i-dont-feel-nested
+                value: how-about-now
+              - key: we-are-properly-nested
+"#;
+        let value: SplitQueuesPolicy = serde_yaml::from_str(nested_rules_yaml).unwrap();
+        assert_eq!(nested_rules_yaml, serde_yaml::to_string(&value).unwrap());
+    }
 }
