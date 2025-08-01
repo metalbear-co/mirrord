2970514ca2fdacdc711176a26ee156e892e252cd
meowjesty <meowjesty_nyan@outlook.com>
2025-08-01 12:56:53 -0300

    unsafe naked

--- mirrord/layer/src/go/linux_aarch64.rs
+++ mirrord/layer/src/go/linux_aarch64.rs
@@ -39,48 +39,46 @@ unsafe extern "C" fn mirrord_syscall_handler(syscall_struct: *const SyscallArgs)
 /// On Go 1.19 one hook catches all (?) syscalls and therefore we call the syscall6 handler always
 /// so syscall6 handler need to handle syscall3 detours as well.
 // We're using asmcogcall to avoid re-implementing it and doing it badly.
-#[naked]
+#[unsafe(naked)]
 unsafe extern "C" fn go_syscall_new_detour() {
-    unsafe {
-        naked_asm!(
-            // save fp and lr to stack and reserve that memory.
-            // if we don't do it and remember to load it back before ret
-            // we will have unfortunate results
-            "str x30, [sp, -0x10]!",
-            "stur x29, [sp, -0x8]",
-            "sub x29, sp, 0x8",
-            // x1 should point to original sp+8, which should be the first parameter
-            "add x1, sp, 0x18",
-            // load mirrord_syscall_handler as first argument (x0)
-            "adr    x0, {syscall_handler}",
-            "adrp    x8, {asmcgocall}",
-            "ldr     x8, [x8, :lo12:{asmcgocall}]",
-            "blr     x8",
-            // result is in x0
-            // clean frame, this is a bit ugly - consider doing it at the end and just adjusting the return code offsets.
-            "ldp x29, x30, [sp, -0x8]",
-            "add sp, sp ,0x10",
-            // check return code
-            "cmn x0, 0xfff",
-            // jump to success if return code == 0
-            "b.cc 2f",
-            // syscall fail flow
-            "mov x4, -0x1",
-            "str x4, [sp, 0x40]",
-            "str xzr, [sp, 0x48]",
-            "neg x0, x0",
-            "str x0, [sp, 0x50]",
-            "ret",
-            // syscall success
-            "2:",
-            "str x0, [sp, 0x40]",
-            "str x1, [sp, 0x48]",
-            "str xzr, [sp, 0x50]",
-            "ret",
-            asmcgocall = sym FN_ASMCGOCALL,
-            syscall_handler = sym mirrord_syscall_handler
-        )
-    }
+    naked_asm!(
+        // save fp and lr to stack and reserve that memory.
+        // if we don't do it and remember to load it back before ret
+        // we will have unfortunate results
+        "str x30, [sp, -0x10]!",
+        "stur x29, [sp, -0x8]",
+        "sub x29, sp, 0x8",
+        // x1 should point to original sp+8, which should be the first parameter
+        "add x1, sp, 0x18",
+        // load mirrord_syscall_handler as first argument (x0)
+        "adr    x0, {syscall_handler}",
+        "adrp    x8, {asmcgocall}",
+        "ldr     x8, [x8, :lo12:{asmcgocall}]",
+        "blr     x8",
+        // result is in x0
+        // clean frame, this is a bit ugly - consider doing it at the end and just adjusting the return code offsets.
+        "ldp x29, x30, [sp, -0x8]",
+        "add sp, sp ,0x10",
+        // check return code
+        "cmn x0, 0xfff",
+        // jump to success if return code == 0
+        "b.cc 2f",
+        // syscall fail flow
+        "mov x4, -0x1",
+        "str x4, [sp, 0x40]",
+        "str xzr, [sp, 0x48]",
+        "neg x0, x0",
+        "str x0, [sp, 0x50]",
+        "ret",
+        // syscall success
+        "2:",
+        "str x0, [sp, 0x40]",
+        "str x1, [sp, 0x48]",
+        "str xzr, [sp, 0x50]",
+        "ret",
+        asmcgocall = sym FN_ASMCGOCALL,
+        syscall_handler = sym mirrord_syscall_handler
+    )
 }
 
 /// Hooks for when hooking a go binary between 1.19 and 1.23
