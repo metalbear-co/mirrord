6851c0b940e479aef6c52cfa804f26bee59deb87
Yuan Lyu <yuanl@metalbear.co>
2025-08-15 22:53:33 -0400

    Reuse or create new DB branches

--- mirrord/config/src/feature.rs
+++ mirrord/config/src/feature.rs
@@ -4,9 +4,13 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use self::{copy_target::CopyTargetConfig, env::EnvConfig, fs::FsConfig, network::NetworkConfig};
-use crate::{config::source::MirrordConfigSource, feature::split_queues::SplitQueuesConfig};
+use crate::{
+    config::source::MirrordConfigSource,
+    feature::{database_branches::DatabaseBranchesConfig, split_queues::SplitQueuesConfig},
+};
 
 pub mod copy_target;
+pub mod database_branches;
 pub mod env;
 pub mod fs;
 pub mod network;
@@ -107,6 +111,10 @@ pub struct FeatureConfig {
     /// will be used, and your local application will not receive any messages from that queue.
     #[config(nested, default, unstable)]
     pub split_queues: SplitQueuesConfig,
+
+    /// # feature.db_branches {#feature-db_branches}
+    #[config(nested, default, unstable)]
+    pub db_branches: DatabaseBranchesConfig,
 }
 
 impl CollectAnalytics for &FeatureConfig {
