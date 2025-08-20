26952b29d303d09be573db7dfda99177ab051bda
meowjesty <meowjesty_nyan@outlook.com>
2025-08-20 18:40:52 -0300

    Proper error message when the operator cannot be retrieved, and it's required.

--- mirrord/operator/src/client.rs
+++ mirrord/operator/src/client.rs
@@ -240,11 +240,16 @@ impl OperatorApi<NoClientCert> {
                 }));
             }
 
+            // kube api failed to get the operator, let's see if it's installed though
             Err(error @ kube::Error::Api(..)) => {
                 match discovery::operator_installed(&client).await {
+                    // operator is required, but we failed for some reason
+                    Err(..) if config.operator == Some(true) => error,
+                    // we tried to find the operator, it's not there, but it isn't required
                     Ok(false) | Err(..) => {
                         return Ok(None);
                     }
+                    // the operator is there, but we failed getting it
                     Ok(true) => error,
                 }
             }
