ce330a96588862cfe164d12502083fcad41a73f7
meowjesty <meowjesty_nyan@outlook.com>
2025-08-22 14:48:55 -0300

    use config in test

--- mirrord/layer/tests/issue3456.rs
+++ mirrord/layer/tests/issue3456.rs
@@ -17,12 +17,28 @@ async fn test_issue3456(
     #[values(Application::NodeIssue3456)] application: Application,
     dylib_path: &Path,
 ) {
+    let dir = tempfile::tempdir().unwrap();
+    let file_id = rand::random::<u64>();
+    let config_path = dir.path().join(format!("{file_id:X}.json"));
+
+    let config = serde_json::json!({
+        "experimental": {
+            "hook_rename": true
+        },
+        "feature": {
+            "fs": {
+                "mode": "write",
+                "read_write": "/tmp"
+            }
+        }
+    });
+
+    tokio::fs::write(&config_path, serde_json::to_string_pretty(&config).unwrap())
+        .await
+        .expect("failed to saving layer config to tmp file");
+
     let (mut test_process, mut intproxy) = application
-        .start_process_with_layer(
-            dylib_path,
-            vec![("MIRRORD_FILE_READ_WRITE_PATTERN", "/tmp")],
-            None,
-        )
+        .start_process_with_layer(dylib_path, Default::default(), Some(&config_path))
         .await;
 
     intproxy
