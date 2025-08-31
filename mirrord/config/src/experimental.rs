3d9fe2efe0e658558ce8ee5a7efdb1d3bce23872
Aviram Hassan <aviram@metalbear.co>
2025-08-31 13:17:49 +0300

    ..

--- mirrord/config/src/experimental.rs
+++ mirrord/config/src/experimental.rs
@@ -132,7 +132,7 @@ pub struct ExperimentalConfig {
     ///
     /// Forces hooking all instances of the connect function.
     /// In very niche cases the connect function has multiple exports and this flag
-    /// makes us hook all of the instances. https://linear.app/metalbear/issue/MBE-1385/mirrord-container-curl-doesnt-work-for-php-curl
+    /// makes us hook all of the instances. <https://linear.app/metalbear/issue/MBE-1385/mirrord-container-curl-doesnt-work-for-php-curl>
     #[config(default = false)]
     pub force_hook_connect: bool,
 }
