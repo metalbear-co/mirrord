5b51095c3082ecadcfa0b8d5a655196d812160cb
Yuan Lyu <yuanl@metalbear.co>
2025-09-05 15:39:47 -0400

    Rework config doc

--- mirrord/config/src/feature/database_branches.rs
+++ mirrord/config/src/feature/database_branches.rs
@@ -48,8 +48,6 @@ impl DatabaseBranchesConfig {
 
 /// Configuration for a database branch.
 ///
-/// By specifying a unique `id`, the branch database will be reused if not expired.
-///
 /// Example:
 ///
 /// ```json
@@ -70,36 +68,36 @@ impl DatabaseBranchesConfig {
 #[derive(MirrordConfig, Clone, Debug, Eq, PartialEq, JsonSchema, Serialize, Deserialize)]
 #[config(map_to = "DatabaseBranchFileConfig")]
 pub struct DatabaseBranchConfig {
-    /// <!--${internal}-->
-    /// A unique identifier chosen by the user. This is useful when reusing branch database
-    /// sharing among different Kubernetes users.
+    /// Users can choose to specify a unique `id`. This is useful for reusing or sharing
+    /// the same database branch among Kubernetes users.
     pub id: Option<String>,
 
-    /// <!--${internal}-->
-    /// Name of the database
+    /// When source database connection detail is not accessible to mirrord operator, users
+    /// can specify the database `name` so it is included in the connection options mirrord
+    /// uses as the override.
     pub name: Option<String>,
 
-    /// <!--${internal}-->
-    /// The time-to-live (TTL) for the branch database, in seconds. Defaults to 300 seconds.
+    /// Mirrord operator starts counting the TTL when a branch is no longer used by any session.
+    /// The time-to-live (TTL) for the branch database is set to 300 seconds by default.
+    /// Users can set `ttl_secs` to customize this value according to their need. Please note
+    /// that longer TTL paired with frequent mirrord session turnover can result in increased
+    /// resource usage. For this reason, branch database TTL caps out at 15 min.
     #[serde(default = "default_ttl_secs")]
     pub ttl_secs: u64,
 
-    /// <!--${internal}-->
-    /// The database type.
     #[serde(rename = "type")]
     pub _type: DatabaseType,
 
-    /// <!--${internal}-->
-    /// The database image version.
+    /// Mirrord operator uses a default version of the database image unless `version` is given.
     pub version: Option<String>,
 
-    /// <!--${internal}-->
-    /// The database connection details.
+    /// `connection` describes how to get the connection information to the source database.
+    /// When the branch database is ready for use, Mirrord operator will replace the connection
+    /// information with the branch database's.
     pub connection: ConnectionSource,
 }
 
-/// <!--${internal}-->
-/// Supported database types.
+/// Currently MySQL is the only supported database type.
 #[derive(Clone, Debug, Eq, PartialEq, JsonSchema, Serialize, Deserialize)]
 #[schemars(rename = "DbBranchingDatabaseType")]
 pub enum DatabaseType {
@@ -107,8 +105,21 @@ pub enum DatabaseType {
     MySql,
 }
 
-/// <!--${internal}-->
-/// Options for connecting to the database.
+/// Different ways of connecting to the source database.
+///
+/// Example:
+///
+/// A single complete connection URL stored in an environment variable accessible from
+/// the target pod template.
+///
+/// ```json
+/// {
+///   "url": {
+///     "type": "env",
+///     "variable": "DB_CONNECTION_URL"
+///   }
+/// }
+/// ```
 #[derive(Clone, Debug, Eq, PartialEq, JsonSchema, Serialize, Deserialize)]
 #[schemars(rename = "DbBranchingConnectionSource")]
 #[serde(rename_all = "snake_case")]
