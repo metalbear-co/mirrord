d69aba3bf77ceccb6ca769ffa99fafd8705cbee3
MintSoup <aregak2005@gmail.com>
2025-09-11 13:43:19 +0400

    Verify that wait(2) fails with ECHILD in test app

--- tests/intproxy_child/intproxy_child.c
+++ tests/intproxy_child/intproxy_child.c
@@ -1,6 +1,18 @@
+#include <asm-generic/errno-base.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/wait.h>
+#include <errno.h>
 
+// This is for verifying that mirrod-intproxy does not remain a child
+// of the user process
 int main() {
-	wait(0);
-	return 0;
+	int rv = wait(0);
+	if (rv == -1 && errno == ECHILD) {
+		return 0;
+	} else {
+		printf("ERROR: wait(2) returned %d\n", rv);
+		printf("errno = %d (%s)\n", errno, strerror(errno));
+		return 1;
+	}
 }
