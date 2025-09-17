9c5e095b0ae09abd4d2e9622ec7ca355a543d5b9
Razz4780 <msmolarekg@gmail.com>
2025-09-17 09:28:07 +0200

    Both

--- mirrord/operator/src/crd/patch.rs
+++ mirrord/operator/src/crd/patch.rs
@@ -3,7 +3,9 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 /// Represents operator patch state of some workload.
-#[derive(CustomResource, Clone, Debug, Deserialize, Serialize, JsonSchema)]
+///
+/// This is an aggregated state of accepted [`MirrordClusterWorkloadPatchRequest`]s.
+#[derive(CustomResource, Clone, Debug, Deserialize, Serialize, JsonSchema, Hash, PartialEq, Eq)]
 #[kube(
     group = "mirrord.metalbear.co",
     version = "v1alpha",
@@ -31,17 +33,14 @@ pub struct PatchedWorkloadRef {
     pub name: String,
 }
 
-/// Request for a patch to be applied to a Kubernetes workload.
+/// An accepted [`MirrordClusterWorkloadPatchRequest`], part of [`MirrordClusterWorkloadPatch`].
 ///
 /// Part of [`MirrordClusterWorkloadPatch`].
 #[derive(Clone, Debug, Deserialize, Serialize, JsonSchema, Hash, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct PatchRequest {
-    /// Opaque identifier of this request's owner.
-    pub owner: String,
-    /// If this patch request is only valid within a single operator instance,
-    /// this field stores its unique ID.
-    pub operator_instance_id: Option<String>,
+    /// UID of the origin [`MirrordClusterWorkloadPatchRequest`] resource.
+    pub uid: String,
     /// Environment variables to be set in the pod template.
     pub env_vars: Vec<EnvVar>,
     /// Number of replicas to be set in the workload spec.
@@ -56,4 +55,33 @@ pub struct EnvVar {
     pub container: String,
     pub variable: String,
     pub value: String,
+}
+
+/// Represents operator patch state of some workload.
+#[derive(CustomResource, Clone, Debug, Deserialize, Serialize, JsonSchema, Hash, PartialEq, Eq)]
+#[kube(
+    group = "mirrord.metalbear.co",
+    version = "v1alpha",
+    kind = "MirrordClusterWorkloadPatchRequest",
+    printcolumn = r#"{"name":"WORKLOAD-API-VERSION", "type":"string", "description":"API version of the workload.", "jsonPath":".spec.workloadRef.apiVersion"}"#,
+    printcolumn = r#"{"name":"WORKLOAD-KIND", "type":"string", "description":"Kind of the workload.", "jsonPath":".spec.workloadRef.kind"}"#,
+    printcolumn = r#"{"name":"WORKLOAD-NAME", "type":"string", "description":"Name of the workload.", "jsonPath":".spec.workloadRef.name"}"#,
+    printcolumn = r#"{"name":"WORKLOAD-NAMESPACE", "type":"string", "description":"Namespace of the workload.", "jsonPath":".spec.workloadRef.namespace"}"#,
+    status = "MirrordClusterWorkloadPatchRequestStatus"
+)]
+#[serde(rename_all = "camelCase")]
+pub struct MirrordClusterWorkloadPatchRequestSpec {
+    /// Reference to the workload being patched.
+    pub workload_ref: PatchedWorkloadRef,
+    /// Environment variables to be set in the pod template.
+    pub env_vars: Vec<EnvVar>,
+    /// Number of replicas to be set in the workload spec.
+    ///
+    /// [`None`] means that replicas should not be patched.
+    pub replicas: Option<i32>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, JsonSchema, Hash, PartialEq, Eq)]
+pub struct MirrordClusterWorkloadPatchRequestStatus {
+    accepted: bool,
 }
