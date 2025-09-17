04c9a57627ac98baee714cad66d65f3e553adcb5
Razz4780 <msmolarekg@gmail.com>
2025-09-17 13:30:32 +0200

    Docs and rename

--- mirrord/operator/src/crd/patch.rs
+++ mirrord/operator/src/crd/patch.rs
@@ -2,7 +2,7 @@ use kube::CustomResource;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-/// Represents operator patch state of some workload.
+/// Internal resource, representing operator patch state of some workload.
 ///
 /// This is an aggregated state of accepted [`MirrordClusterWorkloadPatchRequest`]s.
 #[derive(CustomResource, Clone, Debug, Deserialize, Serialize, JsonSchema, Hash, PartialEq, Eq)]
@@ -19,11 +19,14 @@ use serde::{Deserialize, Serialize};
 pub struct MirrordClusterWorkloadPatchSpec {
     /// Reference to the workload being patched.
     pub workload_ref: PatchedWorkloadRef,
-    /// Collection of individual [`PatchRequest`]s to be applied.
-    pub requests: Vec<PatchRequest>,
+    /// Collection of accepted [`MirrordClusterWorkloadPatchRequest`]s.
+    ///
+    /// These requests will be enforced on the workload.
+    pub requests: Vec<AcceptedPatchRequest>,
 }
 
-/// Reference to a Kubernetes workload patched with [`MirrordClusterWorkloadPatch`].
+/// Reference to a Kubernetes workload patched with [`MirrordClusterWorkloadPatch`] and
+/// [`MirrordClusterWorkloadPatchRequest`].
 #[derive(Clone, Debug, Deserialize, Serialize, JsonSchema, Hash, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct PatchedWorkloadRef {
@@ -34,11 +37,9 @@ pub struct PatchedWorkloadRef {
 }
 
 /// An accepted [`MirrordClusterWorkloadPatchRequest`], part of [`MirrordClusterWorkloadPatch`].
-///
-/// Part of [`MirrordClusterWorkloadPatch`].
 #[derive(Clone, Debug, Deserialize, Serialize, JsonSchema, Hash, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
-pub struct PatchRequest {
+pub struct AcceptedPatchRequest {
     /// UID of the origin [`MirrordClusterWorkloadPatchRequest`] resource.
     pub uid: String,
     /// Environment variables to be set in the pod template.
@@ -57,7 +58,7 @@ pub struct EnvVar {
     pub value: String,
 }
 
-/// Represents operator patch state of some workload.
+/// Internal resource, representing an operator request to patch some workload.
 #[derive(CustomResource, Clone, Debug, Deserialize, Serialize, JsonSchema, Hash, PartialEq, Eq)]
 #[kube(
     group = "mirrord.metalbear.co",
