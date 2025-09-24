7cdb00225bf3b209382cad7f48a11ce508e59754
Daniel Graf <danielg@metalbear.co>
2025-09-24 16:12:21 +0200

    revert to original line

--- mirrord/layer/src/socket/ops.rs
+++ mirrord/layer/src/socket/ops.rs
@@ -32,7 +32,9 @@ use nix::{
     sys::socket::{SockaddrIn, SockaddrIn6, SockaddrLike, SockaddrStorage, sockopt},
 };
 use socket2::SockAddr;
-use tracing::{Level, error, trace};
+#[cfg(debug_assertions)]
+use tracing::Level;
+use tracing::{error, trace};
 
 #[cfg(target_os = "macos")]
 use super::apple_dnsinfo::*;
