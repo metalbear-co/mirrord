bdffb24f61ac485acb1bc55a9e3f4b7cc1765a98
Daniel Graf <danielg@metalbear.co>
2025-09-25 09:32:39 +0200

    layer revert: begin

--- scripts/build_go_apps.sh
+++ scripts/build_go_apps.sh
@@ -11,39 +11,12 @@ then
     exit 1
 fi
 
-requested_minor="$1"
-requested_version="1.${requested_minor}"
-
 1>&2 echo "Using $(go version)"
 
 for go_mod in $(find . -name "go\.mod")
 do
-    directory=$(dirname "$go_mod")
-    cd "$directory"
-
-    required_version=$(awk '/^go[ \t]+/ {print $2; exit}' go.mod | tr -d ' ')
-    if [ -z "$required_version" ]; then
-        required_version="0.0"
-    fi
-
-    python3 - "$requested_version" "$required_version" <<'PY2'
-import sys
-
-def parse(ver: str):
-    return tuple(int(part) for part in ver.split('.'))
-
-requested = parse(sys.argv[1])
-required = parse(sys.argv[2])
-
-if requested < required:
-    sys.exit(1)
-PY2
-
-    if [ $? -ne 0 ]; then
-        1>&2 echo "Skipping $directory because it requires Go $required_version"
-        cd - 1>/dev/null
-        continue
-    fi
+    directory=$(dirname $go_mod)
+    cd $directory
 
     1>&2 echo "Building test app $directory/$1.go_test_app"
     go build -o "$1.go_test_app"
