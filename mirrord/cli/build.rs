3940ee6b1d5135f5e68951ed90be2686c8217b20
Daniel Graf <danielg@metalbear.co>
2025-09-26 10:30:41 +0200

    windows build and mirrord build.rs fixes

--- mirrord/cli/build.rs
+++ mirrord/cli/build.rs
@@ -1,11 +1,17 @@
+#[cfg(target_os = "windows")]
 use std::process::exit;
 
 fn main() {
     println!("cargo::rerun-if-env-changed=MIRRORD_LAYER_FILE_MACOS_ARM64");
     if std::env::var("MIRRORD_LAYER_FILE").is_err() {
+        const VAR_LAYER_FILE: &str = if cfg!(target_os = "windows") {
+            "CARGO_CDYLIB_FILE_MIRRORD_LAYER_WIN"
+        } else {
+            "CARGO_CDYLIB_FILE_MIRRORD_LAYER"
+        };
         println!(
             "cargo:rustc-env=MIRRORD_LAYER_FILE={}",
-            std::env::var("CARGO_CDYLIB_FILE_MIRRORD_LAYER").unwrap()
+            std::env::var(VAR_LAYER_FILE).unwrap()
         );
     };
 
