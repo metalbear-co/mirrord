3ebdfae64c5adbf97d03286d06c896176dd8f4ca
Razz4780 <msmolarekg@gmail.com>
2025-09-29 20:14:09 +0200

    Extended session CRD

--- mirrord/operator/src/crd/session.rs
+++ mirrord/operator/src/crd/session.rs
@@ -1,3 +1,6 @@
+use std::{collections::BTreeMap, net::SocketAddr};
+
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::MicroTime;
 use kube::CustomResource;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -6,24 +9,32 @@ use serde::{Deserialize, Serialize};
 #[kube(
     group = "mirrord.metalbear.co",
     version = "v1alpha",
-    kind = "MirrordClusterSession"
+    kind = "MirrordClusterSession",
+    status = "MirrordClusterSessionStatus"
 )]
 #[serde(rename_all = "camelCase")]
 pub struct MirrordClusterSessionSpec {
-    /// Resources needed to report session metrics to the mirrord Jira app
+    /// Resources needed to report this session's metrics to the mirrord Jira app.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub jira_metrics_resources: Option<JiraMetricsResources>,
 
     /// Owner of this session
-    pub owner: MirrordClusterSessionOwner,
+    pub owner: SessionOwner,
 
-    /// Session's [`Target`](mirrord_config::target::Target)
-    pub target: SessionTarget,
+    /// Namespace of this session.
+    pub namespace: String,
+
+    /// Target of this session.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub target: Option<SessionTarget>,
+
+    /// mirrord features used in this session.
+    pub features: SessionFeatures,
 }
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
-pub struct MirrordClusterSessionOwner {
+pub struct SessionOwner {
     /// Unique ID.
     pub user_id: String,
     /// Creator local username.
@@ -38,31 +49,109 @@ pub struct MirrordClusterSessionOwner {
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct SessionTarget {
-    /// Target's Resource apiVersion
+    /// Target resource apiVersion
     pub api_version: String,
-
-    /// Target's Resource Kind
+    /// Target resource kind.
     pub kind: String,
-
-    /// Target Namespace
-    pub namespace: String,
-
-    /// Target Name (will be empty for targetless)
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub name: Option<String>,
-
-    /// Target Container
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub container: Option<String>,
+    /// Target resource name.
+    pub name: String,
+    /// Target container name.
+    pub container: String,
 }
 
-/// Resources needed to report session metrics to the mirrord Jira app
+/// Resources needed to report session metrics to the mirrord Jira app.
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct JiraMetricsResources {
-    /// The Jira webhook URL, used to update total session time in the mirrord Jira app
-    pub jira_webhook_url: String,
-
-    /// The user's current git branch, used for sending session metrics to mirrord Jira app
+    /// The user's current git branch.
     pub branch_name: String,
+}
+
+/// Describes the set of mirrord features used in a mirrord session.
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct SessionFeatures {
+    pub copy_target: SessionCopyTarget,
+    pub sqs_splitting: SessionQueueSplitting,
+    pub kafka_splitting: SessionQueueSplitting,
+    pub mysql_branching: SessionMySqlBranching,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct SessionCopyTarget {
+    pub enabled: bool,
+    pub scale_down: bool,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct SessionQueueSplitting {
+    #[serde(skip_serializing_if = "BTreeMap::is_empty", default)]
+    pub filters: BTreeMap<String, BTreeMap<String, String>>,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct SessionMySqlBranching {
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
+    pub branches: Vec<String>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct MirrordClusterSessionStatus {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub connected_timestamp: Option<MicroTime>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub copy_target: Option<SessionFeatureStatus<SessionCopiedPodReady>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub sqs_splitting_status: Option<SessionFeatureStatus<SessionEnvOverrides>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub kafka_splitting_status: Option<SessionFeatureStatus<SessionEnvOverrides>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub mysql_branching_status: Option<SessionFeatureStatus<SessionEnvOverrides>>,
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
+    pub agents: Vec<SessionAgent>,
+}
+
+/// Generic status of preparing a session feature.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub enum SessionFeatureStatus<T> {
+    Ready(T),
+    Error(FatalError),
+}
+
+/// Describes an output of preparing the copy target session feature - copied pod to use as a
+/// target.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct SessionCopiedPodReady {
+    pub pod: String,
+    pub container: String,
+}
+
+/// Describes an output of preparing a session feature - a list of environment overrides.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct SessionEnvOverrides {
+    pub env_overrides: BTreeMap<String, String>,
+}
+
+/// Describes a fatal error that has occurred when preparing a session feature.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct FatalError {
+    pub code: u16,
+    pub reason: String,
+    pub message: String,
+}
+
+/// Describes a mirrord agent spawned for a session.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct SessionAgent {
+    /// Address on which the agent accepts mirrord-protocol connections.
+    pub address: SocketAddr,
 }
