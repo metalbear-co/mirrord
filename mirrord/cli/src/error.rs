7c9fcb605dfaedcae986a83aa17e1252713cab65
Vladislav Rashkov <vladislavrashkov@Vladislavs-MacBook-Pro-2.local>
2025-10-14 18:52:19 +0300

    cargo fmt

--- mirrord/cli/src/error.rs
+++ mirrord/cli/src/error.rs
@@ -473,7 +473,9 @@ pub(crate) enum CliError {
     NestedExec,
 
     #[error("Database branch error: {0}")]
-    #[diagnostic(help("Make sure the database branch exists and the operator is running correctly."))]
+    #[diagnostic(help(
+        "Make sure the database branch exists and the operator is running correctly."
+    ))]
     DbBranchNotFound(String),
 
     #[error("The '{0}' command is not currently supported on Windows")]
