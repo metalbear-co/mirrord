9e4ea6553e9244e828840135c4ed4739cf3e00fe
Gemma Tipper <gemmat@metalbear.co>
2025-10-21 11:55:08 +0100

    Add config type generation to CI

--- wizard-frontend/eslint.config.js
+++ wizard-frontend/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "**/mirrord-schema.d.ts"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
