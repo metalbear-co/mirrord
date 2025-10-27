3c0d8e9e4cbc28e0dcf3778456fcc2e048666aa6
MintSoup <aregak2005@gmail.com>
2025-10-27 16:53:44 +0400

    Remove unused ProtocolError variant

--- mirrord/protocol-io/src/lib.rs
+++ mirrord/protocol-io/src/lib.rs
@@ -49,8 +49,6 @@ pub trait ProtocolEndpoint: 'static + Sized + Clone {
 pub enum ProtocolError {
     #[error("IO error: {0}")]
     IO(#[from] io::Error),
-    #[error("unexpected peer message")]
-    UnexpectedPeerMessage,
 }
 
 #[derive(Clone)]
