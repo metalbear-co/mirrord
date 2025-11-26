a13b2b73aae62c6cd527969975c06e7f281626f0
Gemma Tipper <gemmat@metalbear.co>
2025-11-26 13:18:27 +0000

    Wizard frontend: query backend for isReturning

--- wizard-frontend/src/App.tsx
+++ wizard-frontend/src/App.tsx
@@ -1,31 +1,56 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import NotFound from "./pages/NotFound";
-import {UserDataContext} from "./components/UserDataContext";
+import { UserDataContext } from "./components/UserDataContext";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    {/* value refers to isReturning */}
-    <UserDataContext.Provider value={false}>
+const InnerApp = () => {
+  const { error, isLoading, data } = useQuery({
+    queryKey: ["userIsReturning"],
+    queryFn: () =>
+      fetch(window.location.href + "api/v1/is-returning").then(async (res) =>
+        res.ok && (await res.text()) === "true" ? true : false
+      ),
+  });
+
+  if (error) {
+    console.log(error)
+  }
+
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <UserDataContext.Provider value={data}>
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Homepage/>} />
+            <Route path="/" element={<Homepage />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
     </UserDataContext.Provider>
-  </QueryClientProvider>
-);
+  );
+};
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <InnerApp />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
