b0895b76b36d1391bf92f6c4a7b397b71ae46e20
Pavel Zagalsky <zagalsky@gmail.com>
2025-12-08 21:24:04 +0700

    more ps1 fixes

--- scripts/win_build_test_apps.ps1
+++ scripts/win_build_test_apps.ps1
@@ -14,7 +14,7 @@ if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
 }
 
 Write-Host 'Installing dependencies: nodejs, go, python3, curl, mingw, cmake, nasm, llvm'
-choco install -y nodejs go python3 curl mingw cmake nasm llvm
+choco install -y --force nodejs go python3 curl mingw cmake nasm llvm
 
 Write-Host 'Installing Python packages required by tests'
 python -m pip install --upgrade pip
