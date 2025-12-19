66b7eeb94b980fccb4d786940f8ef19cf8f29ae7
Aviram Hassan <aviram@metalbear.co>
2025-12-20 00:08:44 +0200

    ..

--- mirrord/layer/src/hooks.rs
+++ mirrord/layer/src/hooks.rs
@@ -208,6 +208,7 @@ impl<'a> HookManager<'a> {
         writer.put_nop();
         writer.put_nop();
         writer.put_nop();
+        writer.put_nop();
         writer.flush();
         function = NativePointer(writer.code_offset() as *mut libc::c_void);
         drop(writer);
