00e6a63cac3b9f1da8a4e3939abd4d016997a980
Aviram Hassan <aviram@metalbear.co>
2025-12-22 14:37:44 +0200

    ..

--- mirrord/cli/src/browser.rs
+++ mirrord/cli/src/browser.rs
@@ -62,7 +62,9 @@ where
             progress.failure(None);
         }
     } else {
-        progress.failure(Some(format!("cannot find Google Chrome - you can use link in Chrome to set {url}")));
+        progress.failure(Some(format!(
+            "cannot find Google Chrome - you can use link in Chrome to set {url}"
+        )));
     }
 }
 
