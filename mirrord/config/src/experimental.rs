76db3cb6e4dc9f29b20df9fc009d21692acf85e2
Yuan Lyu <yuanl@metalbear.com>
2025-12-24 20:36:37 -0500

    Update mirrord-schema.json

--- mirrord/config/src/experimental.rs
+++ mirrord/config/src/experimental.rs
@@ -138,7 +138,6 @@ pub struct ExperimentalConfig {
     #[config(default = false)]
     pub dlopen_cgo: bool,
 
-    #[cfg(target_os = "macos")]
     /// ### _experimental_ applev {#experimental-applev}
     ///
     /// Configuraiton for inspecting and modifying apple variables. macOS only.
@@ -164,11 +163,9 @@ impl CollectAnalytics for &ExperimentalConfig {
         analytics.add("force_hook_connect", self.force_hook_connect);
         analytics.add("non_blocking_tcp_connect", self.non_blocking_tcp_connect);
         analytics.add("dlopen_cgo", self.dlopen_cgo);
-        #[cfg(target_os = "macos")]
         analytics.add("applev", self.applev.is_some());
     }
 }
 
-#[cfg(target_os = "macos")]
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, JsonSchema, Default)]
-pub struct AppleVariablesConfig;
+pub struct AppleVariablesConfig {}
