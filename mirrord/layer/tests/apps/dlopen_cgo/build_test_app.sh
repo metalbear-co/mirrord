eee060e1240a3e4fa5421a81d95f159835d48afd
Yuan Lyu <yuanl@metalbear.com>
2026-01-08 00:27:00 -0500

    Update dlopen unit test
    Added a new cgo lib that does basic file ops.
    The cpp app now dlopen both cgo libs.

--- mirrord/layer/tests/apps/dlopen_cgo/build_test_app.sh
+++ mirrord/layer/tests/apps/dlopen_cgo/build_test_app.sh
@@ -4,15 +4,24 @@ set -euo pipefail
 # Resolve directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-GO_FILE="$SCRIPT_DIR/main.go"
+SERVER_GO_FILE="$SCRIPT_DIR/server/main.go"
+SERVER_SO_FILE="$SCRIPT_DIR/server/libgo_server.so"
+
+FILEOPS_GO_FILE="$SCRIPT_DIR/fileops/main.go"
+FILEOPS_SO_FILE="$SCRIPT_DIR/fileops/libgo_fileops.so"
+
 CPP_FILE="$SCRIPT_DIR/main.cpp"
-SO_FILE="$SCRIPT_DIR/libgo_server.so"
 OUT_BIN="$SCRIPT_DIR/out.cpp_dlopen_cgo"
 
 echo "Script directory: $SCRIPT_DIR"
 
 # Ensure files exist
-if [[ ! -f "$GO_FILE" ]]; then
+if [[ ! -f "$SERVER_GO_FILE" ]]; then
+    echo "ERROR: main.go not found in $SCRIPT_DIR"
+    exit 1
+fi
+
+if [[ ! -f "$FILEOPS_GO_FILE" ]]; then
     echo "ERROR: main.go not found in $SCRIPT_DIR"
     exit 1
 fi
@@ -22,13 +31,18 @@ if [[ ! -f "$CPP_FILE" ]]; then
     exit 1
 fi
 
-echo "Building Go shared library..."
-go build -buildmode=c-shared -o "$SO_FILE" "$GO_FILE"
+go version
+echo "Building Go shared server library..."
+go build -buildmode=c-shared -o "$SERVER_SO_FILE" "$SERVER_GO_FILE"
+
+echo "Building Go shared file ops library..."
+go build -buildmode=c-shared -o "$FILEOPS_SO_FILE" "$FILEOPS_GO_FILE"
 
 echo "Building C++ loader app..."
 g++ "$CPP_FILE" -o "$OUT_BIN" -ldl
 
 echo "Done!"
 echo "Artifacts:"
-echo " - $SO_FILE"
+echo " - $SERVER_SO_FILE"
+echo " - $FILEOPS_SO_FILE"
 echo " - $OUT_BIN"
