51a0e66d6f6c621312623c0aff132c50f3e7126f
MintSoup <aregak2005@gmail.com>
2026-01-16 14:04:51 +0400

    Only apply to the currently selected user

--- mirrord/cli/src/fix.rs
+++ mirrord/cli/src/fix.rs
@@ -41,6 +41,27 @@ pub enum FixKubeconfigError {
 
     #[error("absolute path {0:?} contains non-unicode characters")]
     StringConversion(OsString),
+
+    #[error("there is no `current-context` field in kubeconfig")]
+    NoCurrentCtx,
+
+    #[error("`current-context` ({0:?}) refers to a nonexistent context")]
+    MissingCtx(String),
+
+    #[error("currently selected context {0:?} is malformed")]
+    MalformedCtx(String),
+
+    #[error("currently selected context {ctx:?} mentions user {user:?}, which does not exist")]
+    MissingUser { ctx: String, user: String },
+
+    #[error(
+        "user {user}'s auth config uses {cmd_path:?}, which cannot be resolved using the current $PATH ({err})"
+    )]
+    CannotResolve {
+        user: String,
+        cmd_path: PathBuf,
+        err: which::Error,
+    },
 }
 
 async fn fix_kubeconfig(args: FixKubeconfig) -> Result<(), FixKubeconfigError> {
@@ -51,76 +72,74 @@ async fn fix_kubeconfig(args: FixKubeconfig) -> Result<(), FixKubeconfigError> {
     );
     let config = Kubeconfig::read_from(&kubeconfig_path)?;
 
-    for (user_idx, user) in config.auth_infos.iter().enumerate() {
-        let Some(auth) = &user.auth_info else {
-            continue;
-        };
+    let current_ctx_name = config
+        .current_context
+        .ok_or(FixKubeconfigError::NoCurrentCtx)?;
 
-        let Some(exec) = &auth.exec else {
-            continue;
-        };
+    let current_ctx_entry = config
+        .contexts
+        .into_iter()
+        .find(|ctx| ctx.name == current_ctx_name)
+        .ok_or_else(|| FixKubeconfigError::MissingCtx(current_ctx_name.clone()))?;
 
-        let Some(command) = &exec.command else {
-            continue;
-        };
+    let current_ctx = current_ctx_entry
+        .context
+        .ok_or_else(|| FixKubeconfigError::MalformedCtx(current_ctx_name.clone()))?;
 
-        let cmd_path = PathBuf::from(command);
+    let Some(current_user_name) = current_ctx.user else {
+        println!("Currently selected context has no user credentials");
+        return Ok(());
+    };
 
-        if cmd_path.is_absolute() {
-            continue;
+    let (current_user_idx, current_user) = config
+        .auth_infos
+        .into_iter()
+        .enumerate()
+        .find(|(_, u)| u.name == *current_user_name)
+        .ok_or_else(|| FixKubeconfigError::MissingUser {
+            ctx: current_ctx_name.clone(),
+            user: current_user_name.clone(),
+        })?;
+
+    let command: Option<_> = try { current_user.auth_info?.exec?.command? };
+
+    let command = match command {
+        Some(cmd) => cmd,
+        None => {
+            println!("Currently selected user has no `exec` command, nothing to do.");
+            return Ok(());
         }
+    };
 
-        let absolute = match which::which(&cmd_path) {
-            Ok(path) => path
-                .into_os_string()
-                .into_string()
-                .map_err(FixKubeconfigError::StringConversion)?,
-            Err(err) => {
-                println!(
-                    "WARNING: user `{}` has an auth config that uses `exec` with non-absolute binary {cmd_path:?}, which cannot be resolved using the current $PATH ({err})",
-                    user.name
-                );
-                continue;
-            }
-        };
+    let cmd_path = PathBuf::from(command);
 
-        let replace = loop {
-            print!(
-                "User `{}` has an auth config that uses `exec` with non-absolute binary {cmd_path:?}, located at {absolute:?}. Replace with absolute path? [y/n] ",
-                user.name
-            );
-            stdout().flush()?;
-
-            let mut answer = String::new();
-            let mut reader = BufReader::new(stdin());
-            reader.read_line(&mut answer)?;
-
-            match &*answer {
-                "y\n" => {
-                    break true;
-                }
-                "n\n" => {
-                    break false;
-                }
-                _ => {}
-            }
-        };
-
-        if !replace {
-            continue;
-        }
-
-        let document = Document::new(tokio::fs::read_to_string(&kubeconfig_path).await?)?;
-
-        let patch = Patch {
-            route: route!("users", user_idx, "user", "exec", "command"),
-            operation: Op::Replace(Value::String(absolute)),
-        };
-
-        let patched = apply_yaml_patches(&document, &[patch])?;
-
-        tokio::fs::write(&kubeconfig_path, patched.source()).await?;
+    if cmd_path.is_absolute() {
+        println!("Currently selected user's `exec` command already uses an absolute path.");
+        return Ok(());
     }
+
+    let absolute = which::which(&cmd_path)
+        .map_err(|err| FixKubeconfigError::CannotResolve {
+            user: current_user_name.clone(),
+            cmd_path: cmd_path.clone(),
+            err,
+        })?
+        .into_os_string()
+        .into_string()
+        .map_err(FixKubeconfigError::StringConversion)?;
+
+    let document = Document::new(tokio::fs::read_to_string(&kubeconfig_path).await?)?;
+
+    let patch = Patch {
+        route: route!("users", current_user_idx, "user", "exec", "command"),
+        operation: Op::Replace(Value::String(absolute.clone())),
+    };
+
+    let patched = apply_yaml_patches(&document, &[patch])?;
+
+    tokio::fs::write(&kubeconfig_path, patched.source()).await?;
+
+    println!("Replaced user {current_ctx_name:?}'s exec {cmd_path:?} with {absolute:?}");
 
     Ok(())
 }
