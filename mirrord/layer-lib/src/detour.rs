cf07c9adbe3035e99e5e6fb8a71aca8d6464e6dc
Daniel Graf <danielg@metalbear.co>
2026-01-26 13:34:14 +0100

    weird docs

--- mirrord/layer-lib/src/detour.rs
+++ mirrord/layer-lib/src/detour.rs
@@ -466,7 +466,8 @@ pub trait OptionDetourExt<T>: OptionExt {
     /// # Examples
     ///
     /// ```no_run
-    /// let x: Detour<Option<i32>> = Detour::Sucess(Some(5));
+    /// use mirrord_layer_lib::detour::Detour;
+    /// let x: Detour<Option<i32>> = Detour::Success(Some(5));
     /// let y: Option<Detour<i32>> = Some(Detour::Success(5));
     /// assert_eq!(x, y.transpose());
     /// ```
