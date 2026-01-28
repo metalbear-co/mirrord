9001cd329532c3c550a6cc6663a7c8042135fc85
Daniel Graf <danielg@metalbear.co>
2026-01-28 11:49:12 +0100

    remove-mutate-insert

--- mirrord/layer-lib/src/socket/sockets.rs
+++ mirrord/layer-lib/src/socket/sockets.rs
@@ -174,10 +174,11 @@ pub fn set_socket_state(socket: SocketDescriptor, new_state: SocketState) {
         }
     };
 
-    if let Some(socket_ref) = sockets.get_mut(&socket) {
-        let socket_mut = Arc::get_mut(socket_ref)
-            .expect("SocketManager: socket Arc unexpectedly shared during state update");
-        socket_mut.state = new_state;
+    if let Some(socket_entry) = sockets.remove(&socket) {
+        let mut socket_inner =
+            Arc::try_unwrap(socket_entry).expect("SocketManager: socket Arc unexpectedly shared during state update");
+        socket_inner.state = new_state;
+        sockets.insert(socket, Arc::new(socket_inner));
         tracing::debug!("SocketManager: Updated socket {} state", socket);
     } else {
         tracing::warn!(
