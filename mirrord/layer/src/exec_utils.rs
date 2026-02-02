d1fabc7ffaae36a206395768e959f7cfd5df007c
MintSoup <aregak2005@gmail.com>
2026-02-02 18:16:32 +0400

    Create new connection in posix_spawn

--- mirrord/layer/src/exec_utils.rs
+++ mirrord/layer/src/exec_utils.rs
@@ -3,6 +3,7 @@ use std::{
     ffi::{CStr, CString, c_void},
     mem::zeroed,
     ops::{BitAnd, Not},
+    os::fd::AsRawFd,
     path::PathBuf,
     sync::OnceLock,
 };
@@ -12,25 +13,26 @@ use libc::{
     posix_spawn_file_actions_init, posix_spawn_file_actions_t, posix_spawnattr_getflags,
     posix_spawnattr_t,
 };
+use mirrord_intproxy_protocol::NewSessionRequest;
 use mirrord_layer_macro::{hook_fn, hook_guard_fn};
 use mirrord_sip::{MIRRORD_PATCH_DIR, SipError, SipPatchOptions, sip_patch};
 use null_terminated::Nul;
 use tracing::{info, trace, warn};
 
 use crate::{
-    EXECUTABLE_ARGS,
+    EXECUTABLE_ARGS, PROXY_CONNECTION, PROXY_CONNECTION_TIMEOUT,
     common::{CheckedInto, proxy_conn_fd, strip_mirrord_path},
     detour::{
         Bypass::{
             ExecOnNonExistingFile, FileOperationInMirrordBinTempDir, NoSipDetected, TooManyArgs,
         },
-        Detour,
-        Detour::{Bypass, Error, Success},
+        Detour::{self, Bypass, Error, Success},
     },
     error::HookError,
     exec_hooks::{hooks, *},
     graceful_exit,
     hooks::HookManager,
+    proxy_connection::ProxyConnection,
     replace,
 };
 
@@ -249,6 +251,14 @@ pub(crate) unsafe fn patch_sip_for_new_process(
     }
 }
 
+unsafe extern "C" {
+    // not exposed by libc crate :(
+    fn posix_spawn_file_actions_addinherit_np(
+        file_actions: *mut libc::posix_spawn_file_actions_t,
+        fd: c_int,
+    ) -> c_int;
+}
+
 /// Hook for `libc::posix_spawn`.
 /// Same as `execve_detour`, with all the extra arguments present here being passed untouched.
 // TODO: do we also need to hook posix_spawnp?
@@ -267,35 +277,41 @@ pub(crate) unsafe extern "C" fn posix_spawn_detour(
         let mut file_actions_buf: posix_spawn_file_actions_t = zeroed();
         let mut file_actions_buf_used = false;
 
-        if let Some(proxy_conn_fd) = proxy_conn_fd() {
-            // Check if POSIX_SPAWN_CLOEXEC_DEFAULT flag is set,
-            // and if so, add the intproxy connection fd to inherit list
-            // so it won't get closed.
-            let mut flags: c_short = 0;
-            if attrp.is_null().not()
-                && posix_spawnattr_getflags(attrp, &mut flags) == 0
-                && flags.bitand(POSIX_SPAWN_CLOEXEC_DEFAULT as c_short) != 0
-            {
-                if file_actions.is_null() {
-                    posix_spawn_file_actions_init(&mut file_actions_buf);
-                    file_actions_buf_used = true;
-                    file_actions = &mut file_actions_buf as *mut _;
-                }
+        let parent_layer = PROXY_CONNECTION.get().map(|t| t.layer_id());
+        let conn = ProxyConnection::new(
+            crate::setup().proxy_address(),
+            NewSessionRequest {
+                parent_layer,
+                process_info: EXECUTABLE_ARGS
+                    .get()
+                    .expect("should always be set in layer constructor")
+                    .to_process_info(crate::setup().layer_config()),
+            },
+            PROXY_CONNECTION_TIMEOUT
+                .get()
+                .copied()
+                .expect("PROXY_CONNECTION_TIMEOUT should be set by now!"),
+        )
+        .expect("failed to create new intproxy connection");
 
-                unsafe extern "C" {
-                    // not exposed by libc crate :(
-                    fn posix_spawn_file_actions_addinherit_np(
-                        file_actions: *mut libc::posix_spawn_file_actions_t,
-                        fd: c_int,
-                    ) -> c_int;
-                }
-                posix_spawn_file_actions_addinherit_np(file_actions, proxy_conn_fd);
-            };
+        let new_conn_fd = conn.as_raw_fd();
+
+        // Add the intproxy connection fd to inherit list so it won't
+        // get closed.
+        if file_actions.is_null() {
+            posix_spawn_file_actions_init(&mut file_actions_buf);
+            file_actions_buf_used = true;
+            file_actions = &mut file_actions_buf as *mut _;
         }
+
+        posix_spawn_file_actions_addinherit_np(file_actions, new_conn_fd);
+
+        // Forget the connection so it's not closed by the drop impl.
+        std::mem::forget(conn);
 
         let result = match patch_sip_for_new_process(path, argv, envp) {
             Detour::Success((path, argv, envp)) => {
-                match hooks::prepare_execve_envp(Detour::Success(envp.clone())) {
+                match hooks::prepare_execve_envp(Detour::Success(envp.clone()), Some(new_conn_fd)) {
                     Detour::Success(envp) => FN_POSIX_SPAWN(
                         pid,
                         path.into_raw().cast_const(),
