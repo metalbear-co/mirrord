40f393f290bca6e27cfb8380e2414e59101862bc
Yuan Lyu <yuanl@metalbear.com>
2026-02-07 10:39:44 -0500

    Compile test only on unix platform

--- mirrord/layer/tests/apps/double_listen/src/main.rs
+++ mirrord/layer/tests/apps/double_listen/src/main.rs
@@ -1,5 +1,7 @@
+#[cfg(target_family = "unix")]
 use std::{fs::File, net::TcpListener, os::fd::AsRawFd};
 
+#[cfg(target_family = "unix")]
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4567").expect("tcp listener bind");
 
@@ -10,4 +12,10 @@ fn main() {
 
     // trigger a trivial proxy message
     File::open("/why_double_listen").expect("file open failed");
+}
+
+#[cfg(not(target_family = "unix"))]
+fn main() {
+    eprintln!("ERROR: test double_listen is not supported on non-Unix platforms");
+    std::process::exit(1);
 }
