ce9e0799585cc7d6162ae584e1ec7285b9f6b340
Daniel Graf <danielg@metalbear.co>
2026-02-24 16:14:33 +0100

    we only need mirrord, layer library would be part of it

--- mirrord/layer-tests/build.rs
+++ mirrord/layer-tests/build.rs
@@ -1,34 +1,11 @@
-fn recheck_and_setup_layer_file() {
-    println!("cargo::rerun-if-env-changed=MIRRORD_LAYER_FILE");
-
-    if std::env::var("MIRRORD_LAYER_FILE").is_err() {
-        let layer_path = if cfg!(windows) {
-            std::env::var("CARGO_CDYLIB_FILE_MIRRORD_LAYER_WIN").unwrap()
-        } else {
-            std::env::var("CARGO_CDYLIB_FILE_MIRRORD_LAYER").unwrap()
-        };
-        unsafe { std::env::set_var("MIRRORD_LAYER_FILE", layer_path) };
-    };
-    println!(
-        "cargo:rustc-env=MIRRORD_LAYER_FILE={}",
-        std::env::var("MIRRORD_LAYER_FILE").unwrap()
-    );
-}
-
-/// By default this crate enables the `local-test-artifacts` feature, which
-/// pulls in the build-dependencies for `mirrord` and the layer crates, and uses
-/// `CARGO_CDYLIB_FILE_*` to populate `MIRRORD_TEST_USE_EXISTING_LIB` and
-/// `MIRRORD_TESTS_USE_BINARY` from the artifacts' computed path.
-/// In CI you can disable `local-test-artifacts` and
-/// provide the env vars to avoid costly redundant rebuilds.
+/// By default this crate enables the `build-artifacts` feature, which
+/// pulls in the build-dependencies for `mirrord` to populate
+/// `MIRRORD_TESTS_USE_BINARY` from the artifact computed path.
+/// In CI you can disable `build-artifacts` and
+/// provide the env var to avoid costly redundant rebuilds.
 fn main() {
-    recheck_and_setup_layer_file();
-    println!(
-        "cargo:rustc-env=MIRRORD_TEST_USE_EXISTING_LIB={}",
-        std::env::var("MIRRORD_LAYER_FILE").unwrap()
-    );
-    println!(
-        "cargo:rustc-env=MIRRORD_TESTS_USE_BINARY={}",
-        std::env::var("CARGO_BIN_FILE_MIRRORD").unwrap()
-    );
+    // assert binary mirrord_tests::utils::run_command::run_mirrord
+    option_env!("CARGO_BIN_FILE_MIRRORD")
+        .or_else(|| option_env!("MIRRORD_TESTS_USE_BINARY"))
+        .expect("Mirrord Binary is required, either provide MIRRORD_TESTS_USE_BINARY or permit layer-tests default features");
 }
