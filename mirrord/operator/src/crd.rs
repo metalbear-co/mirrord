9afc2fd7fd5487ccce7124ae37ed8e448a307b89
Razz4780 <msmolarekg@gmail.com>
2026-03-19 11:03:26 +0100

    MirrordProperties CRD

--- mirrord/operator/src/crd.rs
+++ mirrord/operator/src/crd.rs
@@ -34,6 +34,7 @@ pub mod patch;
 pub mod policy;
 pub mod preview;
 pub mod profile;
+pub mod properties;
 pub mod session;
 pub mod steal_tls;
 
@@ -952,6 +953,7 @@ mod tests {
         policy::{MirrordClusterPolicy, MirrordPolicy},
         preview::PreviewSession,
         profile::{MirrordClusterProfile, MirrordProfile},
+        properties::MirrordPropertyList,
         session::MirrordClusterSession,
         steal_tls::{MirrordClusterTlsStealConfig, MirrordTlsStealConfig},
     };
@@ -995,5 +997,6 @@ mod tests {
         write_crd_yaml::<MirrordProfile>();
         write_crd_yaml::<MirrordTlsStealConfig>();
         write_crd_yaml::<MirrordClusterTlsStealConfig>();
+        write_crd_yaml::<MirrordPropertyList>();
     }
 }
