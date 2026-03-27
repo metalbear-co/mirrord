214517cdd093bce96152ab5e0787ed36f411854c
meowjesty <43983236+meowjesty@users.noreply.github.com>
2026-03-27 16:54:42 -0300

    `mirrord ci container` command. (#4084)
    * Support mirrord for ci container.
    
    * mirrord ci container starts the ci handling (cant stop it yet though)
    
    * kinda works, but intproxy lives forever
    
    * simplify some stuff and docs
    
    * logs and docs
    
    * changelog
    
    * vale
    
    * fix progress infinite loop
    
    * lil docs for foreground progress issue and better exit
    
    * more docs and have it pass ci
    
    * windows pls
    
    * more windows clippy
    
    * allow unused in stop windows
    
    * will this type of cfg work?
    
    * fix merge
    
    * more consistent cfg
    
    * getting rid of more cfgs
    
    * no more maps
    
    * fix docs add docs
    
    * update docs
    
    * nicer return type
    
    * more docs

--- mirrord/cli/src/config.rs
+++ mirrord/cli/src/config.rs
@@ -26,6 +26,11 @@ use mirrord_config::{
     target::TargetType,
 };
 use thiserror::Error;
+
+use crate::config::ci::CiArgs;
+
+pub(crate) mod ci;
+
 /// Macro to automatically handle Windows unsupported commands.
 /// Usage: `windows_unsupported!(args, "command_name", { command_execution })`
 #[macro_export]
@@ -453,31 +458,6 @@ pub(super) struct DumpArgs {
     /// Can be specified multiple times.
     #[arg(short = 'p', long)]
     pub ports: Vec<u16>,
-}
-
-// `mirrord ci start` command
-#[derive(Args, Debug)]
-pub(super) struct CiStartArgs {
-    /// Args passed down to mirrord itself (similar to `mirrord exec`).
-    #[clap(flatten)]
-    pub exec_args: Box<ExecArgs>,
-
-    /// Runs mirrord ci in the foreground (the default behaviour is to run it as a background
-    /// task).
-    #[arg(long)]
-    pub foreground: bool,
-
-    /// CI environment, e.g. "staging", "production", "testing", etc.
-    #[arg(long)]
-    pub environment: Option<String>,
-
-    /// CI pipeline or job name, e.g. "e2e-tests".
-    #[arg(long)]
-    pub pipeline: Option<String>,
-
-    /// CI pipeline trigger, e.g. "push", "pull request", "manual", etc.
-    #[arg(long)]
-    pub triggered_by: Option<String>,
 }
 
 /// Target-related parameters, present in more than one command.
@@ -1054,34 +1034,6 @@ pub(super) struct VpnArgs {
     /// Path to resolver (macOS)
     #[arg(long, default_value = "/etc/resolver")]
     pub resolver_path: PathBuf,
-}
-
-#[derive(Args, Debug)]
-pub(super) struct CiArgs {
-    /// Command to use with `mirrord ci`.
-    #[command(subcommand)]
-    pub command: CiCommand,
-}
-
-/// `mirrord ci` commands.
-#[derive(Subcommand, Debug)]
-pub(super) enum CiCommand {
-    /// Generates a `CiApiKey` that should be set in the ci's environment variable as
-    /// `MIRRORD_CI_API_KEY`.
-    ApiKey {
-        /// Specify config file to use
-        #[arg(short = 'f', long, value_hint = ValueHint::FilePath, default_missing_value = "./.mirrord/mirrord.json", num_args = 0..=1)]
-        config_file: Option<PathBuf>,
-    },
-    /// Starts mirrord for ci. Takes the same arguments as `mirrord exec` plus ci specific options.
-    ///
-    /// - The environment variable `MIRRORD_CI_API_KEY` must be set for this command to work.
-    Start(Box<CiStartArgs>),
-
-    /// Stops mirrord for ci.
-    ///
-    /// - The environment variable `MIRRORD_CI_API_KEY` must be set for this command to work.
-    Stop,
 }
 
 #[derive(Args, Debug)]
