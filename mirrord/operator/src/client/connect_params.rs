1491d27dc1c0fa16ba9d742724ad198cbddc0710
vladrbg <vladr@metalbear.com>
2026-03-31 15:12:21 +0300

    INT-357 RabbitMQ fix for ConnectParams (#4110)
    * rmq connect params fix
    
    * vale

--- mirrord/operator/src/client/connect_params.rs
+++ mirrord/operator/src/client/connect_params.rs
@@ -69,9 +69,19 @@ pub struct ConnectParams<'a> {
     #[serde(
         default,
         skip_serializing_if = "HashMap::is_empty",
-        with = "sqs_output_queues_serde"
+        with = "output_queues_serde"
     )]
     pub sqs_output_queues: HashMap<String, String>,
+
+    /// Multi-cluster: RMQ output queue names.
+    /// Maps original queue names to their output queue names.
+    /// All clusters use the same temp queue names for consistency.
+    #[serde(
+        default,
+        skip_serializing_if = "HashMap::is_empty",
+        with = "output_queues_serde"
+    )]
+    pub rmq_output_queues: HashMap<String, String>,
 
     /// Key for this session
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -121,6 +131,7 @@ impl<'a> ConnectParams<'a> {
             session_ci_info,
             is_default_cluster: None,
             sqs_output_queues: HashMap::new(),
+            rmq_output_queues: HashMap::new(),
             key: Some(key),
         }
     }
@@ -155,22 +166,22 @@ mod force_json_ser {
     }
 }
 
-mod sqs_output_queues_serde {
+mod output_queues_serde {
     use std::collections::HashMap;
 
     use serde::Serializer;
 
     pub fn serialize<S>(
-        sqs_output_queues: &HashMap<String, String>,
+        output_queues: &HashMap<String, String>,
         serializer: S,
     ) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        if sqs_output_queues.is_empty() {
+        if output_queues.is_empty() {
             serializer.serialize_str("")
         } else {
-            let as_json = serde_json::to_string(sqs_output_queues)
+            let as_json = serde_json::to_string(output_queues)
                 .expect("serialization to memory should not fail");
             serializer.serialize_str(&as_json)
         }
