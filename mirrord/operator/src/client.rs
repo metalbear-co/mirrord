62db5b56180ad8280dfa353236aedcd0795ad082
acortiv <tacortiv@proton.me>
2026-04-12 19:24:41 -0600

    test(3164): decouple CredentialStore from kube.rs

--- mirrord/operator/src/client.rs
+++ mirrord/operator/src/client.rs
@@ -15,7 +15,7 @@ use kube::{
 use mirrord_analytics::{AnalyticsHash, AnalyticsOperatorProperties, Reporter};
 use mirrord_auth::{
     certificate::Certificate,
-    credential_store::{CredentialStoreSync, UserIdentity},
+    credential_store::{CredentialClient, CredentialStoreSync, UserIdentity},
     credentials::{CiApiKey, Credentials, LicenseValidity},
 };
 use mirrord_config::{
@@ -883,8 +883,8 @@ where
         })?;
 
         credential_store
-            .get_client_certificate::<MirrordOperatorCrd, MirrordClusterOperatorUserCredential>(
-                &self.client,
+            .get_client_certificate::<_, MirrordOperatorCrd, MirrordClusterOperatorUserCredential>(
+                &self.client.kube_client(),
                 fingerprint,
                 subscription_id,
                 self.operator()
