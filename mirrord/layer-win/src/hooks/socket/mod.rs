953f0ce9e8b331401463d82a5697430ced2f92c3
Daniel Graf <danielg@metalbear.co>
2026-04-30 15:26:48 +0200

    add wsagle to bind failure to debug flaky

--- mirrord/layer-win/src/hooks/socket/mod.rs
+++ mirrord/layer-win/src/hooks/socket/mod.rs
@@ -291,7 +291,11 @@ unsafe extern "system" fn bind_detour(s: SOCKET, name: *const SOCKADDR, namelen:
         let res = unsafe { original(s, addr, addr_len) };
 
         if res != ERROR_SUCCESS_I32 {
-            tracing::error!("bind_detour -> {} failed", reason);
+            tracing::error!(
+                "bind_detour -> {} failed (wsa_last_error={})",
+                reason,
+                unsafe { WSAGetLastError() }
+            );
         } else {
             tracing::debug!("bind_detour -> {} succeeded", reason);
         }
