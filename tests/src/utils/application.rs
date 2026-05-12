7cb7a2846cef6353e0de6852240dc8fd78665572
vladrbg <vladr@metalbear.com>
2026-05-12 14:33:01 +0900

    test

--- tests/src/utils/application.rs
+++ tests/src/utils/application.rs
@@ -54,7 +54,6 @@ pub enum Application {
     PythonCloseSocketKeepConnection,
     RustWebsockets,
     RustSqs,
-    RustAsb,
     /// Tries to open files in the remote target, but these operations should succeed or fail based
     /// on mirrord `FsPolicy`.
     ///
@@ -119,9 +118,6 @@ impl Application {
                 .map(String::from)
                 .to_vec(),
             Application::RustSqs => ["../target/debug/rust-sqs-printer"]
-                .map(String::from)
-                .to_vec(),
-            Application::RustAsb => ["../target/debug/rust-asb-consumer"]
                 .map(String::from)
                 .to_vec(),
             Application::IntproxyChild => {
