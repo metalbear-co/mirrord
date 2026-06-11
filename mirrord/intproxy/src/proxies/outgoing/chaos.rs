3b1b2b7d75b92390ca3ec926d43699b3d61d9e0f
Gemma <58080601+gememma@users.noreply.github.com>
2026-06-11 15:36:46 +0100

    chaos - router, api, rule fixes (#4360)
    * outgoing rules: use random to calculate if a rule hits and latency jitter
    Removed methods on percentage for deterministic probability and jitter calculations,
    switched to using random for both. Also, only record rule hits when the effect is
    applied rather then if a rule gets matched before checking hit probability.
    
    * rule types: add conversion between connection error types and std::io::ErrorKind
    
    * chaos ui: fix router types, session id is not a uuid

--- mirrord/intproxy/src/proxies/outgoing/chaos.rs
+++ mirrord/intproxy/src/proxies/outgoing/chaos.rs
@@ -1,6 +1,7 @@
 use std::{sync::atomic::Ordering, time::Duration};
 
 use mirrord_intproxy_protocol::*;
+use rand::random_range;
 
 use crate::{
     main_tasks::ToLayer,
@@ -14,7 +15,7 @@ use crate::{
 #[derive(Debug)]
 pub enum OutgoingThingToDo {
     Latency {
-        delay: Duration,
+        total_delay: Duration,
         effect: ChaosEffectLatency,
     },
     ConnectionError {
@@ -49,58 +50,40 @@ impl ApplyChaosRuleLol for OutgoingProxyMessage {
             return None;
         };
 
-        match rule.selector {
-            ChaosSelector::Tcp {
-                percentage,
-                effect:
-                    TcpChaosEffect::ConnectionError(effect @ ChaosEffectConnError { error_type, after }),
-                ..
-            } => {
-                let hit_count = rule.hit_count.fetch_add(1, Ordering::Relaxed);
-
-                percentage.should_apply_for_hit(hit_count).then(|| {
-                    OutgoingThingToDo::ConnectionError {
-                        to_layer: ToLayer {
-                            message_id: *message_id,
-                            layer_id: *layer_id,
-                            message: ProxyToLayerMessage::Outgoing(OutgoingResponse::Connect(Err(
-                                ResponseError::Remote(RemoteError::ConnectTimedOut(
-                                    remote_address.clone(),
-                                )),
-                            ))),
-                        },
-                        effect: effect.clone(),
-                    }
-                })
-            }
-            ChaosSelector::Tcp {
-                percentage,
-                effect: TcpChaosEffect::Latency(effect @ ChaosEffectLatency { delay, jitter }),
-                ..
-            } => {
-                let hit_count = rule.hit_count.fetch_add(1, Ordering::Relaxed);
-
-                percentage
-                    .should_apply_for_hit(hit_count)
-                    .then(|| OutgoingThingToDo::Latency {
-                        delay: {
-                            let applied_n_times = percentage.applied_before_hit(hit_count);
-
-                            delay + jitter.saturating_mul(applied_n_times)
-                        },
-                        effect: effect.clone(),
-                    })
-            }
-            ChaosSelector::Tcp {
-                percentage,
-                effect: TcpChaosEffect::Degradation,
-                ..
-            } => {
-                let hit_count = rule.hit_count.fetch_add(1, Ordering::Relaxed);
-                percentage.should_apply_for_hit(hit_count).then(|| ());
-                todo!()
-            }
-            _ => todo!(),
+        if rule.get_selector_percentage().roll_for_hit() {
+            let _ = rule.hit_count.fetch_add(1, Ordering::Relaxed);
+            return match rule.selector {
+                ChaosSelector::Tcp {
+                    effect: TcpChaosEffect::ConnectionError(effect @ ChaosEffectConnError { .. }),
+                    ..
+                } => Some(OutgoingThingToDo::ConnectionError {
+                    to_layer: ToLayer {
+                        message_id: *message_id,
+                        layer_id: *layer_id,
+                        message: ProxyToLayerMessage::Outgoing(OutgoingResponse::Connect(Err(
+                            ResponseError::Remote(RemoteError::ConnectTimedOut(
+                                remote_address.clone(),
+                            )),
+                        ))),
+                    },
+                    effect: effect.clone(),
+                }),
+                ChaosSelector::Tcp {
+                    effect: TcpChaosEffect::Latency(effect @ ChaosEffectLatency { delay, jitter }),
+                    ..
+                } => Some(OutgoingThingToDo::Latency {
+                    total_delay: delay + jitter.div_f32(100.).saturating_mul(random_range(0..=100)),
+                    effect: effect.clone(),
+                }),
+                ChaosSelector::Tcp {
+                    effect: TcpChaosEffect::Degradation,
+                    ..
+                } => {
+                    todo!()
+                }
+                _ => todo!(),
+            };
         }
+        None
     }
 }
