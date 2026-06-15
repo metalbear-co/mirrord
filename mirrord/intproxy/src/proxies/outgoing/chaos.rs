7382c0f498fe385dac756abb5ae1ce0985301925
meowjesty <meowjesty_nyan@outlook.com>
2026-06-15 19:14:56 -0300

    not 2

--- mirrord/intproxy/src/proxies/outgoing/chaos.rs
+++ mirrord/intproxy/src/proxies/outgoing/chaos.rs
@@ -129,7 +129,7 @@ impl OutgoingProxy {
                 })
                 .max_by_key(|rule| rule.priority)?;
 
-            if !rule.get_selector_percentage().roll_for_hit() {
+            if rule.get_selector_percentage().roll_for_hit().not() {
                 return None;
             }
 
