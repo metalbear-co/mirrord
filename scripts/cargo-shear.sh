3028bfb82489366a4a49fc426a86c9dccf9498fe
Daniel Graf <daniel@Daniels-MetalBear-MBP.local>
2026-06-16 12:58:18 +0200

    make clippy, doc-test and shear work with serverless-bootstrap

--- scripts/cargo-shear.sh
+++ scripts/cargo-shear.sh
@@ -6,5 +6,6 @@ placeholder="${PWD}/scripts/cargo-shear.sh"
 
 export MIRRORD_LAYER_FILE="$placeholder"
 export MIRRORD_LAYER_FILE_MACOS_ARM64="$placeholder"
+export MIRRORD_AGENT_BINARY="$placeholder"
 
 exec cargo shear --expand "$@"
