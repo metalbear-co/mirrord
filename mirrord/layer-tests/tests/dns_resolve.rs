797999e1bf04de3ef9fa1b1a4dcb5360df093c6b
MintSoup <aregak2005@gmail.com>
2026-06-25 18:25:49 +0400

    bruh

--- mirrord/layer-tests/tests/dns_resolve.rs
+++ mirrord/layer-tests/tests/dns_resolve.rs
@@ -19,32 +19,32 @@ async fn test_dns_resolve(#[values(Application::RustDnsResolve)] application: Ap
         .start_process(vec![("MIRRORD_REMOTE_DNS", "true")], None)
         .await;
 
-    let msg = intproxy.recv().await;
-    let ClientMessage::GetAddrInfoRequestV2(GetAddrInfoRequestV2 { node, .. }) = msg else {
-        panic!("Invalid message received from layer: {msg:?}");
-    };
-
-    intproxy
-        .send(DaemonMessage::GetAddrInfoResponse(GetAddrInfoResponse(Ok(
-            DnsLookup(vec![LookupRecord {
-                name: node,
-                ip: "93.184.216.34".parse::<std::net::IpAddr>().unwrap(),
-            }]),
-        ))))
-        .await;
-
     // let msg = intproxy.recv().await;
-    // let ClientMessage::GetAddrInfoRequestV2(GetAddrInfoRequestV2 { .. }) = msg else {
+    // let ClientMessage::GetAddrInfoRequestV2(GetAddrInfoRequestV2 { node, .. }) = msg else {
     //     panic!("Invalid message received from layer: {msg:?}");
     // };
 
     // intproxy
-    //     .send(DaemonMessage::GetAddrInfoResponse(GetAddrInfoResponse(
-    //         Err(mirrord_protocol::ResponseError::DnsLookup(DnsLookupError {
-    //             kind: NoRecordsFound(3),
-    //         })),
-    //     )))
+    //     .send(DaemonMessage::GetAddrInfoResponse(GetAddrInfoResponse(Ok(
+    //         DnsLookup(vec![LookupRecord {
+    //             name: node,
+    //             ip: "93.184.216.34".parse::<std::net::IpAddr>().unwrap(),
+    //         }]),
+    //     ))))
     //     .await;
+
+    let msg = intproxy.recv().await;
+    let ClientMessage::GetAddrInfoRequestV2(GetAddrInfoRequestV2 { .. }) = msg else {
+        panic!("Invalid message received from layer: {msg:?}");
+    };
+
+    intproxy
+        .send(DaemonMessage::GetAddrInfoResponse(GetAddrInfoResponse(
+            Err(mirrord_protocol::ResponseError::DnsLookup(DnsLookupError {
+                kind: NoRecordsFound(3),
+            })),
+        )))
+        .await;
 
     test_process.wait_assert_success().await;
     test_process.assert_no_error_in_stderr().await;
