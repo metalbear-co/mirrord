1f224d004543bd492224d1bbc02b690f013aa715
MintSoup <aregak2005@gmail.com>
2026-07-02 11:42:07 +0400

    hello

--- mirrord/layer/src/socket/hooks.rs
+++ mirrord/layer/src/socket/hooks.rs
@@ -655,10 +655,16 @@ pub(crate) unsafe extern "C" fn freeifaddrs_detour(ifaddrs: *mut libc::ifaddrs) 
         return;
     }
     unsafe {
+        eprintln!("got ifaddrs ptr: {ifaddrs:?}");
         let allocation_base = (ifaddrs as *mut u8).sub(std::mem::size_of_val(&ifaddrs));
+        eprintln!("alloc base: {allocation_base:?}");
         // See `getifaddrs` implementation for clarification
-        FN_FREEIFADDRS(*(allocation_base as *mut *mut libc::ifaddrs));
+        let original_allocation = *(allocation_base as *mut *mut libc::ifaddrs);
+        eprintln!("original allocation: {original_allocation:?}");
+        FN_FREEIFADDRS(original_allocation);
+        eprintln!("freed original allocation");
         libc::free(allocation_base as *mut c_void);
+        eprintln!("freed real");
     }
 }
 
