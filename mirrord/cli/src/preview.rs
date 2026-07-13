a1ebdfc9f73a8f51230b455426190b009e0a4d13
Vinicius Deolindo <viniciusd@metalbear.com>
2026-07-13 12:52:36 -0300

    preview: override existing sessions by default, retire `--force` (#4518)
    * preview: don't warn users creating a new preview with an existing key
    
    * preview: make `--force` the default
    
    * flake: add `protobuf` to macos devshell as well :)
    
    * changelog!!
    
    * cr: address ze nits
    
    * cli: remove now unused `PreviewDuplicateSession` error

--- mirrord/cli/src/preview.rs
+++ mirrord/cli/src/preview.rs
@@ -9,7 +9,6 @@ use std::{
     borrow::Cow,
     collections::{BTreeMap, HashMap},
     ffi::OsStr,
-    ops::Not,
     time::{Duration, Instant},
 };
 
@@ -137,29 +136,16 @@ async fn preview_start(
             CliError::PreviewListFailed(e.to_string())
         })?;
 
-    // Check if there's an existing session with the same key and warn the user about it.
-    if existing_sessions.is_empty().not() {
-        progress.warning(&format!(
-            "the key '{key}' is already part of an existing preview environment. \
-            If that’s not what you intended, please switch to a different key."
-        ));
-    }
-
     for session in existing_sessions
         .into_iter()
         .filter(|session| session.spec.target == session_target)
     {
-        if !args.force {
-            subtask.failure(None);
-            return Err(CliError::PreviewDuplicateSession {
-                key: key.to_owned(),
-                target: config_target.to_string(),
-            });
-        }
-
         let name = session.name_any();
 
-        subtask.warning(&format!("replacing existing session '{name}' (--force)",));
+        subtask.warning(&format!("replacing existing session '{name}'"));
+        if &session.spec.image == image {
+            subtask.warning(&format!("configured image and existing session's image are the same ('{image}'), this command will only restart the existing deployment"));
+        }
 
         // Delete and wait for the existing session to be fully removed.
         match tokio::time::timeout(
