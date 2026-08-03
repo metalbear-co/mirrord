8bd8c9021b3480da34b17fe2133fa1ec2097f376
Gemma Tipper <gemmat@metalbear.co>
2026-08-03 09:35:51 +0100

    vpn: use rust_yaml::Value instead of serde_yaml::Value

--- mirrord/vpn/src/config.rs
+++ mirrord/vpn/src/config.rs
@@ -2,7 +2,7 @@ use ipnet::IpNet;
 use k8s_openapi::api::core::v1::ConfigMap;
 use kube::Api;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct VpnConfig {
     pub dns_domain: String,
     pub dns_nameservers: Vec<String>,
@@ -19,25 +19,7 @@ impl VpnConfig {
             )
             .ok()?;
 
-        let cluster_config = serde_saphyr::from_str::<serde_json::Value>(
-            kubeadm_configmap.data?.get("ClusterConfiguration")?,
-        )
-        .inspect_err(|error| tracing::error!(%error, "unable to parse kubeadm config"))
-        .ok()?;
-
-        let dns_domain =
-            serde_json::from_value(cluster_config.get("networking")?.get("dnsDomain")?.clone())
-                .ok()?;
-
-        let service_subnet = serde_json::from_value::<String>(
-            cluster_config
-                .get("networking")?
-                .get("serviceSubnet")?
-                .clone(),
-        )
-        .ok()?
-        .parse()
-        .ok()?;
+        let (dns_domain, service_subnet) = Self::from_kubeadm_configmap(kubeadm_configmap)?;
 
         let kubelet_configmap = api
             .get("kubelet-config")
@@ -47,18 +29,51 @@ impl VpnConfig {
             )
             .ok()?;
 
-        let kubelet_config =
-            serde_saphyr::from_str::<serde_json::Value>(kubelet_configmap.data?.get("kubelet")?)
-                .inspect_err(|error| tracing::error!(%error, "unable to parse kubeadm config"))
-                .ok()?;
-
-        let dns_nameservers =
-            serde_json::from_value(kubelet_config.get("clusterDNS")?.clone()).ok()?;
+        let dns_nameservers = Self::from_kubelet_configmap(kubelet_configmap)?;
 
         Some(VpnConfig {
             dns_domain,
             dns_nameservers,
             service_subnet,
         })
+    }
+
+    pub fn from_kubeadm_configmap(kubeadm_configmap: ConfigMap) -> Option<(String, IpNet)> {
+        let cluster_config = rust_yaml::from_str::<rust_yaml::Value>(
+            kubeadm_configmap.data?.get("ClusterConfiguration")?,
+        )
+        .inspect_err(|error| tracing::error!(%error, "unable to parse kubeadm config"))
+        .ok()?;
+
+        let dns_domain = cluster_config
+            .get_str("networking")?
+            .get_str("dnsDomain")?
+            .as_str()?
+            .to_owned();
+
+        let service_subnet = cluster_config
+            .get_str("networking")?
+            .get_str("serviceSubnet")?
+            .as_str()?
+            .parse()
+            .ok()?;
+
+        Some((dns_domain, service_subnet))
+    }
+
+    pub fn from_kubelet_configmap(kubelet_configmap: ConfigMap) -> Option<Vec<String>> {
+        let kubelet_config =
+            rust_yaml::from_str::<rust_yaml::Value>(kubelet_configmap.data?.get("kubelet")?)
+                .inspect_err(|error| tracing::error!(%error, "unable to parse kubeadm config"))
+                .ok()?;
+
+        let dns_nameservers = kubelet_config
+            .get_str("clusterDNS")?
+            .as_sequence()?
+            .iter()
+            .filter_map(|x| Some(x.as_str()?.to_owned()))
+            .collect();
+
+        Some(dns_nameservers)
     }
 }
